fc0ffb70a709b98686bab1a70f2492d00aa5c5e3
RedCraft86 <sueki.tayzar@gmail.com>
2024-09-22 23:21:21 -0400

    new sound code

--- Source/FaF_Rev/CharacterAI/FRCharacter.cpp
+++ Source/FaF_Rev/CharacterAI/FRCharacter.cpp
@@ -2,10 +2,9 @@
 
 #include "FRCharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
-#include "Libraries/GTRuntimeLibrary.h"
 #include "Components/AudioComponent.h"
 #include "Components/CapsuleComponent.h"
-#include "Kismet/GameplayStatics.h"
+#include "Camera/CameraComponent.h"
 #include "NavigationSystem.h"
 #include "NavigationPath.h"
 #include "FRPlayer.h"
@@ -18,124 +17,32 @@ AFRCharacter::AFRCharacter()
 	FootstepAudio->SetRelativeLocation({0.0f, 0.0f, -50.0f});
 	FootstepAudio->SetupAttachment(GetCapsuleComponent());
 	FootstepAudio->bAutoActivate = false;
-	FootstepAudio->bEnableLowPassFilter = true;
 	FootstepAudio->bOverrideAttenuation = true;
 	FootstepAudio->AttenuationOverrides.bAttenuate = true;
 	FootstepAudio->AttenuationOverrides.AttenuationShapeExtents = FVector::ZeroVector;
 	FootstepAudio->AttenuationOverrides.FalloffDistance = 4500.0f;
 
-	AudioVolumeCurve.GetRichCurve()->UpdateOrAddKey(0.0f, 1.0f);
-	AudioVolumeCurve.GetRichCurve()->UpdateOrAddKey(1.0f, 0.01f);
+	FootstepAudio->AttenuationOverrides.bEnableOcclusion = true;
+	FootstepAudio->AttenuationOverrides.OcclusionLowPassFilterFrequency = MAX_FILTER_FREQUENCY * 0.5f;
+	FootstepAudio->AttenuationOverrides.OcclusionVolumeAttenuation = 0.5f;
+
+	AudioVolumeCurve.GetRichCurve()->UpdateOrAddKey(500.0f, 1.0f);
+	AudioVolumeCurve.GetRichCurve()->UpdateOrAddKey(2500.0f, 0.0f);
 }
 
 void AFRCharacter::PlaySmartAudio(UAudioComponent* InComponent)
 {
 	if (InComponent && InComponent->Sound)
 	{
-	// 	InComponent->AttenuationOverrides.bAttenuate = true;
-	// 	if (const APlayerCameraManager* CM = UGameplayStatics::GetPlayerCameraManager(this, 0))
-	// 	{
-	// 		FCollisionQueryParams TraceParams;
-	// 		TraceParams.AddIgnoredActor(this);
-	// 		TraceParams.AddIgnoredActor(UGameplayStatics::GetPlayerPawn(this, 0));
-	// 		
-	// 		FHitResult HitResult;
-	// 		GetWorld()->LineTraceSingleByChannel(HitResult, CM->GetCameraLocation(),
-	// 			GetActorLocation(), ECC_Visibility, TraceParams);
-	// 		if (!HitResult.bBlockingHit)
-	// 		{
-	// 			InComponent->SetVolumeMultiplier(1.0f);
-	// 			InComponent->SetRelativeLocation(FVector::ZeroVector);
-	// 			InComponent->LowPassFilterFrequency = MAX_FILTER_FREQUENCY;
-	// 			InComponent->Play();
-	// 			OnAudioPlayed.Broadcast(this, InComponent);
-	// 			return;
-	// 		}
-	// 		
-	// 		FVector Origin, BoxExtent;
-	// 		TArray<FVector> Vertices = UGTRuntimeLibrary::GetBoundingBoxVertices(this,
-	// 			true, false, Origin, BoxExtent);
-	// 		Vertices.Add(Origin);
-	// 		for (const FVector& Vert : Vertices)
-	// 		{
-	// 			GetWorld()->LineTraceSingleByChannel(HitResult, CM->GetCameraLocation(),
-	// 				Vert, ECC_Visibility, TraceParams);
-	// 			if (!HitResult.bBlockingHit)
-	// 			{
-	// 				InComponent->SetVolumeMultiplier(1.0f);
-	// 				InComponent->SetRelativeLocation(FVector::ZeroVector);
-	// 				InComponent->LowPassFilterFrequency = MAX_FILTER_FREQUENCY;
-	// 				InComponent->Play();
-	// 				OnAudioPlayed.Broadcast(this, InComponent);
-	// 				return;
-	// 			}
-	// 		}
-	//
-	// 		const UNavigationPath* Path = UNavigationSystemV1::FindPathToLocationSynchronously(this,
-	// 			GetActorLocation(), CM->GetCameraLocation(), this);
-	// 		const TArray<FVector> PathPoints = Path ? Path->PathPoints : TArray<FVector>();
-	// 		const float PathLen = Path ? Path->GetPathLength() : 0.0f;
-	// 		if (PathLen < 1000.0f || PathPoints.IsEmpty())
-	// 		{
-	// 			InComponent->SetVolumeMultiplier(1.0f);
-	// 			InComponent->SetRelativeLocation(FVector::ZeroVector);
-	// 			InComponent->LowPassFilterFrequency = MAX_FILTER_FREQUENCY;
-	// 			InComponent->Play();
-	// 			OnAudioPlayed.Broadcast(this, InComponent);
-	// 			return;
-	// 		}
-	//
-	// 		if (PathLen > InComponent->AttenuationOverrides.FalloffDistance)
-	// 		{
-	// 			InComponent->SetVolumeMultiplier(0.05f);
-	// 			InComponent->SetRelativeLocation(FVector::ZeroVector);
-	// 			InComponent->LowPassFilterFrequency = MAX_FILTER_FREQUENCY;
-	// 			InComponent->Play();
-	// 			OnAudioPlayed.Broadcast(this, InComponent);
-	// 		}
-	// 		else if (PathPoints.Num() > 1)
-	// 		{
-	// 			InComponent->AttenuationOverrides.bAttenuate = false;
-	// 			
-	// 			FVector2D CurveRange; AudioVolumeCurve.GetTimeRange(CurveRange.X, CurveRange.Y);
-	// 			InComponent->SetVolumeMultiplier(AudioVolumeCurve.GetValue(FMath::GetMappedRangeValueClamped(
-	// 				FVector2D(0.0f, InComponent->AttenuationOverrides.FalloffDistance),
-	// 				CurveRange, PathLen)));
-	//
-	// 			InComponent->LowPassFilterFrequency = MAX_FILTER_FREQUENCY * 0.5f;
-	// 			InComponent->SetWorldLocation(PathPoints[PathPoints.Num() - 2] + FVector(0.0f, 0.0f, 70.0f));
-	// 			InComponent->Play();
-	// 			OnAudioPlayed.Broadcast(this, InComponent);
-	//
-	// #if WITH_EDITORONLY_DATA
-	// 			if (bDebugAudio)
-	// 			{
-	// #if WITH_EDITOR
-	// 				UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("[%s, %s] Vol: %f | Pos: %s"),
-	// 					*GetName(), *InComponent->Sound->GetName(), InComponent->VolumeMultiplier, *InComponent->GetComponentLocation().ToString()),
-	// 					true, false, DebugColor, 1.0f, GetFName());
-	// 				
-	// 				DrawDebugSphere(GetWorld(), PathPoints[PathPoints.Num() - 1] + FVector(0.0f, 0.0f, 70.0f),
-	// 					8, 16, DebugColor, false, 1.0f, 0, 1);
-	// 				
-	// 				for (int i = 0; i < PathPoints.Num() - 1; i++)
-	// 				{
-	// 				
-	// 					DrawDebugLine(GetWorld(), PathPoints[i] + FVector(0.0f, 0.0f, 70.0f),
-	// 						PathPoints[i + 1] + FVector(0.0f, 0.0f, 70.0f),
-	// 						DebugColor, false, 1.0f, 0, 1);
-	// 				
-	// 					DrawDebugSphere(GetWorld(), PathPoints[i] + FVector(0.0f, 0.0f, 70.0f),
-	// 						8, 16, DebugColor, false, 1.0f, 0, 1);
-	// 				}
-	// #endif
-	// 			}
-	// #endif
-	// 		}
-	// 	}
-	// 	else
+		if (const AFRPlayerBase* Player = PlayerChar.LoadSynchronous())
 		{
+			const UNavigationPath* Path = UNavigationSystemV1::FindPathToLocationSynchronously(this,
+				GetActorLocation(), Player->PlayerCamera->GetComponentLocation(), this);
+
+			const float Dist = FMath::Clamp(Path ? Path->GetPathLength() : 0.0f, 500.0f, 2500.0f);
+			InComponent->SetVolumeMultiplier(AudioVolumeCurve.GetValue(Dist));
 			InComponent->Play();
+			
 			OnAudioPlayed.Broadcast(this, InComponent);
 		}
 	}
