3a486cd70a3137de960e9006e2f9898d31ae0457
RedCraft86 <sueki.tayzar@gmail.com>
2024-09-28 19:31:01 -0400

    virtual mouse

---
+++ Source/FaF_Rev/UserWidgets/VirtualMouseWidget.cpp
@@ -0,0 +1,59 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "VirtualMouseWidget.h"
+#include "Components/CanvasPanelSlot.h"
+
+UVirtualMouse::UVirtualMouse() : bPreviewOnly(true)
+{
+	Background.ImageSize = FVector2D(16.0f);
+	Background.TintColor = FLinearColor::Gray;
+	Background.DrawAs = ESlateBrushDrawType::Type::RoundedBox;
+	Background.OutlineSettings.Color = FLinearColor::Black;
+	Background.OutlineSettings.bUseBrushTransparency = true;
+	Background.OutlineSettings.Width = 2.0f;
+
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
+	Padding = {1.0f};
+	BrushColor = {1.0f, 1.0f, 1.0f, 0.5f};
+	Visibility = ESlateVisibility::HitTestInvisible;
+	//RenderTransform.Translation = FVector2D(-8.0f);
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+}
+
+void UVirtualMouse::SimulateClick()
+{
+	FSlateApplication& SlateApp = FSlateApplication::Get();
+
+	const FVector2D Pos = GetCachedGeometry().LocalToAbsolute(FVector2D::ZeroVector);
+	const FPointerEvent MouseEvent(0, SlateApp.CursorPointerIndex, Pos,
+		Pos, SlateApp.GetPressedMouseButtons(), EKeys::LeftMouseButton,
+		0, SlateApp.GetPlatformApplication()->GetModifierKeys()
+	);
+
+	SlateApp.ProcessMouseButtonDownEvent(nullptr, MouseEvent);
+	SlateApp.ProcessMouseButtonUpEvent(MouseEvent);
+}
+
+void UVirtualMouse::SynchronizeProperties()
+{
+	Super::SynchronizeProperties();
+	if (FApp::IsGame())
+	{
+		if (bPreviewOnly || GetVisibility() == ESlateVisibility::Collapsed) // We cannot be Collapsed either.
+		{
+			SetRenderOpacity(0.0f);
+			SetVisibility(ESlateVisibility::HitTestInvisible);
+		}
+	}
+#if WITH_EDITOR
+	if (CachedSlot != Slot)
+	{
+		CachedSlot = Slot;
+		if (UCanvasPanelSlot* CanvasSlot = Cast<UCanvasPanelSlot>(Slot))
+		{
+			CanvasSlot->SetSize(FVector2D(20.0f));
+		}
+	}
+#endif
+}
+
---
+++ Source/FaF_Rev/UserWidgets/VirtualMouseWidget.h
@@ -0,0 +1,31 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Components/Border.h"
+#include "VirtualMouseWidget.generated.h"
+
+UCLASS()
+class FAF_REV_API UVirtualMouse : public UBorder
+{
+	GENERATED_BODY()
+
+public:
+
+	UVirtualMouse();
+
+	/** If enabled, the widget will be hidden in-game (Zero Opacity) but not collapsed. */
+	UPROPERTY(EditAnywhere, Category = "Appearance", AdvancedDisplay)
+		uint8 bPreviewOnly:1;
+
+	UFUNCTION(BlueprintCallable, Category = "VirtualMouse")
+		void SimulateClick();
+
+protected:
+	
+	virtual void SynchronizeProperties() override;
+#if WITH_EDITOR
+	virtual const FText GetPaletteCategory() override { return FText::FromString("Input"); }
+private: TObjectPtr<UPanelSlot> CachedSlot = nullptr;
+#endif
+};
