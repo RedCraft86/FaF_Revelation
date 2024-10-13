393b4f70b0bd771af0381e91ebb28dcb2b359999
RedCraft86 <sueki.tayzar@gmail.com>
2024-10-13 14:49:13 -0400

    heart beat game widget

---
+++ Source/FaF_Rev/Gameplay/HeartBeatGame/HeartBeatGameWidget.cpp
@@ -0,0 +1,196 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "HeartBeatGameWidget.h"
+#include "VirtualMouseWidget.h"
+#include "Animation/UMGSequencePlayer.h"
+#include "Blueprint/WidgetTree.h"
+#include "Components/TextBlock.h"
+#include "Components/HorizontalBox.h"
+#include "Components/Button.h"
+#include "Components/Image.h"
+
+void UHeartBeatGameButton::ButtonClicked()
+{
+	SetVisibility(ESlateVisibility::HitTestInvisible);
+	if (!bPressed)
+	{
+		Cooldown = 0.2f;
+		PlayAnimation(SuccessAnim);
+		if (Parent) Parent->OnCorrectKey();
+	}
+	else
+	{
+		PlayAnimation(FailAnim);
+		if (Parent) Parent->OnWrongKey();
+	}
+}
+
+void UHeartBeatGameButton::NativeConstruct()
+{
+	Super::NativeConstruct();
+	Button->OnClicked.AddUniqueDynamic(this, &UHeartBeatGameButton::ButtonClicked);
+}
+
+void UHeartBeatGameButton::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+	if (Cooldown > 0.0f)
+	{
+		Cooldown -= InDeltaTime;
+		if (Cooldown <= 0.0f)
+		{
+			SetVisibility(ESlateVisibility::Visible);
+		}
+	}
+	
+	if (!Parent || bStopTick) return;
+	if (MyGeometry.LocalToAbsolute(FVector2D::ZeroVector).X + MyGeometry.GetAbsoluteSize().X + 5.0f < Parent->VirtualMouse->GetPosition().X)
+	{
+		if (!bPressed)
+		{
+			PlayAnimation(FailAnim);
+			if (Parent) Parent->OnMissedKey();
+			SetVisibility(ESlateVisibility::HitTestInvisible);
+		}
+
+		bStopTick = true;
+	}
+}
+
+void UHeartBeatGameWidget::StartGame(const FKey InKeyA, const FKey InKeyB, const uint8 InMaxChances, const TArray<FString> InSequence)
+{
+	if (bInGame || InSequence.IsEmpty() || InMaxChances <= 0 || !InKeyA.IsValid() || !InKeyB.IsValid())
+	{
+		return;
+	}
+
+	bInGame = true;
+	Sequence = InSequence;
+	SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+
+	Chances = InMaxChances;
+	MaxChance->SetText(FText::FromString(FString::FromInt(InMaxChances)));
+	CurChance->SetText(MaxChance->GetText());
+			
+	KeyRange = {InKeyA, InKeyB};
+	KeyImageA->SetBrush(GetKeyIcon(InKeyA.GetFName()).Brush);
+	KeyImageA->SetBrush(GetKeyIcon(InKeyA.GetFName()).Brush);
+			
+	Buttons.Empty();
+	Container->ClearChildren();
+
+	FTimerHandle Handle;
+	GetWorld()->GetTimerManager().SetTimer(Handle, this, &UHeartBeatGameWidget::AddChildEntries, 0.05f, false);
+
+	PlayAnimation(FadeAnim);
+}
+
+void UHeartBeatGameWidget::OnWrongKey()
+{
+	Chances--;
+	CurChance->SetText(FText::FromString(FString::FromInt(Chances)));
+	PlayAnimation(WrongAnim);
+	ProcessNextButton();
+}
+
+void UHeartBeatGameWidget::OnMissedKey()
+{
+	Buttons.RemoveAt(0);
+	OnWrongKey();
+}
+
+void UHeartBeatGameWidget::OnCorrectKey()
+{
+	Buttons.RemoveAt(0);
+	ProcessNextButton();
+}
+
+void UHeartBeatGameWidget::AddChildEntries()
+{
+	EmptySpaceImage.ImageSize.X = GetCachedGeometry().GetAbsoluteSize().X * 0.5f;
+	UImage* InitialIMG = WidgetTree->ConstructWidget<UImage>();
+	InitialIMG->SetBrush(EmptySpaceImage);
+	Container->AddChild(InitialIMG);
+			
+	for (const FString& Element : Sequence)
+	{
+		if (Element.ToLower().StartsWith(TEXT("key")))
+		{
+			UHeartBeatGameButton* BTN = WidgetTree->ConstructWidget<UHeartBeatGameButton>(ButtonClass);
+			BTN->bAltKey = Element.ToLower().EndsWith(TEXT("b"));
+			BTN->Parent = this;
+			Buttons.Add(BTN);
+			Container->AddChild(BTN);
+		}
+		else if (const float AsFloat = FCString::Atof(*Element); AsFloat > 0.01f)
+		{
+			EmptySpaceImage.ImageSize.X = AsFloat;
+			UImage* IMG = WidgetTree->ConstructWidget<UImage>();
+			IMG->SetBrush(EmptySpaceImage);
+			Container->AddChild(IMG);
+		}
+	}
+
+	EmptySpaceImage.ImageSize.X = GetCachedGeometry().GetAbsoluteSize().X;
+	UImage* EndIMG = WidgetTree->ConstructWidget<UImage>();
+	EndIMG->SetBrush(EmptySpaceImage);
+	Container->AddChild(EndIMG);
+
+	FTimerHandle Handle;
+	GetWorld()->GetTimerManager().SetTimer(Handle, this, &UHeartBeatGameWidget::StartScrollAnim, 0.05f, false);
+}
+
+void UHeartBeatGameWidget::StartScrollAnim()
+{
+	PlayAnimation(ScrollAnim, 0, 1, EUMGSequencePlayMode::Forward, ScrollRate *
+		FMath::Min(1.0f, GetCachedGeometry().GetLocalSize().X / Container->GetCachedGeometry().GetLocalSize().X));
+}
+
+void UHeartBeatGameWidget::RemoveWidget()
+{
+	if (UUMGSequencePlayer* Player = PlayAnimation(FadeAnim, 0, 1, EUMGSequencePlayMode::Reverse))
+	{
+		Player->OnSequenceFinishedPlaying().AddLambda([this](UUMGSequencePlayer&)
+		{
+			StopAnimation(ScrollAnim);
+			RemoveFromParent();
+		});
+	}
+}
+
+void UHeartBeatGameWidget::ProcessNextButton()
+{
+	if (Chances <= 0)
+	{
+		bInGame = false;
+		Buttons.Empty();
+		OnFail.Broadcast();
+		RemoveWidget();
+		return;
+	}
+	if (Buttons.IsEmpty())
+	{
+		bInGame = false;
+		OnSuccess.Broadcast();
+		FTimerHandle Handle;
+		GetWorld()->GetTimerManager().SetTimer(Handle, [this]()
+		{
+			RemoveWidget();
+		}, 1.0f, false);
+		return;
+	}
+	if (bLastKey != Buttons[0]->bAltKey)
+	{
+		bLastKey = Buttons[0]->bAltKey;
+		CurrentKey = bLastKey ? KeyRange.Value : KeyRange.Key;
+		PlayAnimation(SwapAnim, 0, 1, bLastKey ? EUMGSequencePlayMode::Forward : EUMGSequencePlayMode::Reverse);
+	}
+}
+
+void UHeartBeatGameWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	SetVisibility(ESlateVisibility::Collapsed);
+	BackingButton->OnClicked.AddUniqueDynamic(this, &UHeartBeatGameWidget::OnWrongKey);
+	bInGame = false;
+}
---
+++ Source/FaF_Rev/Gameplay/HeartBeatGame/HeartBeatGameWidget.h
@@ -0,0 +1,134 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "Components/RichTextBlockImageDecorator.h"
+#include "HeartBeatGameWidget.generated.h"
+
+class UImage;
+class UButton;
+class UTextBlock;
+
+UCLASS(Abstract)
+class FAF_REV_API UHeartBeatGameButton final : public UUserWidget
+{
+	GENERATED_BODY()
+
+	friend class UHeartBeatGameWidget;
+
+public:
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		TObjectPtr<UButton> Button;
+
+	UPROPERTY(Transient, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> SuccessAnim;
+
+	UPROPERTY(Transient, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> FailAnim;
+
+protected:
+	
+	bool bAltKey = false;
+	float Cooldown = 0.0f;
+	bool bPressed = false;
+	bool bStopTick = false;
+	UPROPERTY(Transient) TObjectPtr<UHeartBeatGameWidget> Parent;
+	
+	UFUNCTION() void ButtonClicked();
+	virtual void NativeConstruct() override;
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+};
+
+UCLASS(Abstract)
+class FAF_REV_API UHeartBeatGameWidget final : public UUserWidget
+{
+	GENERATED_BODY()
+
+	//friend class UHeartBeatGame;
+	friend class UHeartBeatGameButton;
+
+public:
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		TObjectPtr<class UVirtualMouse> VirtualMouse;
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		TObjectPtr<class UHorizontalBox> Container;
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		TObjectPtr<UButton> BackingButton;
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		TObjectPtr<UTextBlock> MaxChance;
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		TObjectPtr<UTextBlock> CurChance;
+	
+	UPROPERTY(Transient, meta = (BindWidget))
+		TObjectPtr<UImage> KeyImageA;
+
+	UPROPERTY(Transient, meta = (BindWidget))
+		TObjectPtr<UImage> KeyImageB;
+
+	UPROPERTY(Transient, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> FadeAnim;
+	
+	UPROPERTY(Transient, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> ScrollAnim;
+
+	UPROPERTY(Transient, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> WrongAnim;
+
+	UPROPERTY(Transient, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> SwapAnim;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Settings")
+		TSubclassOf<UHeartBeatGameButton> ButtonClass;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Settings")
+		float ScrollRate = 1.0f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Settings")
+		FSlateBrush EmptySpaceImage;
+	
+	UFUNCTION(BlueprintImplementableEvent)
+		FRichImageRow GetKeyIcon(const FName& InKey);
+
+	UFUNCTION(BlueprintCallable, Category = "HeartBeatGame")
+		uint8 GetChances() const { return Chances; }
+
+	UFUNCTION(BlueprintCallable, Category = "HeartBeatGame")
+		FKey GetCurrentKey() const { return CurrentKey; }
+
+	UFUNCTION(BlueprintCallable, Category = "HeartBeatGame")
+		void StartGame(const FKey InKeyA, const FKey InKeyB, const uint8 InMaxChances, const TArray<FString> InSequence);
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FGameCallback);
+	UPROPERTY(BlueprintAssignable) FGameCallback OnSuccess;
+	UPROPERTY(BlueprintAssignable) FGameCallback OnFail;
+
+protected:
+	
+	uint8 Chances = 3;
+	bool bInGame = false;
+	TArray<FString> Sequence;
+	TPair<FKey, FKey> KeyRange;
+	bool bLastKey = false;
+	FKey CurrentKey;
+	
+	//UPROPERTY(Transient) TObjectPtr<UHeartBeatGame> Controller;
+	UPROPERTY(Transient) TArray<TObjectPtr<UHeartBeatGameButton>> Buttons;
+
+	UFUNCTION() void OnWrongKey();
+	UFUNCTION() void OnMissedKey();
+	UFUNCTION() void OnCorrectKey();
+
+	void AddChildEntries();
+	void StartScrollAnim();
+
+	void RemoveWidget();
+	void ProcessNextButton();
+	virtual void NativeConstruct() override;
+};
