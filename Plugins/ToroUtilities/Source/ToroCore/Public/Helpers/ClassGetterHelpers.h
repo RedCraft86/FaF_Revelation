7130f6e9e8a73d7df447de8f7688e475b1d5685b
RedCraft86 <sueki.tayzar@gmail.com>
2024-12-25 11:42:04 -0500

    Subsystem getter templates

---
+++ Plugins/ToroUtilities/Source/ToroCore/Public/Helpers/ClassGetterHelpers.h
@@ -0,0 +1,29 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/LocalPlayer.h"
+#include "Engine/GameInstance.h"
+#include "Kismet/GameplayStatics.h"
+
+#define LOCAL_PLAYER_SUBSYSTEM_GETTER(Class) \
+	static Class* Get(const UObject* WorldContext, const int PlayerIndex = 0) \
+	{ \
+		const APlayerController* Controller = UGameplayStatics::GetPlayerController(WorldContext, PlayerIndex); \
+		const ULocalPlayer* LocalPlayer = Controller ? Controller->GetLocalPlayer() : nullptr; \
+		return LocalPlayer ? LocalPlayer->GetSubsystem<Class>() : nullptr; \
+	}
+
+#define GAME_INSTANCE_SUBSYSTEM_GETTER(Class) \
+	static Class* Get(const UObject* WorldContext) \
+	{ \
+		const UWorld* World = WorldContext ? GEngine->GetWorldFromContextObject(WorldContext, EGetWorldErrorMode::LogAndReturnNull) : nullptr; \
+		const UGameInstance* GI = World ? World->GetGameInstance() : nullptr; \
+		return GI ? GI->GetSubsystem<Class>() : nullptr; \
+	}
+
+#define ENGINE_SUBSYSTEM_GETTER(Class) \
+	static Class* Get(const UObject* WorldContext) \
+	{ \
+		return GEngine ? GEngine->GetEngineSubsystem<Class>() : nullptr; \
+	}
