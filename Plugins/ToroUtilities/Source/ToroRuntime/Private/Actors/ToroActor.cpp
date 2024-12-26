317e9790f4355078ce6debdf8d70d7773bf0f2c3
RedCraft86 <sueki.tayzar@gmail.com>
2024-12-25 23:23:10 -0500

    Migrate custom actor class

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/Actors/ToroActor.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Actors/ToroActor.h"
+
+AToroActor::AToroActor() : bEnabled(true), bStartWithCollisionEnabled(true)
+	, RuntimeGuid(FGuid::NewGuid())
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SetRootComponent(SceneRoot);
+}
+
+void AToroActor::SetEnabled(const bool bInEnabled)
+{
+	if (bEnabled != bInEnabled)
+	{
+		bEnabled = bInEnabled;
+		ON_ENABLE_STATE_CHANGED
+	}
+}
+
+void AToroActor::BeginPlay()
+{
+	Super::BeginPlay();
+	SetActorEnableCollision(bStartWithCollisionEnabled);
+	if (!bEnabled) OnEnableStateChanged(bEnabled);
+}
+
+void AToroActor::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	if (!RuntimeGuid.IsValid())
+	{
+		RuntimeGuid = FGuid::NewGuid();
+	}
+}
+
+void AToroActor::OnEnableStateChanged(const bool bIsEnabled)
+{
+	SetActorHiddenInGame(!bIsEnabled);
+	SetActorEnableCollision(bIsEnabled);
+	SetActorTickEnabled(PrimaryActorTick.bStartWithTickEnabled && bIsEnabled);
+}
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Actors/ToroActor.h
@@ -0,0 +1,53 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "ToroActor.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActorEnableChangedSignature, const bool, bEnabled);
+
+#define ON_ENABLE_STATE_CHANGED { OnEnableStateChanged(bEnabled); OnEnableStateChangedEvent.Broadcast(bEnabled); OnEnableStateChangedBP.Broadcast(bEnabled); }
+
+UCLASS(Abstract)
+class TORORUNTIME_API AToroActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	AToroActor();
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Subobjects")
+		TObjectPtr<USceneComponent> SceneRoot;
+
+	UPROPERTY(BlueprintAssignable, DisplayName = "On Enable State Changed")
+		FActorEnableChangedSignature OnEnableStateChangedBP;
+	
+	UFUNCTION(BlueprintCallable, Category = "Actor")
+		virtual void SetEnabled(const bool bInEnabled);
+	
+	UFUNCTION(BlueprintPure, Category = "Actor")
+		bool IsEnabled() const { return bEnabled; }
+	
+	UFUNCTION(BlueprintPure, Category = "Actor")
+		FGuid GetRuntimeGuid() const { return RuntimeGuid; }
+
+	DECLARE_MULTICAST_DELEGATE_OneParam(FActorEnableChanged, const bool);
+	FActorEnableChanged OnEnableStateChangedEvent;
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Settings", meta = (DisplayPriority = -10))
+		bool bEnabled;
+
+	UPROPERTY(EditAnywhere, Category = "Collision")
+		bool bStartWithCollisionEnabled;
+	
+	UPROPERTY(EditAnywhere, Category = "Actor", NonPIEDuplicateTransient, TextExportTransient, NonTransactional)
+		FGuid RuntimeGuid;
+	
+	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void OnEnableStateChanged(const bool bIsEnabled);
+};
