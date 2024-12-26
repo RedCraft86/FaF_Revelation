bac7f417ec0f90109f3ce4058d079b90d6fbd2fe
RedCraft86 <sueki.tayzar@gmail.com>
2024-12-26 00:08:28 -0500

    Migrate custom volume

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/Actors/ToroVolume.cpp
@@ -0,0 +1,76 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "ToroVolume.h"
+#include "Components/BrushComponent.h"
+#include "Components/BillboardComponent.h"
+
+AToroVolume::AToroVolume() : bEnabled(true), RuntimeGuid(FGuid::NewGuid())
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	
+	GetBrushComponent()->SetCollisionResponseToAllChannels(ECR_Ignore);
+	GetBrushComponent()->SetCollisionObjectType(ECC_WorldDynamic);
+	GetBrushComponent()->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
+
+#if WITH_EDITORONLY_DATA
+	DefaultIconBillboard = CreateEditorOnlyDefaultSubobject<UBillboardComponent>("DefaultIconBillboard");
+	DefaultIconBillboard->SetSprite(LoadObject<UTexture2D>(nullptr, TEXT("/Engine/EditorResources/S_TriggerBox.S_TriggerBox")));
+	DefaultIconBillboard->SetWorldScale3D(FVector{0.5f});
+	DefaultIconBillboard->bIsScreenSizeScaled = true;
+	DefaultIconBillboard->bIsEditorOnly = true;
+	DefaultIconBillboard->SetVisibility(false);
+	DefaultIconBillboard->SetHiddenInGame(true);
+	DefaultIconBillboard->SetIsVisualizationComponent(true);
+	DefaultIconBillboard->SetupAttachment(GetRootComponent());
+#endif
+
+	SetCanBeDamaged(false);
+}
+
+void AToroVolume::SetEnabled(const bool bInEnabled)
+{
+	if (bEnabled != bInEnabled)
+	{
+		bEnabled = bInEnabled;
+		ON_ENABLE_STATE_CHANGED
+	}
+}
+
+void AToroVolume::BeginPlay()
+{
+	Super::BeginPlay();
+	if (!bEnabled) ON_ENABLE_STATE_CHANGED
+#if WITH_EDITORONLY_DATA
+	if (DefaultIconBillboard)
+	{
+		DefaultIconBillboard->DestroyComponent();
+	}
+#endif
+}
+
+void AToroVolume::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	if (!RuntimeGuid.IsValid())
+	{
+		RuntimeGuid = FGuid::NewGuid();
+	}
+#if WITH_EDITORONLY_DATA
+	if (DefaultIconBillboard)
+	{
+		TArray<USceneComponent*> TempChildren;
+		DefaultIconBillboard->GetChildrenComponents(false, TempChildren);
+		for (USceneComponent* Child : TempChildren)
+		{
+			Child->AttachToComponent(GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+		}
+	}
+#endif
+}
+
+void AToroVolume::OnEnableStateChanged(const bool bIsEnabled)
+{
+	SetActorHiddenInGame(!bIsEnabled);
+	SetActorEnableCollision(bIsEnabled);
+}
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Actors/ToroVolume.h
@@ -0,0 +1,50 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "ToroActor.h"
+#include "ActorIconData.h"
+#include "GameFramework/Volume.h"
+#include "ToroVolume.generated.h"
+
+UCLASS()
+class TORORUNTIME_API AToroVolume : public AVolume
+{
+	GENERATED_BODY()
+
+public:
+
+	AToroVolume();
+
+#if WITH_EDITORONLY_DATA
+	UPROPERTY(VisibleDefaultsOnly, Category = "Subobjects")
+		TObjectPtr<UBillboardComponent> DefaultIconBillboard;
+#endif
+
+	UPROPERTY(BlueprintAssignable, DisplayName = "On Enable State Changed")
+		FActorEnableChangedSignature OnEnableStateChangedBP;
+	
+	UFUNCTION(BlueprintCallable, Category = "Actor")
+		void SetEnabled(const bool bInEnabled);
+	
+	UFUNCTION(BlueprintPure, Category = "Actor")
+		bool IsEnabled() const { return bEnabled; }
+	
+	UFUNCTION(BlueprintPure, Category = "Actor")
+		FGuid GetRuntimeGuid() const { return RuntimeGuid; }
+
+	DECLARE_MULTICAST_DELEGATE_OneParam(FActorEnableChanged, const bool);
+		FActorEnableChanged OnEnableStateChangedEvent;
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Settings", meta = (DisplayPriority = -10))
+		bool bEnabled;
+	
+	UPROPERTY(EditAnywhere, Category = "Settings", NonPIEDuplicateTransient, TextExportTransient, NonTransactional, meta = (DisplayPriority = -10))
+		FGuid RuntimeGuid;
+	
+	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void OnEnableStateChanged(const bool bIsEnabled);
+};
