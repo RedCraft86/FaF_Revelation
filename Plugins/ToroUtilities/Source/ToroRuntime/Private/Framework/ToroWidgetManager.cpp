bc253d597f8638bc44406669a64598d3e6b144a7
RedCraft86 <sueki.tayzar@gmail.com>
2024-12-27 21:54:22 -0500

    Widget manager

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/Framework/ToroWidgetManager.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "ToroWidgetManager.h"
+#include "Blueprint/UserWidget.h"
+
+EToroValidPins AToroWidgetManager::GetToroWidgetManager(AToroWidgetManager*& OutObject,
+	const UObject* WorldContextObject, const TSubclassOf<AToroWidgetManager>& Class, const int32 PlayerIndex)
+{
+	AToroWidgetManager* Obj = Cast<AToroWidgetManager>(UGameplayStatics::GetPlayerState(WorldContextObject, PlayerIndex));
+	OutObject = IsValid(Obj) && (!Class || Obj->IsA(Class)) ? Obj : nullptr;
+	return IsValid(OutObject) ? EToroValidPins::Valid : EToroValidPins::NotValid;
+}
+
+UUserWidget* AToroWidgetManager::FindOrAddWidget(const TSubclassOf<UUserWidget>& Class)
+{
+	if (!Class) return nullptr;
+	UUserWidget* Widget = FindWidget(Class);
+	if (!Widget)
+	{
+		Widget = CreateWidget<UUserWidget>(GetPlayerController(), Class);
+		WidgetObjs.Add(Class, Widget);
+	}
+	return Widget;
+}
+
+UUserWidget* AToroWidgetManager::FindWidget(const TSubclassOf<UUserWidget>& Class)
+{
+	if (!Class) return nullptr;
+	if (const TObjectPtr<UUserWidget>* Widget = WidgetObjs.Find(Class); Widget && *Widget)
+	{
+		return *Widget;
+	}
+
+	return nullptr;
+}
+
+void AToroWidgetManager::BeginPlay()
+{
+	Super::BeginPlay();
+	for (const TSubclassOf<UUserWidget>& Class : DefaultWidgets)
+	{
+		UUserWidget* Widget = CreateWidget<UUserWidget>(GetPlayerController(), Class);
+		WidgetObjs.Add(Class, Widget);
+	}
+}
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Framework/ToroWidgetManager.h
@@ -0,0 +1,48 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "ExecPinEnums.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerState.h"
+#include "ToroWidgetManager.generated.h"
+
+/* Player State is repurposed as a Widget Manager */
+
+UCLASS()
+class TORORUNTIME_API AToroWidgetManager : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+
+	AToroWidgetManager() {}
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		TSet<TSubclassOf<UUserWidget>> DefaultWidgets;
+
+	UFUNCTION(BlueprintCallable, Category = "Game", meta = (WorldContext = "WorldContextObject", DynamicOutputParam = "OutObject", DeterminesOutput = "Class", ExpandEnumAsExecs = "ReturnValue", AutoCreateRefTerm = "Class", CompactNodeTitle = "Get Widget Manager"))
+		static EToroValidPins GetToroWidgetManager(AToroWidgetManager*& OutObject, const UObject* WorldContextObject, const TSubclassOf<AToroWidgetManager>& Class, const int32 PlayerIndex = 0);
+
+	template <typename T = AToroWidgetManager>
+	static T* Get(const UObject* WorldContextObject, const int32 PlayerIndex = 0)
+	{
+		return Cast<T>(UGameplayStatics::GetPlayerState(WorldContextObject, PlayerIndex));
+	}
+	
+	UFUNCTION(BlueprintCallable, Category = "WidgetManager", meta = (DynamicOutputParam = "ReturnValue", DeterminesOutputType = "Class"))
+		UUserWidget* FindOrAddWidget(const TSubclassOf<UUserWidget>& Class);
+	
+	UFUNCTION(BlueprintPure, Category = "WidgetManager", meta = (DynamicOutputParam = "ReturnValue", DeterminesOutputType = "Class"))
+		UUserWidget* FindWidget(const TSubclassOf<UUserWidget>& Class);
+
+	template<typename T = UUserWidget>
+	T* GetWidget() { return Cast<T>(FindWidget(T::StaticClass())); }
+	
+protected:
+	
+	UPROPERTY(Transient)
+		TMap<TSubclassOf<UUserWidget>, TObjectPtr<UUserWidget>> WidgetObjs;
+
+	virtual void BeginPlay() override;
+};
