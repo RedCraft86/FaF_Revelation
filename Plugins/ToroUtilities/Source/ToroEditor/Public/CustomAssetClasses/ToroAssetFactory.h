b1cfea156bc495084c0011959ef9abd98bcdadfc
RedCraft86 <sueki.tayzar@gmail.com>
2024-12-31 18:27:44 -0500

    Custom asset editor classes

---
+++ Plugins/ToroUtilities/Source/ToroEditor/Public/CustomAssetClasses/ToroAssetFactory.h
@@ -0,0 +1,43 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Factories/Factory.h"
+#include "ToroAssetFactory.generated.h"
+
+UCLASS(Abstract)
+class TOROEDITOR_API UToroAssetFactory : public UFactory
+{
+	GENERATED_BODY()
+
+public:
+
+	UToroAssetFactory()
+	{
+		bCreateNew = true;
+		bEditAfterNew = true;
+		SupportedClass = UDataAsset::StaticClass();
+	}
+
+	virtual bool ConfigureProperties() override
+	{
+		DataClass = UDataAsset::StaticClass();
+		return DataClass ? true : false;
+	}
+
+	virtual FString GetDefaultNewAssetName() const override
+	{
+		return TEXT("New") + (DataClass ? DataClass->GetName() : GetSupportedClass()->GetName());
+	}
+
+	virtual UObject* FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn) override
+	{
+		const UClass* ObjClass = DataClass ? *DataClass : Class;
+		check(ObjClass->IsChildOf(SupportedClass));
+		return NewObject<UDataAsset>(InParent, ObjClass, Name, Flags | RF_Transactional);
+	}
+	
+protected:
+
+	TSubclassOf<UDataAsset> DataClass;
+};
---
+++ Plugins/ToroUtilities/Source/ToroEditor/Public/CustomAssetClasses/ToroAssetTypeActions.h
@@ -0,0 +1,23 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "AssetTypeActions_Base.h"
+
+class FToroAssetTypeActions : public FAssetTypeActions_Base
+{
+public:
+
+	explicit FToroAssetTypeActions() {}
+	
+	virtual uint32 GetCategories() override
+	{
+		if (const FAssetToolsModule* AssetToolsModule = FModuleManager::LoadModulePtr<FAssetToolsModule>("AssetTools"))
+		{
+			return AssetToolsModule->Get().RegisterAdvancedAssetCategory(TEXT("CustomTypes"),
+				NSLOCTEXT("AssetCategory", "CustomAssetCategory", "Custom Assets"));
+		}
+
+		return EAssetTypeCategories::Misc;
+	}
+};
