fb0f515b65a9c1c7593e84644743eaf504ccd43a
RedCraft86 <sueki.tayzar@gmail.com>
2025-01-02 14:45:54 -0500

    move music manager

--- Plugins/ToroUtilities/Source/ToroRuntime/Public/MusicSystem/MusicManager.h
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/MusicSystem/MusicManager.h
@@ -5,26 +5,26 @@
 #include "ExecPinEnums.h"
 #include "Kismet/GameplayStatics.h"
 #include "GameFramework/GameStateBase.h"
-#include "ToroMusicManager.generated.h"
+#include "MusicManager.generated.h"
 
 /* Game State is repurposed as a Widget Manager */
 
 UCLASS()
-class TORORUNTIME_API AToroMusicManager : public AGameStateBase
+class TORORUNTIME_API AMusicManager : public AGameStateBase
 {
 	GENERATED_BODY()
 
 public:
 
-	AToroMusicManager();
+	AMusicManager();
 
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Subobjects")
 		TObjectPtr<USceneComponent> SceneRoot;
 
 	UFUNCTION(BlueprintCallable, Category = "Game", meta = (WorldContext = "WorldContextObject", DynamicOutputParam = "OutObject", DeterminesOutput = "Class", ExpandEnumAsExecs = "ReturnValue", AutoCreateRefTerm = "Class", CompactNodeTitle = "Get Music Manager"))
-		static EToroValidPins GetToroMusicManager(AToroMusicManager*& OutObject, const UObject* WorldContextObject, const TSubclassOf<AToroMusicManager>& Class);
+		static EToroValidPins GetMusicManager(AMusicManager*& OutObject, const UObject* WorldContextObject, const TSubclassOf<AMusicManager>& Class);
 
-	template <typename T = AToroMusicManager>
+	template <typename T = AMusicManager>
 	static T* Get(const UObject* WorldContextObject)
 	{
 		return Cast<T>(UGameplayStatics::GetGameState(WorldContextObject));
