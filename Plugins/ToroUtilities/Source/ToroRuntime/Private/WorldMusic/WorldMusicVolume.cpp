b707d00d8d503fa703b69098813d0eec46fae6fd
RedCraft86 <sueki.tayzar@gmail.com>
2025-01-06 23:17:15 -0500

    music volume for one shots

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/WorldMusic/WorldMusicVolume.cpp
@@ -0,0 +1,42 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "WorldMusic/WorldMusicVolume.h"
+#include "Kismet/GameplayStatics.h"
+
+AWorldMusicVolume::AWorldMusicVolume() : bStopOnExit(false), Cooldown(10.0f)
+	, FadeTime(1.0), Volume(1.0), StartRange(0.0f)
+{
+	PrimaryActorTick.bCanEverTick = false;
+#if WITH_EDITOR
+	if (DebugIcon) DebugIcon->IconPath = TEXT("/ToroUtilities/Icons/MusicVolume.MusicVolume");
+#endif
+}
+
+void AWorldMusicVolume::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	if (GetWorldTimerManager().TimerExists(CooldownTimer)) return;
+	if (MusicManager && OtherActor == Player)
+	{
+		MusicManager->PlayLayer(Sound, FadeTime, Volume, StartRange);
+	}
+}
+
+void AWorldMusicVolume::NotifyActorEndOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorEndOverlap(OtherActor);
+	if (bStopOnExit && MusicManager && OtherActor == Player)
+	{
+		MusicManager->StopLayer(Sound, FadeTime);
+	}
+	
+	GetWorldTimerManager().SetTimer(CooldownTimer, this,
+		&AWorldMusicVolume::EmptyFunction, Cooldown, false);
+}
+
+void AWorldMusicVolume::BeginPlay()
+{
+	Super::BeginPlay();
+	Player = UGameplayStatics::GetPlayerPawn(this, 0);
+	MusicManager = AWorldMusicManager::Get(this);
+}
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/WorldMusic/WorldMusicVolume.h
@@ -0,0 +1,47 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "ToroVolume.h"
+#include "WorldMusicManager.h"
+#include "WorldMusicVolume.generated.h"
+
+UCLASS()
+class TORORUNTIME_API AWorldMusicVolume final : public AToroVolume
+{
+	GENERATED_BODY()
+
+public:
+	
+	AWorldMusicVolume();
+	
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		bool bStopOnExit;
+	
+	UPROPERTY(EditAnywhere, Category = "Settings", meta = (ClampMin = 1.0f))
+		float Cooldown;
+
+	UPROPERTY(EditAnywhere, Category = "Settings", NoClear)
+		TObjectPtr<USoundBase> Sound;
+
+	UPROPERTY(EditAnywhere, Category = "Settings|Sound", meta = (EditCondition = "Sound", ClampMin = 0.1f))
+		float FadeTime;
+
+	UPROPERTY(EditAnywhere, Category = "Settings|Sound", meta = (EditCondition = "Sound", ClampMin = 0.1f))
+		float Volume;
+
+	UPROPERTY(EditAnywhere, Category = "Settings|Sound", meta = (EditCondition = "Sound", ClampMin = 0.0f))
+		FVector2D StartRange;
+
+private:
+
+	UPROPERTY() FTimerHandle CooldownTimer;
+	UPROPERTY(Transient) TObjectPtr<APawn> Player;
+	UPROPERTY(Transient) TObjectPtr<AWorldMusicManager> MusicManager;
+
+	void EmptyFunction() { CooldownTimer.Invalidate(); }
+	
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	virtual void NotifyActorEndOverlap(AActor* OtherActor) override;
+	virtual void BeginPlay() override;
+};
