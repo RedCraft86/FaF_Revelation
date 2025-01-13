0007a9f6f4005284bc0de5ec08a0916ed8198ced
RedCraft86 <sueki.tayzar@gmail.com>
2025-01-12 23:32:37 -0500

    toro settings base

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/UserSettings/ToroUserSettings.cpp
@@ -0,0 +1,163 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "UserSettings/ToroUserSettings.h"
+
+DEFINE_PROPERTY_SETTER(bool, ShowFPS,{
+	
+})
+
+DEFINE_PROPERTY_SETTER(FString, Username, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, FieldOfView, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(bool, SmoothCamera, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(float, SensitivityX, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(float, SensitivityY, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(float, Gamma, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, Brightness, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(bool, FancyBloom, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(bool, ScreenSpaceFogScattering, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, MotionBlurAmount, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(bool, LumenGI, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, LumenGIQuality, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(bool, ReflectionMethod, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, LumenReflectionMode, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, LumenReflectionQuality, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(EColorBlindMode, ColorBlindMode, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, ColorBlindIntensity, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(bool, RTXDynamicVibrance, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(float, DynamicVibranceIntensity, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(float, DynamicVibranceSaturation, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(EImageFidelityMode, ImageFidelityMode, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, FXAADithering, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, TAAUpsampling, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, TSRResolution, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, SMAAQuality, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, SMAAEdgeDetection, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, DLSSQuality, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(float, DLSSSharpness, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(bool, DLSSRayReconstruction, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(bool, DLSSFrameGeneration, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, NvidiaReflex, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, FSRQuality, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(float, FSRSharpness, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(bool, FSRFrameGeneration, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(bool, FSRAsyncFrameInterp, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, XeSSQuality, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(uint8, NISQuality, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(float, NISSharpness, {
+	
+})
+
+DEFINE_PROPERTY_SETTER(float, NISScreenPercentage, {
+	
+})
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/UserSettings/ToroUserSettings.h
@@ -0,0 +1,208 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/GameUserSettings.h"
+#include "ToroUserSettings.generated.h"
+
+UENUM(BlueprintType)
+enum class EColorBlindMode : uint8
+{
+	None,
+	
+	// Deuteranope (red-green weak/blind)
+	Deuteranope,
+	
+	// Protanope (red weak/blind)
+	Protanope,
+	
+	// Tritanope (blue-yellow weak / bind)
+	Tritanope
+};
+
+UENUM(BlueprintType)
+enum class ESoundType : uint8
+{
+	Master,
+	Music,
+	SoundFX,
+	Voice
+};
+
+UENUM(BlueprintType)
+enum class EImageFidelityMode : uint8
+{
+	// No Anti-Aliasing or Upscaling
+	None,
+	
+	// Fast Approximate Anti-Aliasing
+	FXAA,
+
+	// Temporal Anti-Aliasing
+	TAA,
+
+	// Enhanced Subpixel Morphological Anti-Aliasing
+	SMAA,
+
+	// Temporal Super-Resolution (Upscaler)
+	TSR,
+
+	// Nvidia Deep Learning Super Sampling (Upscaler)
+	DLSS,
+
+	// FidelityFX™ Super Resolution 3.1 (Upscaler)
+	FSR,
+
+	// Intel® Xe Super Sampling (Upscaler)
+	XeSS,
+	
+	// Nvidia Image Scaling (Upscaler)
+	NIS
+};
+
+#define DECLARE_PROPERTY_FUNCTIONS(Type, Name) \
+		void Set##Name(const Type In##Name); \
+		Type Get##Name() const { return Name; } \
+
+#define DEFINE_PROPERTY_SETTER(Type, Name, Code) \
+	void UToroUserSettings::Set##Name(const Type In##Name) \
+		Code \
+
+#define DECLARE_CONVERTABLE_FUNCTIONS(Type, Name, AltType, AltTypeName) \
+	DECLARE_PROPERTY_FUNCTIONS(Type, Name) \
+	void Set##Name##AltTypeName(const AltType In##Name) { Set##Name(static_cast<Type>(In##Name)); } \
+	AltType Get##Name##AltTypeName() const { return static_cast<AltType>(Name); }
+
+UCLASS(NotBlueprintable, BlueprintType)
+class TORORUNTIME_API UToroUserSettings final : public UGameUserSettings
+{
+	GENERATED_BODY()
+
+public:
+
+	static UToroUserSettings* Get() { return Cast<UToroUserSettings>(GEngine->GameUserSettings); }
+
+	DECLARE_PROPERTY_FUNCTIONS(bool, ShowFPS)
+	DECLARE_PROPERTY_FUNCTIONS(FString, Username)
+	
+	DECLARE_PROPERTY_FUNCTIONS(uint8, FieldOfView)
+	DECLARE_PROPERTY_FUNCTIONS(bool, SmoothCamera)
+	DECLARE_PROPERTY_FUNCTIONS(float, SensitivityX)
+	DECLARE_PROPERTY_FUNCTIONS(float, SensitivityY)
+	
+	DECLARE_PROPERTY_FUNCTIONS(float, Gamma)
+	DECLARE_PROPERTY_FUNCTIONS(uint8, Brightness)
+	DECLARE_PROPERTY_FUNCTIONS(bool, FancyBloom)
+	DECLARE_PROPERTY_FUNCTIONS(bool, ScreenSpaceFogScattering)
+	DECLARE_PROPERTY_FUNCTIONS(uint8, MotionBlurAmount)
+
+	DECLARE_PROPERTY_FUNCTIONS(bool, LumenGI)
+	DECLARE_PROPERTY_FUNCTIONS(uint8, LumenGIQuality)
+	
+	DECLARE_PROPERTY_FUNCTIONS(bool, ReflectionMethod)
+	DECLARE_PROPERTY_FUNCTIONS(uint8, LumenReflectionMode)
+	DECLARE_PROPERTY_FUNCTIONS(uint8, LumenReflectionQuality)
+	
+	DECLARE_CONVERTABLE_FUNCTIONS(EColorBlindMode, ColorBlindMode, uint8, Int)
+	DECLARE_PROPERTY_FUNCTIONS(uint8, ColorBlindIntensity)
+	
+	DECLARE_PROPERTY_FUNCTIONS(bool, RTXDynamicVibrance)
+	DECLARE_PROPERTY_FUNCTIONS(float, DynamicVibranceIntensity)
+	DECLARE_PROPERTY_FUNCTIONS(float, DynamicVibranceSaturation)
+	
+	DECLARE_CONVERTABLE_FUNCTIONS(EImageFidelityMode, ImageFidelityMode, uint8, Int)
+	DECLARE_PROPERTY_FUNCTIONS(uint8, FXAADithering)
+	DECLARE_PROPERTY_FUNCTIONS(uint8, TAAUpsampling)
+	DECLARE_PROPERTY_FUNCTIONS(uint8, TSRResolution)
+	
+	DECLARE_PROPERTY_FUNCTIONS(uint8, SMAAQuality)
+	DECLARE_PROPERTY_FUNCTIONS(uint8, SMAAEdgeDetection)
+
+	DECLARE_PROPERTY_FUNCTIONS(uint8, DLSSQuality)
+	DECLARE_PROPERTY_FUNCTIONS(float, DLSSSharpness)
+	DECLARE_PROPERTY_FUNCTIONS(bool, DLSSRayReconstruction)
+	DECLARE_PROPERTY_FUNCTIONS(bool, DLSSFrameGeneration)
+	DECLARE_PROPERTY_FUNCTIONS(uint8, NvidiaReflex)
+
+	DECLARE_PROPERTY_FUNCTIONS(uint8, FSRQuality)
+	DECLARE_PROPERTY_FUNCTIONS(float, FSRSharpness)
+	DECLARE_PROPERTY_FUNCTIONS(bool, FSRFrameGeneration)
+	DECLARE_PROPERTY_FUNCTIONS(bool, FSRAsyncFrameInterp)
+	
+	DECLARE_PROPERTY_FUNCTIONS(uint8, XeSSQuality)
+	
+	DECLARE_PROPERTY_FUNCTIONS(uint8, NISQuality)
+	DECLARE_PROPERTY_FUNCTIONS(float, NISSharpness)
+	DECLARE_PROPERTY_FUNCTIONS(float, NISScreenPercentage)
+	
+private:
+
+	/* Misc */
+	UPROPERTY(Config) bool ShowFPS;
+	UPROPERTY(Config) FString Username;
+	UPROPERTY(Config) TMap<ESoundType, uint8> SoundTypeToVolume; // float type 0 -> 100 (100 default)
+
+	/* Camera */
+	UPROPERTY(Config) uint8 FieldOfView; // float type -25 -> +25 [0 -> 50] (25 default)
+	UPROPERTY(Config) bool SmoothCamera;
+	UPROPERTY(Config) float SensitivityX;
+	UPROPERTY(Config) float SensitivityY;
+
+	/* Graphics */
+	UPROPERTY(Config) float Gamma;
+	UPROPERTY(Config) uint8 Brightness;  // float type 25 -> 200 (100 default)
+	UPROPERTY(Config) bool FancyBloom;
+	UPROPERTY(Config) bool ScreenSpaceFogScattering;
+	UPROPERTY(Config) uint8 MotionBlurAmount;
+
+	// Global Illumination
+	UPROPERTY(Config) bool LumenGI;
+	UPROPERTY(Config) uint8 LumenGIQuality;
+
+	// Reflections
+	UPROPERTY(Config) uint8 ReflectionMethod;
+	UPROPERTY(Config) uint8 LumenReflectionMode;
+	UPROPERTY(Config) uint8 LumenReflectionQuality;
+
+	/* Color Correction */
+	UPROPERTY(Config) EColorBlindMode ColorBlindMode;
+	UPROPERTY(Config) uint8 ColorBlindIntensity;
+
+	// Nvidia RTX Dynamic Vibrance
+	UPROPERTY(Config) bool RTXDynamicVibrance;
+	UPROPERTY(Config) float DynamicVibranceIntensity;
+	UPROPERTY(Config) float DynamicVibranceSaturation;
+
+	/* Anti-Aliasing / Upscaling */
+	UPROPERTY(Config) EImageFidelityMode ImageFidelityMode;
+
+	// Native AA / Upscaling
+	UPROPERTY(Config) uint8 FXAADithering;
+	UPROPERTY(Config) uint8 TAAUpsampling;
+	UPROPERTY(Config) uint8 TSRResolution; // float type 50 -> 200 (100 default)
+
+	// SMAA
+	UPROPERTY(Config) uint8 SMAAQuality;
+	UPROPERTY(Config) uint8 SMAAEdgeDetection;
+
+	// DLSS
+	UPROPERTY(Config) uint8 DLSSQuality;
+	UPROPERTY(Config) float DLSSSharpness;
+	UPROPERTY(Config) bool DLSSRayReconstruction;
+	UPROPERTY(Config) bool DLSSFrameGeneration;
+	UPROPERTY(Config) uint8 NvidiaReflex;
+
+	// FSR
+	UPROPERTY(Config) uint8 FSRQuality;
+	UPROPERTY(Config) float FSRSharpness;
+	UPROPERTY(Config) bool FSRFrameGeneration; // Frame Interpolation
+	UPROPERTY(Config) bool FSRAsyncFrameInterp; // Default true (Maybe this is why there's latency?)
+
+	// XeSS
+	UPROPERTY(Config) uint8 XeSSQuality;
+
+	// NIS
+	UPROPERTY(Config) uint8 NISQuality;
+	UPROPERTY(Config) float NISSharpness;
+	UPROPERTY(Config) float NISScreenPercentage;
+};
