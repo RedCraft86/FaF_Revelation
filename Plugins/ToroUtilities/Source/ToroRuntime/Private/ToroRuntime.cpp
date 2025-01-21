0caf14a39abdac719063a6374827421a827ef834
RedCraft86 <sueki.tayzar@gmail.com>
2025-01-20 19:56:09 -0500

    more tags

--- Plugins/ToroUtilities/Source/ToroRuntime/Private/ToroRuntime.cpp
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/ToroRuntime.cpp
@@ -3,9 +3,12 @@
 #include "ToroRuntime.h"
 
 DEFINE_LOG_CATEGORY(LogToroRuntime);
+
+UE_DEFINE_GAMEPLAY_TAG(Tag_Zone, "Zone");
+UE_DEFINE_GAMEPLAY_TAG(Tag_OneShot, "OneShot");
 UE_DEFINE_GAMEPLAY_TAG(Tag_Character, "Character");
 UE_DEFINE_GAMEPLAY_TAG(Tag_Player, "Character.Player");
-UE_DEFINE_GAMEPLAY_TAG(Tag_OneShot, "OneShot");
-UE_DEFINE_GAMEPLAY_TAG(Tag_Zone, "Zone");
+UE_DEFINE_GAMEPLAY_TAG(Tag_Friendly, "Character.Friendly");
+UE_DEFINE_GAMEPLAY_TAG(Tag_Enemy, "Character.Enemy");
     
 IMPLEMENT_MODULE(FToroRuntimeModule, ToroRuntime)
