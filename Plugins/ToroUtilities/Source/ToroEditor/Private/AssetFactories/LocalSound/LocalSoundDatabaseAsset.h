c3c60a81e2c4b656e21bf6f65dc79de32539e81c
RedCraft86 <sueki.tayzar@gmail.com>
2025-01-20 20:31:36 -0500

    sound database description

--- Plugins/ToroUtilities/Source/ToroEditor/Private/AssetFactories/LocalSound/LocalSoundDatabaseAsset.h
+++ Plugins/ToroUtilities/Source/ToroEditor/Private/AssetFactories/LocalSound/LocalSoundDatabaseAsset.h
@@ -13,4 +13,19 @@ public:
 	virtual FColor GetTypeColor() const override { return FColor(160, 25, 100); }
 	virtual UClass* GetSupportedClass() const override { return ULocalSoundDatabase::StaticClass(); }
 	virtual FText GetName() const override { return NSLOCTEXT("ToroEditor", "LocalSoundDatabaseName", "Local Sound Database"); }
+	virtual FText GetAssetDescription(const FAssetData& AssetData) const override
+	{
+		const ULocalSoundDatabase* DB = Cast<ULocalSoundDatabase>(AssetData.GetAsset());
+		const int32 All = DB->LocalSounds.Num(), Valid = DB->GetValidCount();
+		FText OutText = FText::Format(INVTEXT("All: {0}\nValid: {1}\nInvalid: {2}"),
+			All, Valid, All - Valid);
+
+		const FText SuperText = FAssetTypeActions_Base::GetAssetDescription(AssetData);
+		if (!SuperText.IsEmptyOrWhitespace())
+		{
+			OutText = FText::Format(INVTEXT("{0}\n{2}"), OutText, SuperText);
+		}
+
+		return OutText;
+	}
 };
