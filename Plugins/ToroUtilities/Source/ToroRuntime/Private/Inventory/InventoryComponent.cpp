1e1dff1cca35a04700c7112f4fcad6a30cf620f8
RedCraft86 <sueki.tayzar@gmail.com>
2025-01-28 23:28:07 -0500

    basic functions

--- Plugins/ToroUtilities/Source/ToroRuntime/Private/Inventory/InventoryComponent.cpp
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/Inventory/InventoryComponent.cpp
@@ -19,6 +19,16 @@ bool FInventoryMetaFilter::Filter(const FInventorySlotData& InSlot) const
 	return false;
 }
 
+bool FInventoryMetaFilter::Filter(const UInventoryItemData* InItem, const FInventorySlotData& InSlot) const
+{
+	if (InSlot.IsValidData() && InItem && InSlot.GetItem() == InItem)
+	{
+		return Filter(InSlot);
+	}
+
+	return false;
+}
+
 FInventorySlotData::FInventorySlotData(const TObjectPtr<UInventoryItemData>& InItem, const uint8 InAmount, const FInventoryMetadata& InMetadata)
 	: Item(InItem), Amount(InAmount), Metadata(InMetadata)
 {
@@ -46,4 +56,185 @@ UInventoryComponent::UInventoryComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 	PrimaryComponentTick.TickGroup = TG_DuringPhysics;
+}
+
+TArray<FGuid> UInventoryComponent::GetSortedSlots() const
+{
+	TArray<TPair<FGuid, FInventorySlotData>> Slots;
+	Slots.Reserve(ItemSlots.Num());
+
+	for (const TPair<FGuid, FInventorySlotData>& Slot : ItemSlots)
+	{
+		if (Slot.Value.IsValidData())
+		{
+			Slots.Emplace(Slot.Key, Slot.Value);
+		}
+	}
+
+	Slots.Sort([](const TPair<FGuid, FInventorySlotData>& A, const TPair<FGuid, FInventorySlotData>& B)
+	{
+		const UInventoryItemData *ItemA = A.Value.GetItem(), *ItemB = B.Value.GetItem();
+		if (ItemA->Priority != ItemB->Priority)
+		{
+			return ItemA->Priority < ItemB->Priority;
+		}
+
+		return ItemA->DisplayName.CompareTo(ItemB->DisplayName) < 0;
+	});
+
+	TArray<FGuid> Out;
+	Out.Reserve(Slots.Num());
+	for (const TPair<FGuid, FInventorySlotData>& Slot : Slots)
+	{
+		Out.Emplace(Slot.Key);
+	}
+
+	return Out;
+}
+
+FGuid UInventoryComponent::FindFirstSlot(const UInventoryItemData* Item, const FInventoryMetaFilter& Filter)
+{
+	for (const TPair<FGuid, FInventorySlotData>& Slot : ItemSlots)
+	{
+		if (Filter.Filter(Item, Slot.Value)) return Slot.Key;
+	}
+
+	return {};
+}
+
+TSet<FGuid> UInventoryComponent::FindSlots(const UInventoryItemData* Item, const FInventoryMetaFilter& Filter)
+{
+	TSet<FGuid> Matches;
+	for (const TPair<FGuid, FInventorySlotData>& Slot : ItemSlots)
+	{
+		if (Filter.Filter(Item, Slot.Value)) Matches.Add(Slot.Key);
+	}
+
+	return {};
+}
+
+void UInventoryComponent::AppendSlotMetadata(const FGuid& InSlot, const FInventoryMetadata& InMetadata)
+{
+	if (ItemSlots.Contains(InSlot))
+	{
+		ItemSlots[InSlot].Metadata.Append(InMetadata);
+		ItemSlots[InSlot].Metadata.Validate();
+	}
+}
+
+void UInventoryComponent::AddSlotMetadata(const FGuid& InSlot, const FGameplayTag InKey, const FString InValue)
+{
+	if (ItemSlots.Contains(InSlot))
+	{
+		ItemSlots[InSlot].Metadata.Add(InKey, InValue);
+		ItemSlots[InSlot].Metadata.Validate();
+	}
+}
+
+void UInventoryComponent::RemoveSlotMetadata(const FGuid& InSlot, const FGameplayTag InKey)
+{
+	if (ItemSlots.Contains(InSlot))
+	{
+		ItemSlots[InSlot].Metadata.Remove(InKey);
+		ItemSlots[InSlot].Metadata.Validate();
+	}
+}
+
+bool UInventoryComponent::SlotHasMetadata(const FGuid& InSlot, const FGameplayTag InKey, const FString InValue)
+{
+	if (!InKey.IsValid() && ItemSlots.Contains(InSlot))
+	{
+		return ItemSlots[InSlot].GetMetadata().HasMetadata(
+			{InKey, InValue}, !InValue.Equals(TEXT("*FilterAny")));
+	}
+
+	return false;
+}
+
+uint8 UInventoryComponent::AddItemToSlot(const FGuid& InSlot, const uint8 Amount, const bool bSilent)
+{
+	if (ItemSlots.Contains(InSlot) && Amount > 0)
+	{
+		uint8 Overflow = 0;
+		FInventorySlotData& SlotRef = ItemSlots[InSlot];
+
+		SlotRef.Amount += Amount;
+		const UInventoryItemData* Item = SlotRef.GetItem();
+		const uint8 StackSizeLimit = Item->GetStackLimit();
+		if (SlotRef.Amount > StackSizeLimit)
+		{
+			Overflow = SlotRef.Amount - StackSizeLimit;
+			SlotRef.Amount = StackSizeLimit;
+		}
+
+		ValidateInventory();
+		if (!bSilent) ON_ITEM_ADDED(Item, Amount - Overflow);
+		return Overflow;
+	}
+
+	return Amount;
+}
+
+void UInventoryComponent::AddInventoryItem(uint8& Overflow, TSet<FGuid>& Slots, const UInventoryItemData* Item, const uint8 Amount, const FInventoryMetadata& Metadata, const bool bSilent)
+{
+	// TODO: implement AddInventoryItem
+}
+
+uint8 UInventoryComponent::AddItem(const UInventoryItemData* Item, const uint8 Amount, const FInventoryMetadata& Metadata, const bool bSilent)
+{
+	uint8 Overflow;
+	TSet<FGuid> Slots;
+	AddInventoryItem(Overflow, Slots, Item, Amount, Metadata, bSilent);
+	return Overflow;
+}
+
+uint8 UInventoryComponent::RemoveItemFromSlot(const FGuid& InSlot, const uint8 Amount, const bool bSilent)
+{
+	if (ItemSlots.Contains(InSlot) && Amount > 0)
+	{
+		uint8 Missing = 0;
+		bool bChanged = false;
+		FInventorySlotData& SlotRef = ItemSlots[InSlot];
+
+		SlotRef.Amount -= Amount;
+		const UInventoryItemData* Item = SlotRef.GetItem();
+		if (SlotRef.Amount <= 0)
+		{
+			Missing = FMath::Abs(SlotRef.Amount);
+			ItemSlots.Remove(InSlot);
+			bChanged = true;
+		}
+
+		ValidateInventory(bChanged);
+		if (!bSilent) ON_ITEM_REMOVED(Item, Amount - Missing);
+		return Missing;
+	}
+
+	return Amount;
+}
+
+void UInventoryComponent::RemoveInventoryItem(uint8& Missing, const UInventoryItemData* Item, const uint8 Amount, const FInventoryMetaFilter& Filter, const bool bSilent)
+{
+	// TODO: implement RemoveInventoryItem
+}
+
+uint8 UInventoryComponent::RemoveItem(const UInventoryItemData* Item, const uint8 Amount, const FInventoryMetaFilter& Filter, const bool bSilent)
+{
+	uint8 Missing;
+	RemoveInventoryItem(Missing, Item, Amount, Filter, bSilent);
+	return Missing;
+}
+
+void UInventoryComponent::ValidateInventory(const bool bForceUpdate)
+{
+	bool bChanged = bForceUpdate;
+	for (auto It = ItemSlots.CreateIterator(); It; ++It)
+	{
+		if (!It.Key().IsValid() || !It.Value().IsValidData())
+		{
+			bChanged = true;
+			It.RemoveCurrent();
+		}
+	}
+	if (bChanged) ON_UPDATE()
 }
--- Plugins/ToroUtilities/Source/ToroRuntime/Public/Inventory/InventoryComponent.h
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Inventory/InventoryComponent.h
@@ -36,10 +36,11 @@ struct TORORUNTIME_API FInventoryMetaFilter
 	{}
 	
 	FInventoryMetaFilter(const EInventoryMetaFilterMode InMode, const bool InCompareValues, const TMap<FGameplayTag, FString>& InMetadata)
-		: FilterMode(InMode), bCompareValues (InCompareValues), Metadata(InMetadata)
+		: FilterMode(InMode), bCompareValues(InCompareValues), Metadata(InMetadata)
 	{}
 
 	bool Filter(const struct FInventorySlotData& InSlot) const;
+	bool Filter(const UInventoryItemData* InItem, const FInventorySlotData& InSlot) const;
 };
 
 USTRUCT(BlueprintType)
@@ -71,6 +72,13 @@ struct TORORUNTIME_API FInventorySlotData
 	UInventoryItemData* GetItem() const;
 };
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FInventoryUpdateSignature);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FInventoryIOUpdateSignature, const UInventoryItemData*, Item, const uint8, Count);
+
+#define ON_UPDATE() { OnInventoryUpdate(); OnUpdate.Broadcast(); OnUpdateBP.Broadcast(); }
+#define ON_ITEM_ADDED(Item, Count) { OnItemAdded.Broadcast(Item, Count); OnItemAddedBP.Broadcast(Item, Count); }
+#define ON_ITEM_REMOVED(Item, Count) { OnItemRemoved.Broadcast(Item, Count); OnItemRemovedBP.Broadcast(Item, Count); }
+
 UCLASS(NotBlueprintable, ClassGroup = (Game), meta = (BlueprintSpawnableComponent))
 class TORORUNTIME_API UInventoryComponent : public UActorComponent
 {
@@ -80,7 +88,67 @@ public:
 	
 	UInventoryComponent();
 
+	UFUNCTION(BlueprintPure, Category = InventoryManager)
+		virtual const TMap<FGuid, FInventorySlotData>& GetConstInventory() const { return ItemSlots; }
+	
+	UFUNCTION(BlueprintPure, Category = InventoryManager)
+		virtual bool IsValidSlot(const FGuid& InSlot) const { return InSlot.IsValid() && ItemSlots.Contains(InSlot); }
+	
+	UFUNCTION(BlueprintPure, Category = InventoryManager)
+		virtual TArray<FGuid> GetSortedSlots() const;
+	
+	UFUNCTION(BlueprintPure, Category = InventoryManager, meta = (AdvancedDisplay = "Filter"))
+		virtual FGuid FindFirstSlot(const UInventoryItemData* Item, const FInventoryMetaFilter& Filter = FInventoryMetaFilter());
+
+	UFUNCTION(BlueprintPure, Category = InventoryManager, meta = (AdvancedDisplay = "Filter"))
+		virtual TSet<FGuid> FindSlots(const UInventoryItemData* Item, const FInventoryMetaFilter& Filter = FInventoryMetaFilter());
+	
+	UFUNCTION(BlueprintCallable, Category = InventoryManager)
+		virtual void AppendSlotMetadata(const FGuid& InSlot, const FInventoryMetadata& InMetadata);
+
+	UFUNCTION(BlueprintCallable, Category = InventoryManager)
+		virtual void AddSlotMetadata(const FGuid& InSlot, const FGameplayTag InKey, const FString InValue);
+	
+	UFUNCTION(BlueprintCallable, Category = InventoryManager)
+		virtual void RemoveSlotMetadata(const FGuid& InSlot, const FGameplayTag InKey);
+
+	UFUNCTION(BlueprintCallable, Category = InventoryManager, meta = (AdvancedDisplay = "InValue"))
+		virtual bool SlotHasMetadata(const FGuid& InSlot, const FGameplayTag InKey, const FString InValue = TEXT("*FilterAny"));
+	
+	UFUNCTION(BlueprintCallable, Category = InventoryManager, meta = (AdvancedDisplay = "bSilent"))
+		virtual uint8 AddItemToSlot(const FGuid& InSlot, const uint8 Amount, const bool bSilent = false);
+	
+	UFUNCTION(BlueprintCallable, Category = InventoryManager, meta = (AdvancedDisplay = "bSilent"))
+		virtual void AddInventoryItem(uint8& Overflow, TSet<FGuid>& Slots, const UInventoryItemData* Item, const uint8 Amount, const FInventoryMetadata& Metadata, const bool bSilent = false);
+	uint8 AddItem(const UInventoryItemData* Item, const uint8 Amount, const FInventoryMetadata& Metadata, const bool bSilent = false);
+
+	UFUNCTION(BlueprintCallable, Category = InventoryManager, meta = (AdvancedDisplay = "bSilent"))
+		virtual uint8 RemoveItemFromSlot(const FGuid& InSlot, const uint8 Amount, const bool bSilent = false);
+    
+	UFUNCTION(BlueprintCallable, Category = InventoryManager, meta = (AdvancedDisplay = "Filter,bSilent"))
+		virtual void RemoveInventoryItem(uint8& Missing, const UInventoryItemData* Item, const uint8 Amount, const FInventoryMetaFilter& Filter = FInventoryMetaFilter(), const bool bSilent = false);
+	uint8 RemoveItem(const UInventoryItemData* Item, const uint8 Amount, const FInventoryMetaFilter& Filter = {}, const bool bSilent = false);
+	
+	DECLARE_MULTICAST_DELEGATE(FInventoryUpdateEvent);
+	FInventoryUpdateEvent OnUpdate;
+	
+	DECLARE_MULTICAST_DELEGATE_TwoParams(FInventoryIOUpdateEvent, const UInventoryItemData*, const uint8);
+	FInventoryIOUpdateEvent OnItemAdded, OnItemRemoved;
+	
 protected:
 
-	UPROPERTY() TArray<FInventorySlotData> Slots;
+	UPROPERTY(BlueprintAssignable, DisplayName = "On Update")
+		FInventoryUpdateSignature OnUpdateBP;
+	
+	UPROPERTY(BlueprintAssignable, DisplayName = "On Item Added")
+		FInventoryIOUpdateSignature OnItemAddedBP;
+	
+	UPROPERTY(BlueprintAssignable, DisplayName = "On Item Removed")
+		FInventoryIOUpdateSignature OnItemRemovedBP;
+	
+	UPROPERTY() FGameCurrency CurrencyData;
+	UPROPERTY() TMap<FGuid, FInventorySlotData> ItemSlots;
+	
+	void ValidateInventory(const bool bForceUpdate = false);
+	virtual void OnInventoryUpdate() {}
 };
