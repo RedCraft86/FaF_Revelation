223787e764cfd89450b56b3099c792746b4e054a
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-02-16 11:05:30 -0500

    compression helpers - oodle

---
+++ Plugins/ToroUtilities/Source/ToroCore/Public/Helpers/CompressionHelpers.h
@@ -0,0 +1,20 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Compression/OodleDataCompressionUtil.h"
+
+namespace CompressionHelpers
+{
+	static bool OodleCompress(const TArray<uint8>& InData, TArray<uint8>& OutData)
+	{
+		return FOodleCompressedArray::CompressTArray(OutData, InData,
+			FOodleDataCompression::ECompressor::Kraken,
+			FOodleDataCompression::ECompressionLevel::SuperFast);
+	}
+	
+	static bool OodleDecompress(const TArray<uint8>& InData, TArray<uint8>& OutData)
+	{
+		return FOodleCompressedArray::DecompressToTArray(OutData, InData);
+	}
+}
