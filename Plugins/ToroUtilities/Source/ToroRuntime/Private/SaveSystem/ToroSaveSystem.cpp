bfb5f7d1808524768d9379038c43163447b917cf
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-02-16 11:17:13 -0500

    generic functionality

--- Plugins/ToroUtilities/Source/ToroRuntime/Private/SaveSystem/ToroSaveSystem.cpp
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/SaveSystem/ToroSaveSystem.cpp
@@ -2,9 +2,19 @@
 
 #include "SaveSystem/ToroSaveSystem.h"
 #include "ToroRuntimeSettings.h"
+#include "ToroRuntime.h"
 
-UToroSaveSystem::UToroSaveSystem()
+UToroSaveObject* UToroSaveSystem::GetSaveObject(const FGameplayTag InTag)
 {
+	if (!InTag.IsValid() || InTag == Tag_Saves) return nullptr;
+	if (TObjectPtr<UToroSaveObject>* Ptr = SaveObjects.Find(InTag))
+	{
+		if (*Ptr) return *Ptr;
+		SaveObjects.Remove(InTag);
+	}
+
+	UE_LOG(LogToroRuntime, Error, TEXT("Failed to find save object with tag: %s"), *InTag.ToString());
+	return nullptr;
 }
 
 void UToroSaveSystem::Initialize(FSubsystemCollectionBase& Collection)
@@ -18,4 +28,13 @@ void UToroSaveSystem::Initialize(FSubsystemCollectionBase& Collection)
 			SaveObjects.Add(Info.Key, Obj);
 		}
 	}
+}
+
+void UToroSaveSystem::Deinitialize()
+{
+	for (const TPair<FGameplayTag, TObjectPtr<UToroSaveObject>>& Obj : SaveObjects)
+	{
+		if (Obj.Value) Obj.Value->SaveObject(nullptr);
+	}
+	Super::Deinitialize();
 }
--- Plugins/ToroUtilities/Source/ToroRuntime/Public/SaveSystem/ToroSaveSystem.h
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/SaveSystem/ToroSaveSystem.h
@@ -7,18 +7,27 @@
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "ToroSaveSystem.generated.h"
 
-UCLASS()
+UCLASS(BlueprintType)
 class TORORUNTIME_API UToroSaveSystem final : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
 
 public:
 
-	UToroSaveSystem();
+	UToroSaveSystem() {}
+
+	UFUNCTION(BlueprintPure, Category = SaveSystem)
+		UToroSaveObject* GetSaveObject(UPARAM(meta = (Categories = "Saves")) const FGameplayTag InTag);
+
+	template <typename T = UToroSaveObject>
+	T* GetSaveObject(const FGameplayTag& InTag)
+	{
+		return Cast<T>(GetSaveObject(InTag));
+	}
 
 private:
 	
 	UPROPERTY(Transient) TMap<FGameplayTag, TObjectPtr<UToroSaveObject>> SaveObjects;
-
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
 };
