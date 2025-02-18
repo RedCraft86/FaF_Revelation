5aa8888f32892c98158ceb8a285956f0c975b763
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-02-18 16:03:07 -0500

    narrative component

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/Framework/ToroNarrativeComponent.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Framework/ToroNarrativeComponent.h"
+#include "Framework/ToroGameMode.h"
+
+UToroNarrativeComponent* UToroNarrativeComponent::Get(const UObject* WorldContext)
+{
+	if (const AToroGameMode* GameMode = AToroGameMode::Get(WorldContext))
+	{
+		return GameMode->Narrative;
+	}
+	
+	return nullptr;
+}
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Framework/ToroNarrativeComponent.h
@@ -0,0 +1,17 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "NarrativeComponent.h"
+#include "ToroNarrativeComponent.generated.h"
+
+UCLASS(NotBlueprintable, ClassGroup = (Game), meta = (BlueprintSpawnableComponent))
+class TORORUNTIME_API UToroNarrativeComponent : public UNarrativeComponent
+{
+	GENERATED_BODY()
+
+public:
+	
+	UToroNarrativeComponent() {}
+	static UToroNarrativeComponent* Get(const UObject* WorldContext);
+};
