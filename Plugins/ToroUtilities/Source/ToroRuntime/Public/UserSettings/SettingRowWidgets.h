15193c867e7f82ee75a41d94959ac16384825b63
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-02-20 16:40:13 -0500

    Setting row bases

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/UserSettings/SettingRowWidgets.h
@@ -0,0 +1,56 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "SettingRowWidgets.generated.h"
+
+UCLASS(Abstract, NotBlueprintable)
+class TORORUNTIME_API USettingRowBase : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	USettingRowBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {}
+};
+
+UCLASS(Abstract)
+class TORORUNTIME_API UToggleSettingBase : public USettingRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UToggleSettingBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {}
+};
+
+UCLASS(Abstract)
+class TORORUNTIME_API USliderSettingBase : public USettingRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	USliderSettingBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {}
+};
+
+UCLASS(Abstract)
+class TORORUNTIME_API USwitcherSettingBase : public USettingRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	USwitcherSettingBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {}
+};
+
+UCLASS(Abstract)
+class TORORUNTIME_API UComboSettingBase : public USettingRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UComboSettingBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {}
+};
