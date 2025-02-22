05f6beb4a94f81b404d5de3a0df3acddecf90563
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-02-21 20:17:12 -0500

    rename

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/UserWidgets/GameWidgetBase.h
@@ -0,0 +1,19 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "UserWidgets/ToroWidgetBase.h"
+#include "GameWidgetBase.generated.h"
+
+UCLASS(Abstract)
+class TORORUNTIME_API UGameWidgetBase : public UToroWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UGameWidgetBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+	{
+		ZOrder = 55;
+	}
+};
