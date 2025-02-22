efa4806fb705b39be5303f782d769522894a1083
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-02-21 19:39:15 -0500

    guide widget base

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/GuidePages/GuideBookWidgetBase.h
@@ -0,0 +1,19 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "UserWidgets/ToroWidgetBase.h"
+#include "GuideBookWidgetBase.generated.h"
+
+UCLASS(Abstract)
+class TORORUNTIME_API UGuideBookWidgetBase : public UToroWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UGuideBookWidgetBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+	{
+		ZOrder = 59;
+	}
+};
