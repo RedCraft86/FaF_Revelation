46a03b45e0b0250c4dd4e98f68f6e42148076a47
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-02-24 14:35:40 -0500

    error dialogues

--- Plugins/ToroUtilities/Source/ToroRuntime/Private/SaveSystem/ToroSaveObject.cpp
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/SaveSystem/ToroSaveObject.cpp
@@ -7,14 +7,15 @@
 #include "Serialization/BufferArchive.h"
 #include "ToroRuntimeSettings.h"
 #include "CompressionHelpers.h"
+#include "WindowsHelpers.h"
 #include "ToroRuntime.h"
 
 UE_DEFINE_GAMEPLAY_TAG(Tag_Saves, "Saves");
 
-FString GetDemoStr()
+FName GetDemoName()
 {
-	const FName Ver = UToroRuntimeSettings::Get()->DemoVersion; const FString Str = Ver.ToString();
-	return Ver.IsNone() || Str == TEXT(" ") || Str.TrimStartAndEnd().IsEmpty() ? TEXT("") : Str;
+	const FName Ver = UToroRuntimeSettings::Get()->DemoVersion;
+	return Ver.IsNone() || Ver.ToString() == TEXT(" ") || Ver.ToString().TrimStartAndEnd().IsEmpty() ? NAME_None : Ver;
 }
 
 UToroSaveObject* UToroSaveObject::Create(UToroSaveManager* Owner, const TSubclassOf<UToroSaveObject>& Class, const FGameplayTag& Tag)
@@ -26,13 +27,13 @@ UToroSaveObject* UToroSaveObject::Create(UToroSaveManager* Owner, const TSubclas
 		if (UToroSaveObject* NewObj = NewObject<UToroSaveObject>(Owner, Class, *Name))
 		{
 			NewObj->SaveTag = Tag;
-			NewObj->SavePath = FPaths::MakeValidFileName(FString::Printf(TEXT("%sSaveGames/%s/%s.tsave"),
-				*FPaths::ProjectSavedDir(), *GetDemoStr(), *Name).Replace(TEXT("//"), TEXT("/")));
 			NewObj->SaveManager = Owner;
+			NewObj->SavePath = FString::Printf(TEXT("%sSaveGames/%s/%s.tsave"), *FPaths::ProjectSavedDir(),
+				*GetDemoName().ToString(), *Name).Replace(TEXT("None/"), TEXT(""));
 			
 			NewObj->OnCreation();
 			NewObj->LoadObject(nullptr);
-			UE_LOG(LogTemp, Warning, TEXT("Created Save Slot For: %s"), *NewObj->SavePath);
+			UE_LOG(LogTemp, Warning, TEXT("Created Save Object For: %s"), *NewObj->SavePath);
 			return NewObj;
 		}
 	}
@@ -60,18 +61,24 @@ void UToroSaveObject::SaveObject(const TFunction<void(const ESaveGameError)>& Ca
 			{
 				LastError = FFileHelper::SaveArrayToFile(CompressedData, *SavePath)
 					? ESaveGameError::None : ESaveGameError::FileWrite;
-	#if WITH_EDITOR
 				if (LastError == ESaveGameError::FileWrite)
 				{
+					WindowsHelpers::OpenDialogue(TEXT("Save System Error"), FString::Printf(
+						TEXT("Failed to write file\n%s"), *SavePath),
+						EWindowsDialogueType::Ok, EWindowsDialogueIcon::Error);
+					
 					UE_LOG(LogToroRuntime, Error, TEXT("Failed write save file! [Path: %s] [Size: %d]"),
 						*SavePath, CompressedData.Num());
 				}
-	#endif
 				if (Callback) Callback(LastError);
 			});
 		}
 		else
 		{
+			WindowsHelpers::OpenDialogue(TEXT("Save System Error"), FString::Printf(
+				TEXT("Failed to compress data for save file\n%s"), *SavePath),
+				EWindowsDialogueType::Ok, EWindowsDialogueIcon::Error);
+			
 			UE_LOG(LogToroRuntime, Error, TEXT("Failed to compress save data! [Size: %d]"), ToBinary.Num());
 			LastError = ESaveGameError::Compress;
 			if (Callback) Callback(LastError);
@@ -124,6 +131,10 @@ void UToroSaveObject::LoadObject(const TFunction<void(const ESaveGameError)>& Ca
 					}
 					else
 					{
+						WindowsHelpers::OpenDialogue(TEXT("Save System Error"), FString::Printf(
+							TEXT("Failed to decompress data from save file\n%s"), *SavePath),
+							EWindowsDialogueType::Ok, EWindowsDialogueIcon::Error);
+						
 						UE_LOG(LogToroRuntime, Error, TEXT("Failed to decompress save data! [Size: %d]"), CompressedData.Num());
 						LastError = ESaveGameError::Decompress;
 					}
@@ -140,6 +151,10 @@ void UToroSaveObject::LoadObject(const TFunction<void(const ESaveGameError)>& Ca
 		}
 		else
 		{
+			WindowsHelpers::OpenDialogue(TEXT("Save System Error"), FString::Printf(
+				TEXT("Failed to read save file\n%s"), *SavePath),
+				EWindowsDialogueType::Ok, EWindowsDialogueIcon::Error);
+			
 			UE_LOG(LogToroRuntime, Error, TEXT("Failed to read save file! [Path: %s]"), *SavePath);
 			LastError = ESaveGameError::FileRead;
 			if (Callback) Callback(LastError);
