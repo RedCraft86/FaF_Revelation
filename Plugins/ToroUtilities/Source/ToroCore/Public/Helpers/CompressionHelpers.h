1a7d56b178cbd0fee49609f71e1ab8353e6f3606
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-02-24 13:59:42 -0500

    early exit for empty arrays

--- Plugins/ToroUtilities/Source/ToroCore/Public/Helpers/CompressionHelpers.h
+++ Plugins/ToroUtilities/Source/ToroCore/Public/Helpers/CompressionHelpers.h
@@ -8,13 +8,15 @@ namespace CompressionHelpers
 {
 	static bool OodleCompress(const TArray<uint8>& InData, TArray<uint8>& OutData)
 	{
+		if (InData.IsEmpty()) return true;
 		return FOodleCompressedArray::CompressTArray(OutData, InData,
 			FOodleDataCompression::ECompressor::Kraken,
 			FOodleDataCompression::ECompressionLevel::SuperFast);
 	}
-	
+
 	static bool OodleDecompress(const TArray<uint8>& InData, TArray<uint8>& OutData)
 	{
+		if (InData.IsEmpty()) return true;
 		return FOodleCompressedArray::DecompressToTArray(OutData, InData);
 	}
 }
