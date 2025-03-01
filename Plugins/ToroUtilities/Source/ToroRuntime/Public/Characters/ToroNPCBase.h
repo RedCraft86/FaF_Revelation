cd5cf90f7668d48baef2425143f51cf3f3710184
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-03-01 15:51:43 -0500

    enemy base

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Characters/ToroNPCBase.h
@@ -0,0 +1,45 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "ToroCharacterBase.h"
+#include "ToroNPCBase.generated.h"
+
+UENUM(BlueprintType)
+enum class EToroEnemyState : uint8
+{
+	None,
+	Roam,
+	Seen,
+	Chase,
+	Search
+};
+
+UCLASS(Abstract)
+class TORORUNTIME_API AToroNPCBase : public AToroCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	
+	AToroNPCBase();
+
+	UFUNCTION(BlueprintPure, Category = Game, DisplayName = "Set NPC State")
+		void SetNPCState(const EToroEnemyState InState);
+
+	UFUNCTION(BlueprintPure, Category = Game, DisplayName = "Get NPC State")
+		EToroEnemyState GetNPCState() const { return bEnemy ? EnemyState : EToroEnemyState::None; }
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = CharacterSettings, DisplayName = "Is Enemy")
+		bool bEnemy;
+
+	UPROPERTY(EditAnywhere, Category = CharacterSettings)
+		EToroEnemyState EnemyState;
+
+	UPROPERTY(Transient)
+		TObjectPtr<class AToroPlayerBase> PlayerChar;
+	
+	virtual void BeginPlay() override;
+};
