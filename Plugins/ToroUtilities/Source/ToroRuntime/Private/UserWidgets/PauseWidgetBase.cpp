f361d6190f9feb3a3266e199c36188aa087aaaa9
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-03-01 20:57:55 -0500

    pause widget

--- Plugins/ToroUtilities/Source/ToroRuntime/Private/UserWidgets/PauseWidgetBase.cpp
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/UserWidgets/PauseWidgetBase.cpp
@@ -1,3 +1,113 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
 #include "UserWidgets/PauseWidgetBase.h"
+#include "UserSettings/SettingsWidgetBase.h"
+#include "SaveSystem/ToroSaveManager.h"
+#include "SaveSystem/BaseSaveObjects.h"
+#include "Characters/ToroPlayerBase.h"
+#include "GeneralProjectSettings.h"
+#include "Components/TextBlock.h"
+#include "EnhancedCodeFlow.h"
+
+UPauseWidgetBase::UPauseWidgetBase(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer), SaveTag(Tag_GameSave)
+{
+	ZOrder = 58;
+	bAutoActivate = false;
+}
+
+void UPauseWidgetBase::OnResumeClicked()
+{
+	PlayerChar->GetPlayerController()->SetPauseState(false);
+	SetVisibility(ESlateVisibility::HitTestInvisible);
+}
+
+void UPauseWidgetBase::OnSettingsClicked()
+{
+	PlayerChar->GetPlayerController()->SetGameInputMode(EGameInputMode::GameAndUI, true,
+		EMouseLockMode::LockAlways, false, SettingsWidget);
+	SetHidden(true);
+}
+
+void UPauseWidgetBase::OnCheckpointClicked()
+{
+	if (UToroSaveManager* SaveManager = UToroSaveManager::Get(this))
+	{
+		if (UGameSaveObjectBase* SaveObject = SaveManager->GetSaveObject<UGameSaveObjectBase>(SaveTag))
+		{
+			SaveObject->SaveObject(nullptr);
+		}
+	}
+
+	SetHidden(true);
+	SetVisibility(ESlateVisibility::HitTestInvisible);
+	FFlow::Delay(this, 2.0f, [this]()
+	{
+		UGameplayStatics::OpenLevel(this, *UGameplayStatics::GetCurrentLevelName(this));
+	});
+}
+
+void UPauseWidgetBase::OnMainMenuClicked()
+{
+	if (UToroSaveManager* SaveManager = UToroSaveManager::Get(this))
+	{
+		if (UGameSaveObjectBase* SaveObject = SaveManager->GetSaveObject<UGameSaveObjectBase>(SaveTag))
+		{
+			SaveObject->SaveObject(nullptr);
+		}
+	}
+
+	SetHidden(true);
+	SetVisibility(ESlateVisibility::HitTestInvisible);
+	FFlow::Delay(this, 2.0f, [this]()
+	{
+		UGameplayStatics::OpenLevelBySoftObjectPtr(this, MainMenuLevel);
+	});
+}
+
+void UPauseWidgetBase::OnExitSettings()
+{
+	SetHidden(false);
+	PlayerChar->GetPlayerController()->SetGameInputMode(EGameInputMode::GameAndUI, true,
+		EMouseLockMode::LockAlways, false, this);
+}
+
+void UPauseWidgetBase::InitWidget()
+{
+	Super::InitWidget();
+	SetVisibility(ESlateVisibility::Visible);
+	PlayerChar->GetPlayerController()->SetGameInputMode(EGameInputMode::GameAndUI, true,
+		EMouseLockMode::LockAlways, false, this);
+}
+
+void UPauseWidgetBase::NativeConstruct()
+{
+	Super::NativeConstruct();
+	SetVisibility(ESlateVisibility::Collapsed);
+	PlayerChar = AToroPlayerBase::Get(this);
+	if (const UGeneralProjectSettings* ProjectSettings = GetDefault<UGeneralProjectSettings>())
+	{
+		GameVersionText->SetText(FText::FromString(FString::Printf(TEXT("Game Version: %s - %s Build"),
+			*ProjectSettings->ProjectVersion, LexToString(FApp::GetBuildConfiguration()))));
+	}
+		
+	ResumeButton->OnClicked.AddUObject(this, &UPauseWidgetBase::OnResumeClicked);
+	SettingsButton->OnClicked.AddUObject(this, &UPauseWidgetBase::OnSettingsClicked);
+	CheckpointButton->OnClicked.AddUObject(this, &UPauseWidgetBase::OnCheckpointClicked);
+	MainMenuButton->OnClicked.AddUObject(this, &UPauseWidgetBase::OnMainMenuClicked);
+
+	SettingsWidget = CreateNew<USettingsWidgetBase>(PlayerChar->GetPlayerController(), SettingsWidgetClass);
+	SettingsWidget->OnExit.AddUObject(this, &UPauseWidgetBase::OnExitSettings);
+}
+
+void UPauseWidgetBase::InternalProcessActivation()
+{
+	SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+	Super::InternalProcessActivation();
+}
+
+void UPauseWidgetBase::InternalProcessDeactivation()
+{
+	Super::InternalProcessDeactivation();
+	PlayerChar->GetPlayerController()->SetGameInputMode(EGameInputMode::GameOnly);
+}
--- Plugins/ToroUtilities/Source/ToroRuntime/Public/UserWidgets/PauseWidgetBase.h
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/UserWidgets/PauseWidgetBase.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "ToroWidgetBase.h"
+#include "ToroAnimatedButtonBase.h"
 #include "PauseWidgetBase.generated.h"
 
 UCLASS(Abstract)
@@ -12,8 +13,48 @@ class TORORUNTIME_API UPauseWidgetBase : public UToroWidget
 
 public:
 
-	UPauseWidgetBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-	{
-		ZOrder = 58;
-	}
+	UPauseWidgetBase(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(Transient, BlueprintReadOnly, meta = (BindWidget))
+		TObjectPtr<UToroAnimatedButtonBase> ResumeButton;
+
+	UPROPERTY(Transient, BlueprintReadOnly, meta = (BindWidget))
+		TObjectPtr<UToroAnimatedButtonBase> SettingsButton;
+
+	UPROPERTY(Transient, BlueprintReadOnly, meta = (BindWidget))
+		TObjectPtr<UToroAnimatedButtonBase> CheckpointButton;
+
+	UPROPERTY(Transient, BlueprintReadOnly, meta = (BindWidget))
+		TObjectPtr<UToroAnimatedButtonBase> MainMenuButton;
+
+	UPROPERTY(Transient, BlueprintReadOnly, meta = (BindWidget))
+		TObjectPtr<class UTextBlock> GameVersionText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> ScreenFadeAnim;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Settings)
+		TSoftObjectPtr<UWorld> MainMenuLevel;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Settings)
+		TSubclassOf<class USettingsWidgetBase> SettingsWidgetClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Settings, meta = (Categories = "Saves"))
+		FGameplayTag SaveTag;
+
+protected:
+
+	UPROPERTY(Transient) TObjectPtr<class AToroPlayerBase> PlayerChar;
+	UPROPERTY(Transient) TObjectPtr<USettingsWidgetBase> SettingsWidget;
+
+	void OnResumeClicked();
+	void OnSettingsClicked();
+	void OnCheckpointClicked();
+	void OnMainMenuClicked();
+	void OnExitSettings();
+
+	virtual void InitWidget() override;
+	virtual void NativeConstruct() override;
+	virtual void InternalProcessActivation() override;
+	virtual void InternalProcessDeactivation() override;
 };
