8bf1d70ccd1a6b5e43b90ec9b078614cdf8fd4e0
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-03-01 20:56:34 -0500

    animated buttons

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/UserWidgets/ToroAnimatedButtonBase.cpp
@@ -0,0 +1,29 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "UserWidgets/ToroAnimatedButtonBase.h"
+#include "Components/Button.h"
+
+void UToroAnimatedButtonBase::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	Button->OnClicked.AddDynamic(this, &UToroAnimatedButtonBase::OnButtonClicked);
+	Button->OnHovered.AddDynamic(this, &UToroAnimatedButtonBase::OnButtonHovered);
+	Button->OnUnhovered.AddDynamic(this, &UToroAnimatedButtonBase::OnButtonUnhovered);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void UToroAnimatedButtonBase::OnButtonClicked()
+{
+	OnClicked.Broadcast();
+	OnClickedBP.Broadcast();
+}
+
+void UToroAnimatedButtonBase::OnButtonHovered()
+{
+	PlayAnimationForward(ButtonAnim);
+}
+
+void UToroAnimatedButtonBase::OnButtonUnhovered()
+{
+	PlayAnimationReverse(ButtonAnim);
+}
