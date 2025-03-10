294eea0ef4c489de372f085244cd9d231a6dd087
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-03-10 16:24:32 -0400

    main menu widget

--- Source/FaF_Revelation/Widgets/MainMenu.cpp
+++ Source/FaF_Revelation/Widgets/MainMenu.cpp
@@ -1,4 +1,75 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
-
 #include "MainMenu.h"
+#include "EnhancedCodeFlow.h"
+#include "Components/TextBlock.h"
+#include "GeneralProjectSettings.h"
+#include "Characters/ToroPlayerBase.h"
+#include "Framework/ToroPlayerController.h"
+#include "UserSettings/SettingsWidgetBase.h"
+
+UMainMenuBase::UMainMenuBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	bGameplayOnly = false;
+}
+
+void UMainMenuBase::OnPlayClicked()
+{
+	DeactivateWidget();
+	PlayerChar->FadeToBlack(1.0f, true);
+	FFlow::Delay(this, 1.0f, [this]
+	{	
+		UGameplayStatics::OpenLevelBySoftObjectPtr(this, GameplayLevel);
+	});
+}
+
+void UMainMenuBase::OnSettingsClicked()
+{
+	SettingsWidget->ActivateWidget();
+	PlayerChar->GetPlayerController()->SetGameInputMode(EGameInputMode::GameAndUI, true,
+		EMouseLockMode::LockAlways, false, SettingsWidget);
+	SetHidden(true);
+}
+
+void UMainMenuBase::OnExtrasClicked()
+{
+	DeactivateWidget();
+	PlayerChar->FadeToBlack(1.0f, true);
+	FFlow::Delay(this, 1.0f, [this]
+	{	
+		UGameplayStatics::OpenLevelBySoftObjectPtr(this, ExtrasLevel);
+	});
+}
+
+void UMainMenuBase::OnQuitClicked()
+{
+	DeactivateWidget();
+	PlayerChar->FadeToBlack(1.0f, true);
+	FFlow::Delay(this, 1.0f, [this]
+	{	
+		UKismetSystemLibrary::QuitGame(this, nullptr, EQuitPreference::Quit, true);
+	});
+}
+
+void UMainMenuBase::InitWidget()
+{
+	Super::InitWidget();
+	PlayerChar = AToroPlayerBase::Get(this);
+	Return_Implementation();
+
+	SettingsWidget = CreateNew<USettingsWidgetBase>(PlayerChar->GetPlayerController(), SettingsWidgetClass);
+	SettingsWidget->ParentUI = this;
+
+	if (const UGeneralProjectSettings* ProjectSettings = GetDefault<UGeneralProjectSettings>())
+	{
+		GameVersionText->SetText(FText::FromString(FString::Printf(TEXT("Game Version: %s | %s Build"),
+			*ProjectSettings->ProjectVersion, LexToString(FApp::GetBuildConfiguration()))));
+	}
+}
+
+void UMainMenuBase::Return_Implementation()
+{
+	SetHidden(false);
+	PlayerChar->GetPlayerController()->SetGameInputMode(EGameInputMode::GameAndUI, true,
+		EMouseLockMode::LockAlways, false, this);
+}
--- Source/FaF_Revelation/Widgets/MainMenu.h
+++ Source/FaF_Revelation/Widgets/MainMenu.h
@@ -2,11 +2,56 @@
 
 #pragma once
 
+#include "GeneralInterface.h"
 #include "UserWidgets/ToroWidgetBase.h"
 #include "MainMenu.generated.h"
 
+class USettingsWidgetBase;
+class UButton;
+
 UCLASS(Abstract)
-class FAF_REVELATION_API UMainMenu final : public UToroWidget
+class FAF_REVELATION_API UMainMenuBase final : public UToroWidget, public IToroGeneralInterface
 {
 	GENERATED_BODY()
+
+public:
+
+	UMainMenuBase(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> PlayButton;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> SettingsButton;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> ExtrasButton;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> QuitButton;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<class UTextBlock> GameVersionText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Settings)
+		TSoftObjectPtr<UWorld> GameplayLevel;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Settings)
+		TSoftObjectPtr<UWorld> ExtrasLevel;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Settings)
+		TSubclassOf<USettingsWidgetBase> SettingsWidgetClass;
+
+protected:
+
+	UPROPERTY(Transient) TObjectPtr<AToroPlayerBase> PlayerChar;
+	UPROPERTY(Transient) TObjectPtr<USettingsWidgetBase> SettingsWidget;
+
+	void OnPlayClicked();
+	void OnSettingsClicked();
+	void OnExtrasClicked();
+	void OnQuitClicked();
+
+	virtual void InitWidget() override;
+	virtual void Return_Implementation() override;
 };
