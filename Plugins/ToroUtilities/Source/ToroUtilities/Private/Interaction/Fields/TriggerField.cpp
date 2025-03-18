607a550afae24a9d7f8d0e16c018000d22909b99
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-03-18 15:58:56 -0400

    interaction

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/Interaction/Fields/TriggerField.cpp
@@ -0,0 +1,25 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Interaction/Fields/TriggerField.h"
+#include "Framework/ToroPlayerCharacter.h"
+#include "Components/BrushComponent.h"
+
+ATriggerField::ATriggerField() : bSingleUse(true)
+{
+	WorldEventComponent = CreateDefaultSubobject<UWorldActionComponent>("WorldEvents");
+	GetBrushComponent()->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void ATriggerField::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	if (!bEnabled) return;
+	if (AToroPlayerCharacter* Player = Cast<AToroPlayerCharacter>(OtherActor))
+	{
+		OnTriggered.Broadcast(Player);
+		OnTriggeredEvent.Broadcast(Player);
+		WorldEventComponent->RunEvents();
+		if (bSingleUse) SetEnabled(false);
+	}
+}
---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/Interaction/InteractionComponent.cpp
@@ -0,0 +1,107 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Interaction/InteractionComponent.h"
+#include "Framework/ToroPlayerCharacter.h"
+
+UInteractionComponent::UInteractionComponent() : bEnabled(false), HoldTime(0.0f), bInteracting(false)
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.TickInterval = 0.05f;
+}
+
+void UInteractionComponent::SetEnabled(const bool bInEnabled)
+{
+	if (bEnabled != bInEnabled)
+	{
+		bEnabled = bInEnabled;
+		SetComponentTickEnabled(bInEnabled);
+		OnUpdateInteraction.Broadcast(bInEnabled, {});
+		if (!bInEnabled ) SetInteracting(false);
+	}
+}
+
+void UInteractionComponent::SetInteracting(const bool bInInteracting)
+{
+	if (bInteracting != bInInteracting)
+	{
+		bInteracting = bInInteracting;
+		if (!bInteracting)
+		{
+			CleanupInteraction();
+		}
+	}
+}
+
+void UInteractionComponent::CleanupInteraction()
+{
+	if (InteractCache.Target)
+	{
+		if (InteractCache.bTriggered) IInteractionInterface::EndInteract(InteractCache.Target, Player);
+		InteractCache.Reset();
+	}
+}
+
+void UInteractionComponent::HandleInteractionTick(float DeltaTime, const FHitResult& HitResult, const FInteractionInfo& InteractResult)
+{
+	if (InteractCache.Target != HitResult.GetActor())
+	{
+		if (InteractCache.bTriggered) IInteractionInterface::EndInteract(InteractCache.Target, Player);
+		const bool bHeldInteraction = InteractResult.Interaction == EInteractableType::Held;
+		HoldTime = bHeldInteraction ? InteractResult.HoldingTime : 0.0f;
+		InteractCache.Target = HitResult.GetActor();
+		InteractCache.Info = InteractResult;
+		if (bHeldInteraction)
+		{
+			InteractCache.bTriggered = false;
+			InteractCache.Progress = 0.0f;
+		}
+		else
+		{
+			InteractCache.Progress = 1.0f;
+			InteractCache.bTriggered = true;
+			IInteractionInterface::BeginInteract(InteractCache.Target, Player, HitResult);
+		}
+	}
+
+	if (HoldTime >= 0.0f)
+	{
+		HoldTime -= DeltaTime;
+		InteractCache.Progress = FMath::GetMappedRangeValueClamped(
+			FVector2D{0.0f, InteractCache.Info.HoldingTime},
+			FVector2D{1.0f, 0.0f}, HoldTime);
+
+		if (HoldTime < 0.0f)
+		{
+			InteractCache.Progress = 1.0f;
+			InteractCache.bTriggered = true;
+			IInteractionInterface::BeginInteract(InteractCache.Target, Player, HitResult);
+		}
+	}
+}
+
+void UInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!InteractionLogic.IsBound()) return;
+	const FHitResult HitResult = InteractionLogic.Execute();
+	if (!HitResult.IsValidBlockingHit()) return;
+
+	FInteractionInfo InteractInfo; 
+	if (bInteracting && IInteractionInterface::GetInteractionInfo(HitResult.GetActor(), HitResult, InteractInfo))
+	{
+		HandleInteractionTick(DeltaTime, HitResult, InteractInfo);
+	}
+	else
+	{
+		CleanupInteraction();
+	}
+
+	OnUpdateInteraction.Broadcast(true, InteractCache);
+}
+
+void UInteractionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	Player = AToroPlayerCharacter::Get(this);
+}
