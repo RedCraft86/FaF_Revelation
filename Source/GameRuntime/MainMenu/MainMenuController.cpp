5e6e1304ffe98f071559bf7721c05f36e6d4f041
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-03-22 19:40:20 -0400

    main menu controller

---
+++ Source/GameRuntime/MainMenu/MainMenuController.cpp
@@ -0,0 +1,82 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "MainMenuController.h"
+#include "SaveSystem/ToroSaveManager.h"
+#include "Framework/ToroPlayerCharacter.h"
+#include "Framework/ToroWidgetManager.h"
+#include "Framework/ToroGameInstance.h"
+#include "Algo/RandomShuffle.h"
+#include "GameSaveObjects.h"
+#include "LoggingHelpers.h"
+
+UE_DEFINE_GAMEPLAY_TAG_BASE(MainMenu)
+
+AMainMenuController::AMainMenuController()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+#if WITH_EDITORONLY_DATA
+	Root->bVisualizeComponent = true;
+#endif
+}
+
+void AMainMenuController::StartLoad() const
+{
+	TArray<TSoftObjectPtr<UWorld>> Scenes;
+	MenuSets.GenerateValueArray(Scenes);
+	Algo::RandomShuffle(Scenes);
+	if (!Scenes.IsEmpty())
+	{
+		FLatentActionInfo Info;
+		Info.ExecutionFunction = NAME_None;
+		Info.CallbackTarget = nullptr;
+		Info.Linkage = 0;
+		Info.UUID = 0;
+
+		UGameplayStatics::LoadStreamLevelBySoftObjectPtr(this, Scenes[0],
+			true, false, Info);
+	}
+	if (const AToroPlayerCharacter* Player = AToroPlayerCharacter::Get(this))
+	{
+		Player->FadeFromBlack(2.0f, false);
+	}
+	if (UMainMenuWidgetBase* Widget = CreateWidget<UMainMenuWidgetBase>(GetWorld(), MainMenuWidget))
+	{
+		Widget->AddToViewport(50);
+	}
+}
+
+void AMainMenuController::BeginPlay()
+{
+	Super::BeginPlay();
+	const UToroGameInstance* GI = UToroGameInstance::Get(this);
+	if (GI && GI->FinishedFirstLoads())
+	{
+		if (AToroPlayerCharacter* Player = AToroPlayerCharacter::Get(this))
+		{
+			Player->AddLockFlag(GAMEPLAY_TAG_CHILD(MainMenu, PlayerLock));
+			Player->Teleport(FVector::ZeroVector, FRotator::ZeroRotator);
+		}
+
+		if (StartupSequence)
+		{
+			StartupSequence->Play();
+			StartupSequence->OnSequenceFinished().BindUObject(this, &ThisClass::StartLoad);
+		}
+		else StartLoad();
+	}
+
+	if (const UGlobalSaveObject* Save = ToroSave::GetObject<UGlobalSaveObject>(this, GAMEPLAY_TAG_CHILD(Global, Save)))
+	{
+		for (auto It = MenuSets.CreateIterator(); It; ++It)
+		{
+			if (!Save->Content.Contains(It->Key) && It->Key != Tag_MainMenu)
+			{
+				It.RemoveCurrent();
+			}
+		}
+	}
+}
---
+++ Source/GameRuntime/MainMenu/MainMenuController.h
@@ -0,0 +1,38 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "MainMenuWidget.h"
+#include "Misc/SequenceActor.h"
+#include "GameFramework/Actor.h"
+#include "GameplayTagContainer.h"
+#include "MainMenuController.generated.h"
+
+UE_DECLARE_GAMEPLAY_TAG_BASE(MainMenu);
+
+UCLASS(NotBlueprintable)
+class GAMERUNTIME_API AMainMenuController final : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	AMainMenuController();
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Subobjects)
+		TObjectPtr<USceneComponent> Root;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		TSubclassOf<UMainMenuWidgetBase> MainMenuWidget;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		TObjectPtr<AToroSequenceActor> StartupSequence;
+
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (ForceInlineRow, Categories = "MainMenu"))
+		TMap<FGameplayTag, TSoftObjectPtr<UWorld>> MenuSets;
+
+protected:
+	
+	void StartLoad() const;
+	virtual void BeginPlay() override;
+};
