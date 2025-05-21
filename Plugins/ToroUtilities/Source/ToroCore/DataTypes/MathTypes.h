f9ed38e7841e0aae122a8c2f23d32f287cb11c97
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-20 23:00:19 -0400

    math and currency

---
+++ Plugins/ToroUtilities/Source/ToroCore/DataTypes/MathTypes.h
@@ -0,0 +1,118 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MathTypes.generated.h"
+
+UENUM(BlueprintType)
+enum class EVectorDirection : uint8
+{
+	Forward,
+	Right,
+	Up
+};
+
+USTRUCT(BlueprintInternalUseOnly)
+struct TOROCORE_API FToroFloatModifier
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = FloatModifier)
+		float Base;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = FloatModifier)
+		TMap<FName, float> Modifiers;
+
+	FToroFloatModifier() : Base(0.0f) {}
+	FToroFloatModifier(const float InBase) : Base(InBase) {}
+	virtual ~FToroFloatModifier() = default;
+	
+	void ClampBase(const float Min, const float Max)
+	{
+		Base = FMath::Clamp(Base, Min, Max);
+	}
+	
+	void ClampModifiers(const float Min, const float Max)
+	{
+		for (auto It = Modifiers.CreateIterator(); It; ++It)
+		{
+			It->Value = FMath::Clamp(It->Value, Min, Max);
+		}
+	}
+
+	void AddMod(const FName Key, const float Value) { Modifiers.Add(Key, Value); }
+	void RemoveMod(const FName Key) { Modifiers.Remove(Key); }
+	virtual float Evaluate() const
+	{
+		return Base;
+	}
+};
+
+USTRUCT(BlueprintType, DisplayName = "Float Multiplier", meta = (ModifierType = "Multipliers"))
+struct TOROCORE_API FToroFloatMulti : public FToroFloatModifier
+{
+	GENERATED_BODY()
+
+	FToroFloatMulti() {}
+	FToroFloatMulti(const float InBase) : Super(InBase) {}
+	virtual float Evaluate() const override
+	{
+		float Value = Base;
+		for (const TPair<FName, float>& Pair : Modifiers)
+		{
+			if (!Pair.Key.IsNone()) Value *= Pair.Value;
+		}
+		return Value;
+	}
+};
+
+USTRUCT(BlueprintType, DisplayName = "Float Sum", meta = (ModifierType = "Additions"))
+struct TOROCORE_API FToroFloatSum : public FToroFloatModifier
+{
+	GENERATED_BODY()
+
+	FToroFloatSum() {}
+	FToroFloatSum(const float InBase) : Super(InBase) {}
+	virtual float Evaluate() const override
+	{
+		float Value = Base;
+		for (const TPair<FName, float>& Pair : Modifiers)
+		{
+			if (!Pair.Key.IsNone()) Value += Pair.Value;
+		}
+		return Value;
+	}
+};
+
+USTRUCT(BlueprintType, DisplayName = "Float Interpolation")
+struct TOROCORE_API FToroFloatInterp
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = FloatInterp)
+		float Current;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = FloatInterp)
+		float Target;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = FloatInterp)
+		float InterpSpeed;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = FloatInterp)
+		bool bConstant;
+
+	FToroFloatInterp() : Current(0.0f), Target(0.0f), InterpSpeed(5.0f), bConstant(false) {}
+	FToroFloatInterp(const float InCurrent, const float InSpeed = 5.0f, const bool bInConstant = false)
+		: Current(InCurrent), Target(InCurrent), InterpSpeed(InSpeed), bConstant(bInConstant)
+	{}
+
+	void SnapToTarget() { Current = Target; }
+	bool IsComplete() const { return FMath::IsNearlyEqual(Current, Target); }
+	void Tick(const float InDeltaTime)
+	{
+		if (IsComplete()) return;
+		Current = bConstant ? FMath::FInterpConstantTo(Current, Target, InDeltaTime, InterpSpeed)
+			: FMath::FInterpTo(Current, Target, InDeltaTime, InterpSpeed);
+	}
+};
---
+++ Plugins/ToroUtilities/Source/ToroCore/DataTypes/MiscTypes.h
@@ -0,0 +1,67 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "MiscTypes.generated.h"
+
+UENUM(BlueprintInternalUseOnly)
+enum class EToroFoundPins : uint8
+{
+	Found,
+	NotFound
+};
+
+UENUM(BlueprintInternalUseOnly)
+enum class EToroValidPins : uint8
+{
+	Valid,
+	NotValid
+};
+
+USTRUCT(BlueprintType)
+struct TOROCORE_API FGameCurrency
+{
+	GENERATED_BODY()
+
+	FGameCurrency() : CurrencyUnits(0) {}
+	FGameCurrency(const int64 InMoney) : CurrencyUnits(InMoney) {}
+	friend FArchive& operator<<(FArchive& Ar, FGameCurrency& Data)
+	{
+		Ar << Data.CurrencyUnits;
+		return Ar;
+	}
+
+	int64 GetAmount() const { return CurrencyUnits; }
+	void SetAmount(const int64 InMoney) { CurrencyUnits = FMath::Max(0, InMoney); }
+	bool HasEnoughMoney(const int64 InRequirement) const { return CurrencyUnits > FMath::Max(0, InRequirement); }
+	void RemoveMoney(const int64 InRemoval) { CurrencyUnits = FMath::Max(0, CurrencyUnits - FMath::Max(0, InRemoval)); }
+	void AddMoney(const int64 InAddition) { CurrencyUnits += FMath::Max(0, InAddition); }
+
+private:
+	
+	UPROPERTY(EditAnywhere, Category = GameCurrency, meta = (ClampMin = 0, UIMin = 0))
+		int64 CurrencyUnits;
+};
+
+UCLASS(DisplayName = "Currency Function Library")
+class TOROCORE_API UGameCurrencyLibrary final : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintPure, Category = GameCurrency)
+	static int64 GetMoney(const FGameCurrency& Target) { return Target.GetAmount(); }
+
+	UFUNCTION(BlueprintCallable, Category = GameCurrency)
+	static void SetMoney(UPARAM(ref) FGameCurrency& Target, const int64 InMoney) { return Target.SetAmount(InMoney); }
+
+	UFUNCTION(BlueprintPure, Category = GameCurrency)
+	static bool HasEnoughMoney(const FGameCurrency& Target, const int64 InRequirement) { return Target.HasEnoughMoney(InRequirement); }
+
+	UFUNCTION(BlueprintCallable, Category = GameCurrency)
+	static void RemoveMoney(UPARAM(ref) FGameCurrency& Target, const int64 InRemoval) { return Target.RemoveMoney(InRemoval); }
+
+	UFUNCTION(BlueprintCallable, Category = GameCurrency)
+	static void AddMoney(UPARAM(ref) FGameCurrency& Target, const int64 InAddition) { return Target.AddMoney(InAddition); }
+};
