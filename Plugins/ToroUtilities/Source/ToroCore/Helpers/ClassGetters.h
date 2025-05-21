e0f24024bdd0a43468a9d5734bf02d5ae760caf8
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-20 22:20:30 -0400

    helpers

---
+++ Plugins/ToroUtilities/Source/ToroCore/Helpers/ClassGetters.h
@@ -0,0 +1,36 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/LocalPlayer.h"
+#include "Engine/GameInstance.h"
+#include "Kismet/GameplayStatics.h"
+
+#define SETTING_GETTER(C) static const C* Get() { return GetDefault<C>(); }
+#define SETTING_GETTER_MUTABLE(C) static C* Get() { return GetMutableDefault<C>(); }
+
+#define ENGINE_SUBSYSTEM_GETTER(C) static C* Get() { return GEngine ? GEngine->GetEngineSubsystem<C>() : nullptr; }
+#define EDITOR_SUBSYSTEM_GETTER(C) static C* Get() { return GEditor ? GEditor->GetEditorSubsystem<C>() : nullptr; }
+
+#define LOCAL_PLAYER_SUBSYSTEM_GETTER(C) \
+	static C* Get(const UObject* WorldContext, const int PlayerIndex = 0) \
+	{ \
+		const APlayerController* Controller = UGameplayStatics::GetPlayerController(WorldContext, PlayerIndex); \
+		const ULocalPlayer* LocalPlayer = IsValid(Controller) ? Controller->GetLocalPlayer() : nullptr; \
+		return IsValid(LocalPlayer) ? LocalPlayer->GetSubsystem<C>() : nullptr; \
+	}
+
+#define GAME_INSTANCE_SUBSYSTEM_GETTER(C) \
+	static C* Get(const UObject* WorldContext) \
+	{ \
+		const UWorld* World = IsValid(WorldContext) ? GEngine->GetWorldFromContextObject(WorldContext, EGetWorldErrorMode::LogAndReturnNull) : nullptr; \
+		const UGameInstance* GI = IsValid(World) ? World->GetGameInstance() : nullptr; \
+		return IsValid(GI) ? GI->GetSubsystem<C>() : nullptr; \
+	}
+
+#define WORLD_SUBSYSTEM_GETTER(C) \
+	static C* Get(const UObject* WorldContext) \
+	{ \
+		const UWorld* World = IsValid(WorldContext) ? GEngine->GetWorldFromContextObject(WorldContext, EGetWorldErrorMode::LogAndReturnNull) : nullptr; \
+		return IsValid(World) ? World->GetSubsystem<C>() : nullptr; \
+	}
---
+++ Plugins/ToroUtilities/Source/ToroCore/Helpers/DataCompression.h
@@ -0,0 +1,24 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Compression/OodleDataCompressionUtil.h"
+
+namespace DataCompression
+{
+	static bool OodleCompress(const TArray<uint8>& InData, TArray<uint8>& OutData)
+	{
+		if (InData.IsEmpty()) return true;
+		return FOodleCompressedArray::CompressTArray(
+			OutData, InData,
+			FOodleDataCompression::ECompressor::Kraken,
+			FOodleDataCompression::ECompressionLevel::SuperFast
+		);
+	}
+
+	static bool OodleDecompress(const TArray<uint8>& InData, TArray<uint8>& OutData)
+	{
+		if (InData.IsEmpty()) return true;
+		return FOodleCompressedArray::DecompressToTArray(OutData, InData);
+	}
+}
