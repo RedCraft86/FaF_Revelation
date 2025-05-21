a3bd9373be911ebc6205cf8852bd3f63bbc63d27
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-21 16:46:07 -0400

    async action base

---
+++ Plugins/ToroUtilities/Source/ToroCore/Private/AsyncTasks/ToroAsyncActionBase.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "AsyncTasks/ToroAsyncActionBase.h"
+#include "Helpers/LoggingMacros.h"
+
+UWorld* UToroAsyncActionBase::GetWorld() const
+{
+	UWorld* World = Super::GetWorld();
+	if (!World) World = GEngine->GetWorldFromContextObject(GetWorldContext(), EGetWorldErrorMode::LogAndReturnNull);
+	if (!World) World = GEngine->GetCurrentPlayWorld();
+	return World;
+}
+
+bool UToroAsyncActionBase::IsRunningChecked()
+{
+	if (bTaskRunning)
+	{
+		UE_KLOG_WARNING(2.0f, TEXT("Async Action %s is already running"), *GetClass()->GetName())
+		return true;
+	}
+
+	bTaskRunning = true;
+	return false;
+}
