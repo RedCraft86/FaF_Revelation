6a4a360b6b1016d194ced25dd60cbf5da47e9b9c
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-21 12:18:03 -0400

    character base class

---
+++ Plugins/ToroUtilities/Source/ToroCore/Private/Framework/ToroCharacter.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Framework/ToroCharacter.h"
+
+DEFINE_GAMEPLAY_TAG_BASE(Character)
+
+AToroCharacter::AToroCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+FVector AToroCharacter::GetLookTarget() const
+{
+	return GetActorLocation() + FVector(0.0f, 0.0f, 60.0f);
+}
+
+void AToroCharacter::Teleport(const FVector& InLocation, const FRotator& InRotation)
+{
+	SetActorLocation(InLocation);
+	Controller->SetControlRotation(InRotation);
+}
---
+++ Plugins/ToroUtilities/Source/ToroCore/Public/Framework/ToroCharacter.h
@@ -0,0 +1,36 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "Helpers/GameplayTagHelpers.h"
+#include "ToroCharacter.generated.h"
+
+DECLARE_GAMEPLAY_TAG_BASE(Character)
+
+UCLASS(Abstract)
+class TOROCORE_API AToroCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	AToroCharacter();
+
+	UFUNCTION(BlueprintPure, Category = Game)
+		FGameplayTag GetCharacterID() const { return CharacterID; }
+
+	UFUNCTION(BlueprintPure, Category = Game)
+		virtual FVector GetLookTarget() const;
+
+	UFUNCTION(BlueprintPure, Category = Game)
+		virtual void GetViewPoint(FVector& Location, FVector& Forward, float& Angle) const {}
+
+	UFUNCTION(BlueprintCallable, Category = Game)
+		virtual void Teleport(const FVector& InLocation, const FRotator& InRotation);
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = General, meta = (DisplayPriority = -2, Categories = "Character"))
+		FGameplayTag CharacterID;
+};
