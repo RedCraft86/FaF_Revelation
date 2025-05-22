352330ad4bcb34605b4f9cb263fe317c7c1c99a8
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-22 15:24:03 -0400

    procedural mesh gen

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/Procedural/ToroMeshGenBase.h
@@ -0,0 +1,64 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "DataTypes/PrimitiveData.h"
+#include "Components/SplineMeshComponent.h"
+#include "ToroMeshGenBase.generated.h"
+
+UENUM(BlueprintType)
+enum class EGeneratorLoopMode : uint8
+{
+	Forward,
+	Reverse,
+	Random
+};
+
+UCLASS(Abstract)
+class TOROUTILITIES_API AToroMeshGenBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	AToroMeshGenBase();
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Subobjects)
+		TObjectPtr<USceneComponent> SceneRoot;
+	
+	template<typename T>
+	T* AddGenericComponent()
+	{
+		if (T* GeneratedComp = NewObject<T>(this))
+		{
+			GeneratedComp->CreationMethod = EComponentCreationMethod::UserConstructionScript;
+			GeneratedComp->OnComponentCreated();
+			GeneratedComp->RegisterComponent();
+			GeneratedComp->SetMobility(EComponentMobility::Movable);
+			GeneratedComp->AttachToComponent(GetRootComponent(),
+				FAttachmentTransformRules::KeepRelativeTransform);
+		
+			return GeneratedComp;
+		}
+
+		return nullptr;
+	}
+	
+protected:
+
+	UPROPERTY(EditAnywhere, Category = Settings, AdvancedDisplay)
+		bool bRealtimeConstruction;
+	
+	UFUNCTION(CallInEditor, Category = Tools)
+		virtual void Construct() { EventConstruct(); }
+	
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = "Construct")
+		void EventConstruct();
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+public:
+	
+	static TArray<int32> LoopMeshArray(const TArray<FTransformMeshData>& Sample, const EGeneratorLoopMode Mode, const uint8 Amount);
+};
---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/Procedural/ToroSplineGenBase.h
@@ -0,0 +1,45 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "ToroMeshGenBase.h"
+#include "Components/SplineComponent.h"
+#include "ToroSplineGenBase.generated.h"
+
+UCLASS(Abstract)
+class TOROUTILITIES_API AToroSplineGenBase : public AToroMeshGenBase
+{
+	GENERATED_BODY()
+
+public:
+	
+	AToroSplineGenBase() : bClosedLoop(false), SplineType(ESplinePointType::Curve)
+	{
+		SplineComponent = CreateDefaultSubobject<USplineComponent>("SplineComponent");
+		SplineComponent->AddSplinePoint(FVector(50.0f, 50.0f, 0.0f), ESplineCoordinateSpace::Local);
+		SetRootComponent(SplineComponent);
+
+		SceneRoot->SetupAttachment(SplineComponent);
+	}
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Subobjects)
+		TObjectPtr<USplineComponent> SplineComponent;
+	
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (DisplayPriority = -10))
+		bool bClosedLoop;
+	
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (DisplayPriority = -10))
+		TEnumAsByte<ESplinePointType::Type> SplineType;
+
+#if WITH_EDITOR
+protected:
+	virtual void OnConstruction(const FTransform& Transform) override
+	{
+		SplineComponent->SetClosedLoop(bClosedLoop);
+		for (int i = 0; i < SplineComponent->GetNumberOfSplinePoints(); i++)
+			SplineComponent->SetSplinePointType(i, SplineType);
+		
+		Super::OnConstruction(Transform);
+	}
+#endif
+};
