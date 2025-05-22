36819306ca483c124033b46e31b4a29a1e031ae5
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-22 08:31:56 -0400

    toro data asset

---
+++ Plugins/ToroUtilities/Source/ToroEditor/Public/AssetFactories/ToroAssetDefinition.h
@@ -0,0 +1,38 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "AssetDefinitionDefault.h"
+#include "Framework/ToroDataAsset.h"
+#include "ToroAssetDefinition.generated.h"
+
+UCLASS(Abstract)
+class TOROEDITOR_API UToroAssetDefinition : public UAssetDefinitionDefault
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual TConstArrayView<FAssetCategoryPath> GetAssetCategories() const override
+	{
+		static const TArray Categories{EAssetCategoryPaths::Blueprint / INVTEXT("ToroUtilities")};
+		return Categories;
+	}
+
+	virtual FText GetAssetDescription(const FAssetData& AssetData) const override
+	{
+		FText DataAssetText = FText::GetEmpty();
+		if (const UToroDataAsset* DA = Cast<UToroDataAsset>(AssetData.GetAsset()))
+		{
+			DataAssetText = DA->GetDescription();
+		}
+
+		FText OutText = FText::GetEmpty();
+		if (const FText SuperText = Super::GetAssetDescription(AssetData); !SuperText.IsEmptyOrWhitespace())
+		{
+			OutText = FText::Format(INVTEXT("{0}\n{1}\n{2}"), DataAssetText, SuperText);
+		}
+
+		return OutText;
+	}
+};
