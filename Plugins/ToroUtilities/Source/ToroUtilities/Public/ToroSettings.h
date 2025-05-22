c907a5a27832627ec00a206bb799d7844a284db9
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-22 13:05:13 -0400

    settings n' configs

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/ToroSettings.h
@@ -0,0 +1,18 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Helpers/ClassGetters.h"
+#include "Engine/DeveloperSettings.h"
+#include "ToroSettings.generated.h"
+
+UCLASS(Config = Game, DefaultConfig, DisplayName = "Toro Utilities")
+class TOROUTILITIES_API UToroSettings final : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+
+	UToroSettings() {}
+	SETTING_GETTER_MUTABLE(UToroSettings)
+};
