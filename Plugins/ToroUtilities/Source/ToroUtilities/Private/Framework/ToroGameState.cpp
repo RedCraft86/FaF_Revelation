2a9b543264d5266a1ca15a96f65c49ee4467e0f7
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-22 21:06:13 -0400

    zone states

--- Plugins/ToroUtilities/Source/ToroUtilities/Private/Framework/ToroGameState.cpp
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/Framework/ToroGameState.cpp
@@ -11,3 +11,23 @@ AToroGameState::AToroGameState()
 	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
 	SetRootComponent(SceneRoot);
 }
+
+bool AToroGameState::IsCharacterInZone(const FGameplayTag Character, const FGameplayTag Zone)
+{
+	return CharacterToZone.FindRef(Character) == Zone;
+}
+
+FGameplayTag AToroGameState::GetCharacterZone(const FGameplayTag Character)
+{
+	return CharacterToZone.FindRef(Character);
+}
+
+FGameplayTagContainer AToroGameState::GetAllCharactersInZone(const FGameplayTag Zone)
+{
+	FGameplayTagContainer Result;
+	for (const TPair<FGameplayTag, FGameplayTag>& Pair : CharacterToZone)
+	{
+		if (Pair.Value == Zone) Result.AddTag(Pair.Key);
+	}
+	return Result;
+}
--- Plugins/ToroUtilities/Source/ToroUtilities/Public/Framework/ToroGameState.h
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/Framework/ToroGameState.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "GameplayTagContainer.h"
 #include "Helpers/ClassGetters.h"
 #include "GameFramework/GameStateBase.h"
 #include "ToroGameState.generated.h"
@@ -19,4 +20,23 @@ public:
 
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Subobjects)
 		TObjectPtr<USceneComponent> SceneRoot;
+
+	UFUNCTION(BlueprintCallable, Category = Game)
+		bool IsCharacterInZone(UPARAM(meta = (Categories = "Character")) const FGameplayTag Character,
+			UPARAM(meta = (Categories = "Zone")) const FGameplayTag Zone);
+
+	UFUNCTION(BlueprintCallable, Category = Game)
+		FGameplayTag GetCharacterZone(UPARAM(meta = (Categories = "Character")) const FGameplayTag Character);
+
+	UFUNCTION(BlueprintCallable, Category = Game)
+		FGameplayTagContainer GetAllCharactersInZone(UPARAM(meta = (Categories = "Zone")) const FGameplayTag Zone);
+	
+	void AssignCharacterToZone(const FGameplayTag Character, const FGameplayTag Zone)
+	{
+		if (Character.IsValid()) CharacterToZone.Add(Character, Zone);
+	}
+	
+protected:
+
+	UPROPERTY() TMap<FGameplayTag, FGameplayTag> CharacterToZone;
 };
