c0542c68c30842a474ec2697978fcd25165e22d5
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-22 20:14:22 -0400

    other controller functions

--- Plugins/ToroUtilities/Source/ToroUtilities/Private/Framework/ToroPlayerController.cpp
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/Framework/ToroPlayerController.cpp
@@ -22,6 +22,73 @@ void AToroPlayerController::SetInputModeData(const FGameInputModeData& InputMode
 	FlushPressedKeys();
 }
 
+void AToroPlayerController::AddPauseRequest(const UObject* InObject)
+{
+	PauseRequests.Remove(nullptr);
+	if (InObject)
+	{
+		PauseRequests.Add(InObject);
+		UpdatePauseState();
+	}
+}
+
+void AToroPlayerController::RemovePauseRequest(const UObject* InObject)
+{
+	PauseRequests.Remove(nullptr);
+	if (InObject)
+	{
+		PauseRequests.Remove(InObject);
+		UpdatePauseState();
+	}
+}
+
+void AToroPlayerController::SetGamePaused(const bool bInPaused)
+{
+	if (IsPaused() != bInPaused)
+	{
+		bGamePaused = bInPaused;
+		UpdatePauseState();
+		OnGamePaused.Broadcast(bGamePaused);
+	}
+}
+
+void AToroPlayerController::EnterCinematic(AActor* InActor)
+{
+	if (!CinematicActor)
+	{
+		CinematicActor = InActor;
+		SetCinematicMode(true, true, false, true, true);
+		// if (AToroPlayerCharacter* PlayerChar = Cast<AToroPlayerCharacter>(GetPawn()))
+		// {
+		// 	PlayerChar->AddLockFlag(Tag_PlayerLock_Cinematic.GetTag()); TODO
+		// }
+	}
+}
+
+void AToroPlayerController::ExitCinematic()
+{
+	if (CinematicActor)
+	{
+		CinematicActor = nullptr;
+		SetCinematicMode(false, false, false, true, true);
+		// if (AToroPlayerCharacter* PlayerChar = Cast<AToroPlayerCharacter>(GetPawn()))
+		// {
+		// 	PlayerChar->ClearLockFlag(Tag_PlayerLock_Cinematic.GetTag()); TODO
+		// }
+	}
+}
+
+void AToroPlayerController::UpdatePauseState()
+{
+	PauseRequests.Remove(nullptr);
+	SetPause(bGamePaused || !PauseRequests.IsEmpty());
+}
+
+void AToroPlayerController::OnAnyKeyEvent(FKey PressedKey)
+{
+	if (OnAnyKeyPressed.IsBound()) OnAnyKeyPressed.Broadcast(PressedKey);
+}
+
 void AToroPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -33,10 +100,30 @@ void AToroPlayerController::BeginPlay()
 	}
 }
 
+void AToroPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	FInputKeyBinding& AnyKeyBinding = InputComponent->BindKey(EKeys::AnyKey,
+		IE_Pressed, this, &AToroPlayerController::OnAnyKeyEvent);
+	AnyKeyBinding.bConsumeInput = false; AnyKeyBinding.bExecuteWhenPaused = true;
+}
+
 void AToroPlayerController::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
 	InputModeData.ClearReferences();
 	Super::EndPlay(EndPlayReason);
+}
+
+bool AToroPlayerController::SetPause(bool bPause, FCanUnpause CanUnpauseDelegate)
+{
+	if (IsPaused() != bPause)
+	{
+		// if (const AToroMusicManager* MusicManager = AToroMusicManager::Get(this))
+		// {
+		// 	MusicManager->SetDipAudio(bPause); TODO
+		// }
+	}
+	return Super::SetPause(bPause, CanUnpauseDelegate);
 }
 
 UEnhancedInputLocalPlayerSubsystem* AToroPlayerController::GetEnhancedInputSubsystem() const
--- Plugins/ToroUtilities/Source/ToroUtilities/Public/Framework/ToroPlayerController.h
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/Framework/ToroPlayerController.h
@@ -21,6 +21,9 @@ public:
 
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Subobjects)
 		TObjectPtr<USceneComponent> SceneRoot;
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAnyKeyPressedEvent, const FKey&, Key);
+	UPROPERTY(BlueprintAssignable) FAnyKeyPressedEvent OnAnyKeyPressed;
 	
 	UFUNCTION(BlueprintCallable, Category = Input)
 		virtual void SetInputModeData(const FGameInputModeData& InputMode);
@@ -28,11 +31,43 @@ public:
 	UFUNCTION(BlueprintPure, Category = Input)
 		const FGameInputModeData& GetInputModeData() const { return InputModeData; }
 
+	UFUNCTION(BlueprintCallable, Category = Controller)
+		void AddPauseRequest(const UObject* InObject);
+
+	UFUNCTION(BlueprintCallable, Category = Controller)
+		void RemovePauseRequest(const UObject* InObject);
+
+	UFUNCTION(BlueprintCallable, Category = Controller)
+		void SetGamePaused(const bool bInPaused);
+
+	UFUNCTION(BlueprintCallable, Category = Controller)
+		bool IsGamePaused() const { return bGamePaused; }
+	
+	UFUNCTION(BlueprintCallable, Category = Player)
+		virtual void EnterCinematic(AActor* InActor);
+
+	UFUNCTION(BlueprintCallable, Category = Player)
+		virtual void ExitCinematic();
+	
+	UFUNCTION(BlueprintPure, Category = Player)
+		AActor* GetCinematicActor() const { return CinematicActor; }
+
+	DECLARE_MULTICAST_DELEGATE_OneParam(FOnGamePaused, const bool)
+	FOnGamePaused OnGamePaused;
+
 protected:
 	
+	UPROPERTY() bool bGamePaused;
 	UPROPERTY() FGameInputModeData InputModeData;
+	UPROPERTY(Transient) TObjectPtr<AActor> CinematicActor;
+	UPROPERTY(Transient) TSet<TWeakObjectPtr<const UObject>> PauseRequests;
+
+	virtual void UpdatePauseState();
+	virtual void OnAnyKeyEvent(FKey PressedKey);
 
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual bool SetPause(bool bPause, FCanUnpause CanUnpauseDelegate = FCanUnpause()) override;
 	class UEnhancedInputLocalPlayerSubsystem* GetEnhancedInputSubsystem() const;
 };
