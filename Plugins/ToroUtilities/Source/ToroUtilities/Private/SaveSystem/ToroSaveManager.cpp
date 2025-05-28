b3abe2d4589aeb27df7f1cab057b8f2f7436c200
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-28 19:42:28 -0400

    save manager

--- Plugins/ToroUtilities/Source/ToroUtilities/Private/SaveSystem/ToroSaveManager.cpp
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/SaveSystem/ToroSaveManager.cpp
@@ -1,3 +1,38 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
 #include "SaveSystem/ToroSaveManager.h"
+#include "ToroSettings.h"
+
+UToroSaveObject* UToroSaveManager::GetSaveObject(const FGameplayTag InTag)
+{
+	if (VerifySaveTag(InTag)) return nullptr;
+	if (TObjectPtr<UToroSaveObject>* Ptr = SaveObjects.Find(InTag))
+	{
+		if (*Ptr) return *Ptr;
+		SaveObjects.Remove(InTag);
+	}
+
+	return nullptr;
+}
+
+void UToroSaveManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	UToroSettings* Settings = UToroSettings::Get();
+	for (const TPair<FGameplayTag, TSoftClassPtr<UToroSaveObject>>& Info : Settings->SaveObjects)
+	{
+		if (UToroSaveObject* Obj = UToroSaveObject::Create(this, Info.Value.LoadSynchronous(), Info.Key))
+		{
+			SaveObjects.Add(Info.Key, Obj);
+		}
+	}
+}
+
+void UToroSaveManager::Deinitialize()
+{
+	for (const TPair<FGameplayTag, TObjectPtr<UToroSaveObject>>& Obj : SaveObjects)
+	{
+		if (Obj.Value) Obj.Value->SaveObject(nullptr);
+	}
+	Super::Deinitialize();
+}
--- Plugins/ToroUtilities/Source/ToroUtilities/Public/SaveSystem/ToroSaveManager.h
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/SaveSystem/ToroSaveManager.h
@@ -2,11 +2,36 @@
 
 #pragma once
 
+#include "ToroSaveObjects.h"
+#include "Helpers/ClassGetters.h"
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "ToroSaveManager.generated.h"
 
 UCLASS(BlueprintType, DisplayName = "Save Manager")
 class TOROUTILITIES_API UToroSaveManager final : public UGameInstanceSubsystem
 {
-	GENERATED_BODY()// TODO
+	GENERATED_BODY()
+
+public:
+
+	UToroSaveManager(): ActiveGameSave(Tag_Save_Game) {}
+	GAME_INSTANCE_SUBSYSTEM_GETTER(UToroSaveManager);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = SaveSystem)
+		FGameplayTag ActiveGameSave;
+
+	UFUNCTION(BlueprintPure, Category = SaveSystem)
+		UToroSaveObject* GetSaveObject(UPARAM(meta = (Categories = "Save")) const FGameplayTag InTag);
+
+	template <typename T = UToroSaveObject>
+	T* GetSaveObject(const FGameplayTag& InTag) { return Cast<T>(GetSaveObject(InTag)); }
+
+	DECLARE_MULTICAST_DELEGATE_OneParam(FOnSaveObjectIO, const UToroSaveObject*)
+	FOnSaveObjectIO OnSaveObjectIO;
+
+private:
+
+	UPROPERTY(Transient) TMap<FGameplayTag, TObjectPtr<UToroSaveObject>> SaveObjects;
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
 };
