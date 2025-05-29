50bf0c79945ea97195c22c91ba7f4dc440c89f54
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-29 14:09:31 -0400

    native action types

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/WorldActions/Actions/MiscActions.cpp
@@ -0,0 +1,78 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "WorldActions/Actions/MiscActions.h"
+#include "Achievement/AchievementManager.h"
+#include "MiscClasses/ToroSequenceActor.h"
+#include "Kismet/GameplayStatics.h"
+
+void FWTaskLevelStreaming::RunEvent(const UObject* WorldContext)
+{
+	if (Level.IsNull()) return;
+	if (bUnloadLevel)
+	{
+		UGameplayStatics::UnloadStreamLevelBySoftObjectPtr(WorldContext, Level,
+			GetLatentInfo(), false);
+	}
+	else
+	{
+		UGameplayStatics::LoadStreamLevelBySoftObjectPtr(WorldContext, Level,
+			bMakeVisibleAfterLoad, false, GetLatentInfo());
+	}
+}
+
+void FWTaskLevelSequence::RunEvent(const UObject* WorldContext)
+{
+	FOR_EACH_SOFT_PTR(Targets, AToroSequenceActor, {
+		if (bStopPlaying)
+		{
+			Ptr->SkipToEnd();
+		}
+		else if (!FMath::IsNearlyZero(PlayRate))
+		{
+			Ptr->SetPlayRate(FMath::Abs(PlayRate));
+			if (PlayRate < 0.0f)
+			{
+				Ptr->SkipToEnd();
+				Ptr->Reverse();
+			}
+			else
+			{
+				Ptr->StopAtCurrentTime();
+				Ptr->SetPlaybackPosition({0.0f, EUpdatePositionMethod::Jump});
+				Ptr->Play();
+			}
+		}
+	})
+}
+
+void FWTaskAchievement::RunEvent(const UObject* WorldContext)
+{
+	if (!VerifyAchievementTag(AchievementID) || Progress == 0) return;
+	if (UAchievementManager* Manager = UAchievementManager::Get(WorldContext))
+	{
+		Manager->Grant(AchievementID, Progress);
+	}
+}
+
+void FWTaskWorldSound::RunEvent(const UObject* WorldContext)
+{
+	FOR_EACH_SOFT_PTR(Targets, AAmbientSound, {
+		if (UAudioComponent* AudioComp = Ptr->GetAudioComponent())
+		{
+			if (bFade)
+			{
+				bStopSound ? AudioComp->FadeOut(FadeTime, 0.0f, FadeCurve) :
+					AudioComp->FadeIn(FadeTime, 1.0f, StartTime, FadeCurve);
+			}
+			else
+			{
+				bStopSound ? AudioComp->Stop() : AudioComp->Play();
+			}
+		}
+	})
+}
+
+void FWTaskSound2D::RunEvent(const UObject* WorldContext)
+{
+	UGameplayStatics::PlaySound2D(WorldContext, Sound, Volume, Pitch);
+}
---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/WorldActions/Actions/MiscActions.h
@@ -0,0 +1,131 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Sound/AmbientSound.h"
+#include "Components/AudioComponent.h"
+#include "Helpers/GameplayTagHelpers.h"
+#include "WorldActions/WorldActionBase.h"
+#include "MiscActions.generated.h"
+
+USTRUCT(BlueprintInternalUseOnly, DisplayName = "Misc Actions", meta = (Hidden))
+struct TOROUTILITIES_API FWTaskMiscActions : public FWorldActionBase
+{ GENERATED_BODY() };
+
+USTRUCT(BlueprintType, DisplayName = "Level Streaming")
+struct TOROUTILITIES_API FWTaskLevelStreaming final : public FWTaskMiscActions
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = LevelStreaming)
+		bool bUnloadLevel;
+	
+	UPROPERTY(EditAnywhere, Category = LevelStreaming, meta = (EditCondition = "!bUnloadLevel", EditConditionHides))
+		bool bMakeVisibleAfterLoad;
+	
+	UPROPERTY(EditAnywhere, Category = LevelStreaming)
+		TSoftObjectPtr<UWorld> Level;
+
+	FWTaskLevelStreaming(): bUnloadLevel(false), bMakeVisibleAfterLoad(true) {}
+
+protected:
+
+	virtual void RunEvent(const UObject* WorldContext) override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Level Sequence")
+struct TOROUTILITIES_API FWTaskLevelSequence final : public FWTaskMiscActions
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = LevelSequence)
+		bool bStopPlaying;
+
+	UPROPERTY(EditAnywhere, Category = LevelSequence, meta = (EditCondition = "!bStopPlaying", EditConditionHides))
+		float PlayRate;
+
+	UPROPERTY(EditAnywhere, Category = LevelSequence)
+		TSet<TSoftObjectPtr<class AToroSequenceActor>> Targets;
+
+	FWTaskLevelSequence(): bStopPlaying(false), PlayRate(1.0f) {}
+
+protected:
+
+	virtual void RunEvent(const UObject* WorldContext) override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Grant Achievement")
+struct TOROUTILITIES_API FWTaskAchievement final : public FWTaskMiscActions
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Achievement, meta = (Categories = "Achievement"))
+		FGameplayTag AchievementID;
+
+	UPROPERTY(EditAnywhere, Category = Achievement, meta = (ClampMin = 1, UIMin = 1, ClampMax = 200, UIMax = 200))
+		uint8 Progress;
+
+	FWTaskAchievement(): AchievementID(GAMEPLAY_TAG_BASE(Achievement)), Progress(1) {}
+
+protected:
+
+	virtual void RunEvent(const UObject* WorldContext) override;
+};
+
+USTRUCT(BlueprintInternalUseOnly, DisplayName = "Sound Actions", meta = (Hidden))
+struct TOROUTILITIES_API FWTaskSoundActions : public FWTaskMiscActions
+{ GENERATED_BODY() };
+
+USTRUCT(BlueprintType, DisplayName = "World Sound")
+struct TOROUTILITIES_API FWTaskWorldSound final : public FWTaskSoundActions
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = WorldSound)
+		bool bStopSound;
+	
+	UPROPERTY(EditAnywhere, Category = WorldSound, meta = (ClampMin = 0.0f, UIMin = 0.0f, EditCondition = "!bStopSound", EditConditionHides))
+		float StartTime;
+	
+	UPROPERTY(EditAnywhere, Category = WorldSound)
+		bool bFade;
+
+	UPROPERTY(EditAnywhere, Category = WorldSound, meta = (ClampMin = 0.1f, UIMin = 0.1f, EditCondition = "bFade", EditConditionHides))
+		float FadeTime;
+	
+	UPROPERTY(EditAnywhere, Category = WorldSound, meta = (EditCondition = "bFade", EditConditionHides))
+		EAudioFaderCurve FadeCurve;
+	
+	UPROPERTY(EditAnywhere, Category = WorldSound)
+		TSet<TSoftObjectPtr<AAmbientSound>> Targets;
+
+	FWTaskWorldSound()
+		: bStopSound(false), StartTime(0.0f), bFade(false)
+		, FadeTime(1.0f), FadeCurve(EAudioFaderCurve::Linear)
+	{}
+
+protected:
+	
+	virtual void RunEvent(const UObject* WorldContext) override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "2D Sound")
+struct TOROUTILITIES_API FWTaskSound2D final : public FWTaskSoundActions
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Sound2D)
+		TObjectPtr<USoundBase> Sound;
+
+	UPROPERTY(EditAnywhere, Category = Sound2D, meta = (ClampMin = 0.1f, UIMin = 0.1f))
+		float Volume;
+	
+	UPROPERTY(EditAnywhere, Category = Sound2D, meta = (ClampMin = 0.1f, UIMin = 0.1f))
+		float Pitch;
+
+	FWTaskSound2D(): Volume(1.0f), Pitch(1.0f) {}
+
+protected:
+
+	virtual void RunEvent(const UObject* WorldContext) override;
+};
