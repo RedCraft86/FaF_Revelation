15708863c45faad171926640d95b45fed8406be9
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-28 20:23:46 -0400

    extra settings

--- Plugins/ToroUtilities/Source/ToroUtilities/Public/ToroSettings.h
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/ToroSettings.h
@@ -4,6 +4,7 @@
 
 #include "InputMappingContext.h"
 #include "GameplayTagContainer.h"
+#include "GeneralProjectSettings.h"
 #include "Helpers/ClassGetters.h"
 #include "SaveSystem/SaveGameTypes.h"
 #include "SaveSystem/ToroSaveObjects.h"
@@ -19,7 +20,7 @@ class TOROUTILITIES_API UToroSettings final : public UDeveloperSettings
 
 public:
 
-	UToroSettings()
+	UToroSettings(): WordsPerSecond(2), bUseLightProbes(true)
 	{
 		CategoryName = TEXT("Project");
 		SectionName = TEXT("ToroUtilities");
@@ -28,9 +29,21 @@ public:
 			{Tag_Save_Global, UToroGlobalSave::StaticClass()},
 			{Tag_Save_Game, UToroGameSave::StaticClass()},
 		};
+
+		LightProbePPM = FSoftObjectPath(TEXT("/ToroUtilities/Assets/PostProcess/PPM_LightProbes.PPM_LightProbes"));
+		BrightnessPPM = FSoftObjectPath(TEXT("/ToroUtilities/Assets/PostProcess/PPM_Brightness.PPM_Brightness"));
+
+		MainSoundMix = FSoftObjectPath(TEXT("/Game/AssetPacks/_Globals/Audio/SMC_Master.SMC_Master"));
+		// SoundClasses[0] = FSoftObjectPath(TEXT("/Game/AssetPacks/_Globals/Audio/SC_Master.SC_Master"));
+		// SoundClasses[1] = FSoftObjectPath(TEXT("/Game/AssetPacks/_Globals/Audio/SC_Music.SC_Music"));
+		// SoundClasses[2] = FSoftObjectPath(TEXT("/Game/AssetPacks/_Globals/Audio/SC_SoundFX.SC_SoundFX"));
+		// SoundClasses[3] = FSoftObjectPath(TEXT("/Game/AssetPacks/_Globals/Audio/SC_Voice.SC_Voice"));
 	}
 
 	SETTING_GETTER_MUTABLE(UToroSettings)
+
+	UPROPERTY(Config, EditAnywhere, Category = Runtime, meta = (ClampMin = 1, UIMin = 1))
+		uint8 WordsPerSecond;
 
 	UPROPERTY(Config, EditAnywhere, Category = Runtime)
 		TSoftObjectPtr<UWorld> GameplayMap;
@@ -50,8 +63,53 @@ public:
 	UPROPERTY(Config, EditAnywhere, Category = SaveSystem, meta = (ForceInlineRow, Categories = "Save"))
 		TMap<FGameplayTag, TSoftClassPtr<UToroSaveObject>> SaveObjects;
 
+	UPROPERTY(Config, EditAnywhere, Category = LightProbes)
+		bool bUseLightProbes;
+
+	UPROPERTY(Config, EditAnywhere, Category = LightProbes)
+		TSoftObjectPtr<UMaterialInterface> LightProbePPM;
+
+	UPROPERTY(Config, EditAnywhere, Category = UserSettings)
+		TSoftObjectPtr<UMaterialInterface> BrightnessPPM;
+
+	UPROPERTY(Config, EditAnywhere, Category = UserSettings)
+		TSoftObjectPtr<USoundMix> MainSoundMix;
+
+	float GetReadingTime(const FString& InString) const
+	{
+		TArray<FString> Words;
+		InString.ParseIntoArray(Words, TEXT(" "));
+		return Words.Num() / FMath::Max(1.0f, (float)WordsPerSecond);
+	}
+
+	bool IsOnGameplayMap(const UObject* WorldContext) const
+	{
+		if (GameplayMap.IsNull()) return false;
+		return UGameplayStatics::GetCurrentLevelName(WorldContext) == GameplayMap.GetAssetName();
+	}
+
 	FString GetDemoName() const
 	{
 		return DemoName.IsNone() ? TEXT("") : DemoName.ToString().Replace(TEXT(" "), TEXT(""));
+	}
+
+	FText GetVersionLabel() const
+	{
+		FText Result;
+		if (const UGeneralProjectSettings* ProjectSettings = GetDefault<UGeneralProjectSettings>())
+		{
+			Result = FText::Format(INVTEXT("Version: {0}"), FText::FromString(ProjectSettings->ProjectVersion));
+		}
+		if (!DemoName.IsNone())
+		{
+			Result = FText::Format(INVTEXT("{0} | {1}"), Result, FText::FromName(DemoName));
+		}
+		return FText::Format(INVTEXT("{0} | {1} Build"), Result,
+			FText::FromString(LexToString(FApp::GetBuildConfiguration())));
+	}
+
+	bool IsUsingLightProbes() const
+	{
+		return bUseLightProbes && !LightProbePPM.IsNull();
 	}
 };
