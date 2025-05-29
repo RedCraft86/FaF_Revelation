450d18535a6c7bb2f2dd00f60248d77c9ecbc1b0
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-29 11:17:24 -0400

    light probe system

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/LightProbes/LightProbeActor.cpp
@@ -0,0 +1,116 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "LightProbes/LightProbeActor.h"
+#include "LightProbes/LightProbeManager.h"
+
+ALightProbeActor::ALightProbeActor(): Intensity(1.0f), Radius(500.0f), Falloff(2.0f), Color(FLinearColor::White)
+	, bDisableWithLumen(false), MaxDistance(3000.0f), FadeRange(100.0f)
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SetRootComponent(SceneRoot);
+
+#if WITH_EDITORONLY_DATA
+	WireShape = CreateEditorOnlyDefaultSubobject<UWireShapeComponent>("WireShape");
+	IconBillboard = CreateEditorOnlyDefaultSubobject<UMaterialBillboardComponent>("IconBillboard");
+	if (IconBillboard)
+	{
+		IconBillboard->SetupAttachment(SceneRoot);
+		IconBillboard->SetHiddenInGame(true);
+	}
+#endif
+}
+
+bool ALightProbeActor::IsRelevantProbe(const FTransform& Camera, const bool bHasLumen) const
+{
+	if (IsHidden() || (bHasLumen && bDisableWithLumen)) return false;
+	if (FMath::IsNearlyZero(Intensity) || Radius < 50.0f) return false;
+
+	const FVector ThisLocation = GetActorLocation();
+	FVector CameraLocation = Camera.GetTranslation();
+
+	if (FVector::Dist(ThisLocation, CameraLocation) > MaxDistance) return false;
+
+	CameraLocation -= Camera.GetRotation().Vector() * Radius;
+	FVector ProbeToCam = ThisLocation - CameraLocation; ProbeToCam.Normalize();
+	return FVector::DotProduct(Camera.GetRotation().Vector(), ProbeToCam) > 0.0f;
+}
+
+void ALightProbeActor::ApplyData(UMaterialInstanceDynamic* Material, const uint8 Idx, const FVector& Camera) const
+{
+	if (Material)
+	{
+		float Multi = Intensity;
+		if (FadeRange > 50.0f)
+		{
+			Multi *= FMath::GetMappedRangeValueClamped(
+			   FVector2D{MaxDistance, MaxDistance - FadeRange},
+			   FVector2D{0.0f, 1.0f}, FVector::Dist(GetActorLocation(), Camera));
+		}
+
+		Material->SetVectorParameterValue(ProbeParam(Idx, false), FLinearColor{
+			Color.R * Multi,
+			Color.G * Multi,
+			Color.B * Multi,
+			Falloff
+		});
+
+		const FVector Pos = GetActorLocation();
+		Material->SetVectorParameterValue(ProbeParam(Idx, true), FLinearColor{
+			(float)Pos.X, (float)Pos.Y, (float)Pos.Z, Radius
+		});
+	}
+}
+
+void ALightProbeActor::SetActorHiddenInGame(bool bNewHidden)
+{
+	Super::SetActorHiddenInGame(bNewHidden);
+	if (ULightProbeManager* Manager = ULightProbeManager::Get(this))
+	{
+		Manager->ForceProbeRecollection();
+	}
+}
+
+#if WITH_EDITOR
+void ALightProbeActor::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	if (WireShape)
+	{
+		FWireSphereData Data;
+		Data.NumOfSides = 64;
+		Data.Radius = Radius;
+		Data.Color = FColor::FromHex(TEXT("97C1D0FF"));
+		WireShape->WireSpheres.Add(TEXT("Radius"), Data);
+	}
+	if (IconBillboard)
+	{
+		if (!IconMaterial)
+		{
+			if (UMaterialInterface* BaseMat = LoadObject<UMaterialInterface>(nullptr,
+				TEXT("/ToroUtilities/Icons/LightProbe/M_LightProbe.M_LightProbe")))
+			{
+				IconMaterial = UMaterialInstanceDynamic::Create(BaseMat, IconBillboard);
+			}
+			
+			if (!IconBillboard->Elements.IsEmpty()) IconBillboard->SetElements({});
+		}
+
+		if (IconMaterial)
+		{
+			IconMaterial->SetVectorParameterValue(TEXT("Color"), Color);
+			if (IconBillboard->Elements.IsEmpty())
+			{
+				FMaterialSpriteElement Sprite;
+				Sprite.Material = IconMaterial;
+				Sprite.BaseSizeX = Sprite.BaseSizeY = 40.0f;
+				Sprite.DistanceToSizeCurve = LoadObject<UCurveFloat>(nullptr,
+					TEXT("/ToroUtilities/Icons/LightProbe/C_LightProbe.C_LightProbe"));
+				
+				IconBillboard->SetElements({Sprite});
+			}
+		}
+	}
+}
+#endif
--- Plugins/ToroUtilities/Source/ToroUtilities/Public/LightProbes/LightProbeManager.h
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/LightProbes/LightProbeManager.h
@@ -2,11 +2,49 @@
 
 #pragma once
 
+#include "LightProbeActor.h"
+#include "Helpers/ClassGetters.h"
 #include "Subsystems/WorldSubsystem.h"
 #include "LightProbeManager.generated.h"
+
+inline FName ProbeParam(const uint8 Idx, const bool bPos)
+{
+	return *FString::Printf(TEXT("%s_%d"),
+		bPos ? TEXT("Position") : TEXT("Color"), Idx + 1);
+}
 
 UCLASS(BlueprintType, DisplayName = "Light Probes")
 class TOROUTILITIES_API ULightProbeManager final : public UTickableWorldSubsystem
 {
-	GENERATED_BODY()// TODO
+	GENERATED_BODY()
+
+public:
+
+	ULightProbeManager(): bHasLumen(false), TickTime(0.0f) {}
+	WORLD_SUBSYSTEM_GETTER(ULightProbeManager);
+
+	void ForceProbeRecollection() { TickTime = 1.0f; }
+	
+private:
+
+	UPROPERTY() bool bHasLumen;
+	UPROPERTY() float TickTime;
+	UPROPERTY(Transient) TObjectPtr<APlayerCameraManager> CamManager;
+	UPROPERTY(Transient) TObjectPtr<class AMasterPostProcess> MasterPP;
+	UPROPERTY(Transient) TArray<TWeakObjectPtr<ALightProbeActor>> LightProbes;
+
+	void UpdateProbes();
+	void CollectProbes();
+	void ResetPPM(const uint8 Idx) const;
+	FTransform GetCamera() const;
+
+	virtual bool IsTickable() const override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+	virtual TStatId GetStatId() const override { return GetStatID(); }
+#if WITH_EDITOR
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual bool IsTickableInEditor() const override { return true; }
+#endif
 };
