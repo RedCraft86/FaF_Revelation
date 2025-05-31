c152edeb78c76733c048a9f75cd3877bf95c88f8
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-31 13:14:59 -0400

    fix oversight

--- Source/FaFRevGame/Narrative/GameNarrative.h
+++ Source/FaFRevGame/Narrative/GameNarrative.h
@@ -19,7 +19,7 @@ public:
 	UFUNCTION(BlueprintPure, Category = Game, meta = (WorldContext = "ContextObject"))
 		static UGameNarrative* GetNarrative(const UObject* ContextObject) { return Get(ContextObject); }
 
-	UPROPERTY(Config, EditAnywhere, Category = Settings)
+	UPROPERTY(EditAnywhere, Category = Settings)
 		TSubclassOf<UUserWidget> NarrativeWidget; // TODO: Correct type?
 
 	virtual void DialogueBegan(UDialogue* Dialogue) override;
