d992dd5c79e7605845240f86e512968c8237d157
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-01 00:22:07 -0400

    actor actions

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/WorldActions/Actions/ActorActions.cpp
@@ -0,0 +1,65 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "WorldActions/Actions/ActorActions.h"
+#include "Engine/StaticMeshActor.h"
+
+void FWAActorVisibility::RunAction()
+{
+	FOR_EACH_SOFT_PTR(Targets, AActor, {
+		Ptr->SetActorHiddenInGame(bToggle ? !Ptr->IsHidden() : !bVisible);
+	})
+}
+
+void FWAActorCollision::RunAction()
+{
+	FOR_EACH_SOFT_PTR(Targets, AActor, {
+		Ptr->SetActorEnableCollision(bToggle ? !Ptr->GetActorEnableCollision() : bCollision);
+	})
+}
+
+void FWAActorTags::OnBeginPlay()
+{
+	Add.Remove(NAME_None);
+	Remove.Remove(NAME_None);
+}
+
+void FWAActorTags::RunAction()
+{
+	FOR_EACH_SOFT_PTR(Targets, AActor, {
+		for (const FName& Tag : Add) { Ptr->Tags.AddUnique(Tag); }
+		Ptr->Tags.RemoveAll([this](const FName& Tag)
+		{
+			return Remove.Contains(Tag);
+		});
+	})
+}
+
+void FWASMActorMaterials::RunAction()
+{
+	FOR_EACH_SOFT_PTR(Targets, AStaticMeshActor, {
+		if (UStaticMeshComponent* MeshComp = Ptr->GetStaticMeshComponent())
+		{
+			for (const TPair<uint8, TSoftObjectPtr<UMaterialInterface>>& Pair : Materials)
+			{
+				if (UMaterialInterface* Material = Pair.Value.LoadSynchronous())
+				{
+					MeshComp->SetMaterial(Pair.Key, Material);
+				}
+			}
+		}
+	})
+}
+
+void FWASMActorPrimitiveData::RunAction()
+{
+	FOR_EACH_SOFT_PTR(Targets, AStaticMeshActor, {
+		if (UStaticMeshComponent* MeshComp = Ptr->GetStaticMeshComponent())
+		{
+			for (const TPair<uint8, float>& Pair : Data)
+			{
+				bSerializable ? MeshComp->SetDefaultCustomPrimitiveDataFloat(Pair.Key, Pair.Value)
+					: MeshComp->SetCustomPrimitiveDataFloat(Pair.Key, Pair.Value);
+			}
+		}
+	})
+}
---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/WorldActions/Actions/ActorActions.h
@@ -0,0 +1,117 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "WorldActions/WorldActionBase.h"
+#include "ActorActions.generated.h"
+
+USTRUCT(BlueprintInternalUseOnly, DisplayName = "Actor", meta = (Hidden))
+struct TOROUTILITIES_API FWAActorActions : public FWorldActionBase
+{ GENERATED_BODY() };
+
+USTRUCT(BlueprintType, DisplayName = "Visibility")
+struct TOROUTILITIES_API FWAActorVisibility final : public FWAActorActions
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = Visibility)
+		bool bToggle;
+
+	UPROPERTY(EditAnywhere, Category = Visibility, meta = (EditCondition = "!bToggle", EditConditionHides))
+		bool bVisible;
+
+	UPROPERTY(EditAnywhere, Category = Visibility)
+		TSet<TSoftObjectPtr<AActor>> Targets;
+	
+	FWAActorVisibility(): bToggle(false), bVisible(true) {}
+
+protected:
+
+	virtual void RunAction() override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Collision")
+struct TOROUTILITIES_API FWAActorCollision final : public FWAActorActions
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = Visibility)
+		bool bToggle;
+
+	UPROPERTY(EditAnywhere, Category = Visibility, meta = (EditCondition = "!bToggle", EditConditionHides))
+		bool bCollision;
+
+	UPROPERTY(EditAnywhere, Category = Visibility)
+		TSet<TSoftObjectPtr<AActor>> Targets;
+	
+	FWAActorCollision(): bToggle(false), bCollision(true) {}
+
+protected:
+
+	virtual void RunAction() override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Tags")
+struct TOROUTILITIES_API FWAActorTags final : public FWAActorActions
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = Tags)
+		TSet<FName> Add;
+
+	UPROPERTY(EditAnywhere, Category = Tags)
+		TSet<FName> Remove;
+
+	UPROPERTY(EditAnywhere, Category = Tags)
+	TSet<TSoftObjectPtr<AActor>> Targets;
+	
+	FWAActorTags() {}
+
+protected:
+
+	virtual void OnBeginPlay() override;
+	virtual void RunAction() override;
+};
+
+USTRUCT(BlueprintInternalUseOnly, DisplayName = "Static Mesh", meta = (Hidden))
+struct TOROUTILITIES_API FWASMActorActions : public FWAActorActions
+{ GENERATED_BODY() };
+
+USTRUCT(BlueprintType, DisplayName = "Materials")
+struct TOROUTILITIES_API FWASMActorMaterials final : public FWASMActorActions
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Materials)
+		TMap<uint8, TSoftObjectPtr<UMaterialInterface>> Materials;
+
+	UPROPERTY(EditAnywhere, Category = Materials)
+		TSet<TSoftObjectPtr<AStaticMeshActor>> Targets;
+
+	FWASMActorMaterials() {}
+
+protected:
+
+	virtual void RunAction() override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "PrimitiveData")
+struct TOROUTILITIES_API FWASMActorPrimitiveData final : public FWASMActorActions
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = PrimitiveData)
+		bool bSerializable;
+
+	UPROPERTY(EditAnywhere, Category = PrimitiveData)
+		TMap<uint8, float> Data;
+
+	UPROPERTY(EditAnywhere, Category = PrimitiveData)
+		TSet<TSoftObjectPtr<AStaticMeshActor>> Targets;
+
+	FWASMActorPrimitiveData(): bSerializable(false) {}
+
+protected:
+
+	virtual void RunAction() override;
+};
