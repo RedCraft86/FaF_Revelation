13b42cc2a04f5105a7c9ebdd34cf9dc1a66bfd5d
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-01 14:45:03 -0400

    Local Sound

---
+++ Source/FaFRevGame/MusicSystem/LocalSoundVolume.cpp
@@ -0,0 +1,75 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "LocalSoundVolume.h"
+#include "GameMusicManager.h"
+#include "Components/AudioComponent.h"
+#include "Framework/ToroPlayerCharacter.h"
+#include "Interfaces/CharInterface.h"
+
+ALocalSoundVolume::ALocalSoundVolume(): Cooldown(5.0f), Volume(1.0f), StartRange(0.0f)
+	, FadeInOut(1.0f), AllowedMusicStates({0}), bInVolume(false), bOnCooldown(false), MusicState(0)
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	SoundComponent = CreateDefaultSubobject<UAudioComponent>("SoundComponent");
+	SoundComponent->SetupAttachment(GetRootComponent());
+	SoundComponent->SetUISound(true);
+	SoundComponent->bCanPlayMultipleInstances = false;
+	SoundComponent->bAutoActivate = false;
+	SoundComponent->bIsMusic = true;
+
+#if WITH_EDITOR
+	BrushColor = FColor(180, 0, 255);
+#endif
+}
+
+void ALocalSoundVolume::UpdateAudio()
+{
+	if (bInVolume && bOnCooldown && AllowedMusicStates.Contains(MusicState))
+	{
+		SoundComponent->FadeIn(FadeInOut.X, 1.0f,
+			FMath::FRandRange(StartRange.X, StartRange.Y));
+
+		bOnCooldown = true;
+		FTimerHandle Handle;
+		GetWorldTimerManager().SetTimer(Handle, this,
+			&ALocalSoundVolume::CooldownFinish, Cooldown, false);
+	}
+	else SoundComponent->FadeOut(FadeInOut.Y, 0.0f);
+}
+
+void ALocalSoundVolume::BeginPlay()
+{
+	Super::BeginPlay();
+	SoundComponent->SetSound(Sound);
+	SoundComponent->SetVolumeMultiplier(Volume);
+	GetWorldTimerManager().SetTimerForNextTick([this]()
+	{
+		if (AGameMusicManager* MusicManager = AGameMusicManager::Get<AGameMusicManager>(this))
+		{
+			MusicManager->OnThemeChanged.AddUObject(this, &ALocalSoundVolume::OnMusicThemeChanged);
+			MusicManager->OnStateChanged.AddUObject(this, &ALocalSoundVolume::OnMusicStateChanged);
+		}
+	});
+}
+
+void ALocalSoundVolume::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	if (const FGameplayTag Char = ICharInterface::GetCharacterID(OtherActor); Char == Tag_Character_Player)
+	{
+		bInVolume = true;
+		UpdateAudio();
+	}
+}
+
+void ALocalSoundVolume::NotifyActorEndOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorEndOverlap(OtherActor);
+	if (const FGameplayTag Char = ICharInterface::GetCharacterID(OtherActor); Char == Tag_Character_Player)
+	{
+		bInVolume = false;
+		UpdateAudio();
+	}
+}
---
+++ Source/FaFRevGame/MusicSystem/LocalSoundVolume.h
@@ -0,0 +1,66 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Actors/ToroVolume.h"
+#include "LocalSoundVolume.generated.h"
+
+#define UPDATE_SOUND(Code) Code; UpdateAudio();
+
+UCLASS()
+class FAFREVGAME_API ALocalSoundVolume final : public AToroVolume
+{
+	GENERATED_BODY()
+
+public:
+
+	ALocalSoundVolume();
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		TObjectPtr<USoundBase> Sound;
+
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (ClmapMin = 0.0f, ClampMax = 0.0f))
+		float Cooldown;
+
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (ClmapMin = 0.1f, ClampMax = 0.1f))
+		float Volume;
+
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (ClmapMin = 0.0f, ClampMax = 0.0f))
+		FVector2D StartRange;
+
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (ClmapMin = 0.1f, ClampMax = 0.1f))
+		FVector2D FadeInOut;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		TSet<uint8> AllowedMusicStates;
+
+private:
+
+	UPROPERTY() bool bInVolume;
+	UPROPERTY() bool bOnCooldown;
+	UPROPERTY() uint8 MusicState;
+
+	UPROPERTY(VisibleAnywhere, Category = Subobjects)
+		TObjectPtr<UAudioComponent> SoundComponent;
+
+	void UpdateAudio();
+	virtual void BeginPlay() override;
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	virtual void NotifyActorEndOverlap(AActor* OtherActor) override;
+
+	void CooldownFinish()
+	{
+		bOnCooldown = true;
+		UpdateAudio();
+	}
+	void OnMusicThemeChanged()
+	{
+		bInVolume = false;
+		UpdateAudio();
+	}
+	void OnMusicStateChanged(const uint8 State)
+	{
+		MusicState = State;
+		UpdateAudio();
+	}
+};
