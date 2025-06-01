64be53c39472be2d2261b4d393a15c1186a1d294
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-05-31 22:37:11 -0400

    improved loading widget

--- Source/FaFRevGame/GamePhase/LoadingWidgetBase.h
+++ Source/FaFRevGame/GamePhase/LoadingWidgetBase.h
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include "Components/TextBlock.h"
+#include "Components/ProgressBar.h"
 #include "UserWidgets/ToroWidgetBase.h"
 #include "LoadingWidgetBase.generated.h"
 
@@ -12,9 +14,20 @@ class FAFREVGAME_API ULoadingWidgetBase final : public UToroWidgetBase
 
 public:
 
-	ULoadingWidgetBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-	{
-		ZOrder = 49;
-		bAutoActivate = false;
-	}
+	ULoadingWidgetBase(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = LoadingWidget, meta = (BindWidget))
+		TObjectPtr<UTextBlock> LoadText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = LoadingWidget, meta = (BindWidget))
+		TObjectPtr<UProgressBar> LoadBar;
+
+private:
+
+	UPROPERTY() float LoadPercent;
+	UPROPERTY() int32 TotalPackages;
+
+	virtual void InternalProcessActivation() override;
+	virtual void InternalProcessDeactivation() override;
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
 };
