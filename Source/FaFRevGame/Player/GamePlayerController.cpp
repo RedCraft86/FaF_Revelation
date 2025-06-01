74e883f70c1f150d868ea1e629d929231b64cd3e
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-01 19:13:23 -0400

    dip audio on pause

---
+++ Source/FaFRevGame/Player/GamePlayerController.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "GamePlayerController.h"
+#include "MusicSystem/GameMusicManager.h"
+
+bool AGamePlayerController::SetPause(bool bPause, FCanUnpause CanUnpauseDelegate)
+{
+	if (AGameMusicManager* MusicManager = AGameMusicManager::Get<AGameMusicManager>(this))
+	{
+		bPause ? MusicManager->AddDipRequest(this) : MusicManager->RemoveDipRequest(this);
+	}
+	return Super::SetPause(bPause, CanUnpauseDelegate);
+}
---
+++ Source/FaFRevGame/Player/GamePlayerController.h
@@ -0,0 +1,14 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Framework/ToroPlayerController.h"
+#include "GamePlayerController.generated.h"
+
+UCLASS()
+class FAFREVGAME_API AGamePlayerController final : public AToroPlayerController
+{
+	GENERATED_BODY()
+
+	virtual bool SetPause(bool bPause, FCanUnpause CanUnpauseDelegate = FCanUnpause()) override;
+};
