81d2700fdc0cf723b73077c48744462ccb737339
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-03 18:49:59 -0400

    inspectable

---
+++ Source/FaFRevGame/Inspection/InspectableActor.h
@@ -0,0 +1,43 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "ExitInterface.h"
+#include "Actors/ToroActor.h"
+#include "Interaction/InteractionInterface.h"
+#include "InspectableActor.generated.h"
+
+UCLASS()
+class FAFREVGAME_API AInspectableActor : public AToroActor, public IInteractionInterface, public IExitInterface
+{
+	GENERATED_BODY()
+
+public:
+
+	AInspectableActor();
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Subobjects)
+		TObjectPtr<USceneComponent> InspectRoot;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		float TurnSpeed;
+
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (AllowPreserveRatio = true))
+		FVector InspectScale;
+
+	UPROPERTY(EditAnywhere, Category = Settings, AdvancedDisplay)
+		FInteractionInfo Interaction;
+
+private:
+
+	UPROPERTY() FTransform TargetTransform;
+	UPROPERTY() FTransform CurrentTransform;
+	UPROPERTY(Transient) TObjectPtr<AGamePlayer> CachedPlayer;
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	virtual void Exit_Implementation() override;
+	virtual FInteractionInfo GetInteractionInfo_Implementation(const FHitResult& HitResult) override;
+	virtual void OnBeginInteract_Implementation(AGamePlayer* Player, const FHitResult& HitResult) override;
+};
