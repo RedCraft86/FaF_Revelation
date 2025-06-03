922bd3dab34ce4a306a72039434df83925636885
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-03 11:06:21 -0400

    field actors

---
+++ Source/FaFRevGame/Interaction/Fields/TriggerField.h
@@ -0,0 +1,56 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "InteractField.h"
+#include "Actors/ToroVolume.h"
+#include "TriggerField.generated.h"
+
+UCLASS(NotBlueprintable, BlueprintType)
+class FAFREVGAME_API ATriggerField final : public AToroVolume
+{
+	GENERATED_BODY()
+
+public:
+
+	ATriggerField()
+	{
+		ActionComponent = CreateDefaultSubobject<UWorldActionComponent>("ActionComponent");
+		ActionComponent->bManualEdFunction = true;
+
+		GetBrushComponent()->SetCollisionProfileName("Trigger");
+	}
+
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (DisplayPriority = -1))
+		bool bSingleUse;
+
+	UPROPERTY(EditAnywhere, Category = Settings, NoClear, meta = (ExcludeBaseStruct, HideViewOptions, ShowTreeView))
+		TArray<TInstancedStruct<FWorldActionBase>> Actions;
+
+	UPROPERTY(BlueprintAssignable, DisplayName = "On Triggered")
+	FGameFieldEvent OnTriggeredEvent;
+	FGameFieldEventNative OnTriggered;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, Category = Subobjects)
+		TObjectPtr<UWorldActionComponent> ActionComponent;
+
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override
+	{
+		if (!IsEnabled()) return;
+		if (AGamePlayer* Player = Cast<AGamePlayer>(OtherActor))
+		{
+			OnTriggered.Broadcast(Player);
+			OnTriggeredEvent.Broadcast(Player);
+			ActionComponent->SetActions(Actions);
+			ActionComponent->RunActions();
+			if (bSingleUse) SetEnabled(false);
+		}
+	}
+	virtual void OnConstruction(const FTransform& Transform) override
+	{
+		ActionComponent->SetActions(Actions);
+		ActionComponent->UpdateEdFunctions();
+	}
+};
