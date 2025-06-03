77c206a376173f08a31721af1cc9a324cbeeab10
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-03 09:29:18 -0400

    widget bases

---
+++ Source/FaFRevGame/Interaction/InteractionWidget.h
@@ -0,0 +1,17 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "ToroSettings.h"
+#include "UserWidgets/ToroWidgetBase.h"
+#include "InteractionWidget.generated.h"
+
+UCLASS(Abstract)
+class FAFREVGAME_API UInteractionWidget final : public UToroWidgetBase
+{
+	GENERATED_BODY()
+
+private:
+
+	virtual bool CanCreateWidget() const override { return UToroSettings::Get()->IsOnGameplayMap(this); }
+};
