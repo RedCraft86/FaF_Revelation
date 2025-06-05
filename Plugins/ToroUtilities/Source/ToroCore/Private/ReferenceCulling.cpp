cbf184a7a085f5693a62fd0b55c07d473b9013a4
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-04 22:09:05 -0400

    ref culling

---
+++ Plugins/ToroUtilities/Source/ToroCore/Private/ReferenceCulling.cpp
@@ -0,0 +1,68 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Components/ReferenceCulling.h"
+
+URefCullComponent::URefCullComponent(): bAffectTicking(false), bCachedTickState(false)
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bStartWithTickEnabled = true;
+	PrimaryComponentTick.TickGroup = TG_DuringPhysics;
+	PrimaryComponentTick.TickInterval = 2.0f;
+}
+
+URefCullComponent* URefCullComponent::Get(const AActor* Target)
+{
+	return Target ? Target->FindComponentByClass<URefCullComponent>() : nullptr;
+}
+
+void URefCullComponent::AddRenderRequest(const UObject* Object)
+{
+	if (Object && !Requests.Contains(Object))
+	{
+		Requests.Add(Object);
+		UpdateRenderingState();
+	}
+}
+
+void URefCullComponent::RemoveRenderRequest(const UObject* Object)
+{
+	if (Object && Requests.Remove(Object) > 0)
+	{
+		UpdateRenderingState();
+	}
+}
+
+void URefCullComponent::UpdateRenderingState()
+{
+	if (AActor* OwnerActor = GetOwner())
+	{
+		OwnerActor->SetActorHiddenInGame(Requests.IsEmpty());
+		if (bAffectTicking)
+		{
+			if (OwnerActor->IsHidden())
+			{
+				bCachedTickState = OwnerActor->IsActorTickEnabled();
+				OwnerActor->SetActorTickEnabled(false);
+			}
+			else
+			{
+				OwnerActor->SetActorTickEnabled(bCachedTickState);
+			}
+		}
+	}
+}
+
+void URefCullComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* TickFunc)
+{
+	Super::TickComponent(DeltaTime, TickType, TickFunc);
+	bool bChanged = false;
+	for (auto It = Requests.CreateIterator(); It; ++It)
+	{
+		if (!It->IsValid())
+		{
+			It.RemoveCurrent();
+			bChanged = true;
+		}
+	}
+	if (bChanged) UpdateRenderingState();
+}
---
+++ Plugins/ToroUtilities/Source/ToroCore/Public/Components/ReferenceCulling.h
@@ -0,0 +1,37 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "ReferenceCulling.generated.h"
+
+UCLASS(NotBlueprintable, ClassGroup = (Optimization), meta = (BlueprintSpawnableComponent))
+class TOROCORE_API URefCullComponent final : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	URefCullComponent();
+
+	static URefCullComponent* Get(const AActor* Target);
+	UFUNCTION(BlueprintPure, Category = Game, meta = (DefaultToSelf = "Target"))
+		static URefCullComponent* GetReferenceCulling(const AActor* Target) { return Get(Target); }
+
+	UPROPERTY(EditAnywhere, Category = Culling)
+		bool bAffectTicking;
+
+	UFUNCTION(BlueprintCallable, Category = Culling)
+		void AddRenderRequest(const UObject* Object);
+
+	UFUNCTION(BlueprintCallable, Category = Culling)
+		void RemoveRenderRequest(const UObject* Object);
+
+private:
+
+	UPROPERTY() bool bCachedTickState;
+	UPROPERTY(Transient) TSet<TWeakObjectPtr<const UObject>> Requests;
+
+	void UpdateRenderingState();
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* TickFunc) override;
+};
