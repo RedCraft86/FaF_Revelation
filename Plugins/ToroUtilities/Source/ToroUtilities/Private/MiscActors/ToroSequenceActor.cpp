1860c66ed9d56d87740b9cd270541470abad6813
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-09 20:38:00 -0400

    sequence actor

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/MiscActors/ToroSequenceActor.cpp
@@ -0,0 +1,74 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "MiscActors/ToroSequenceActor.h"
+#include "LevelSequencePlayer.h"
+
+void AToroSequenceActor::Play()
+{
+	GetSequencePlayer()->Play();
+	LockPlayer();
+}
+
+void AToroSequenceActor::Reverse()
+{
+	GetSequencePlayer()->PlayReverse();
+	LockPlayer();
+}
+
+void AToroSequenceActor::Stop() const
+{
+	GetSequencePlayer()->Stop();
+	UnlockPlayer();
+}
+
+void AToroSequenceActor::SkipToEnd() const
+{
+	GetSequencePlayer()->GoToEndAndStop();
+	UnlockPlayer();
+}
+
+void AToroSequenceActor::StopAtCurrentTime() const
+{
+	GetSequencePlayer()->StopAtCurrentTime();
+	UnlockPlayer();
+}
+
+void AToroSequenceActor::SetPlayRate(const float InRate) const
+{
+	GetSequencePlayer()->SetPlayRate(InRate);
+}
+
+void AToroSequenceActor::SetPlaybackPosition(FMovieSceneSequencePlaybackParams InParams) const
+{
+	GetSequencePlayer()->SetPlaybackPosition(InParams);
+}
+
+void AToroSequenceActor::OnFinished()
+{
+	UnlockPlayer();
+	OnFinishedEvent.Broadcast();
+}
+
+void AToroSequenceActor::LockPlayer()
+{
+	// AToroPlayerController* Controller = AToroPlayerController::Get(this);
+	// if (bLockPlayer && Controller)
+	// {
+	// 	Controller->EnterCinematic(this);
+	// }
+}
+
+void AToroSequenceActor::UnlockPlayer() const
+{
+	// AToroPlayerController* Controller = AToroPlayerController::Get(this);
+	// if (bLockPlayer && Controller)
+	// {
+	// 	Controller->ExitCinematic();
+	// }
+}
+
+void AToroSequenceActor::BeginPlay()
+{
+	Super::BeginPlay();
+	GetSequencePlayer()->OnNativeFinished.BindUObject(this, &AToroSequenceActor::OnFinished);
+}
