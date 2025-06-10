5f84cbc4f4ecf3a7b02ee70cde43801a5c3b027c
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-10 17:54:43 -0400

    world actions base content

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/WorldActions/WorldActionComponent.cpp
@@ -0,0 +1,76 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "WorldActions/WorldActionComponent.h"
+
+UWorldActionComponent::UWorldActionComponent(): bAutoConstruction(false)
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+}
+
+void UWorldActionComponent::SetActions(TArray<TInstancedStruct<FWorldActionBase>>& InActions, const bool bConstruct)
+{
+	ActionPtrs.Empty(InActions.Num());
+	for (TInstancedStruct<FWorldActionBase>& Action : InActions)
+	{
+		ActionPtrs.Add(Action.GetMutablePtr());
+	}
+	if (bConstruct) RunConstruction();
+}
+
+void UWorldActionComponent::AppendActions(TArray<TInstancedStruct<FWorldActionBase>>& InActions, const bool bConstruct)
+{
+	ActionPtrs.Reserve(ActionPtrs.Num() + InActions.Num());
+	for (TInstancedStruct<FWorldActionBase>& Action : InActions)
+	{
+		ActionPtrs.Add(Action.GetMutablePtr());
+	}
+	if (bConstruct) RunConstruction();
+}
+
+void UWorldActionComponent::RunActions()
+{
+	FOR_EACH_ACTION_PTR(ActionPtrs, this,
+		Ptr->RunAction();
+	)
+}
+
+void UWorldActionComponent::RunConstruction()
+{
+	bool bWantsTick = false;
+	FOR_EACH_ACTION_PTR(ActionPtrs, this,
+		if (!FApp::IsGame()) Ptr->OnConstruction();
+		if (!bWantsTick) bWantsTick = Ptr->ShouldTick();
+	)
+	PrimaryComponentTick.bStartWithTickEnabled = bWantsTick;
+}
+
+void UWorldActionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	FOR_EACH_ACTION_PTR(ActionPtrs, this,
+		Ptr->OnBeginPlay();
+	)
+}
+
+void UWorldActionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* TickFunc)
+{
+	Super::TickComponent(DeltaTime, TickType, TickFunc);
+	FOR_EACH_ACTION_PTR(ActionPtrs, this,
+		if (Ptr->ShouldTick()) Ptr->OnTick(DeltaTime);
+	)
+}
+
+#if WITH_EDITOR
+void UWorldActionComponent::PostLoad()
+{
+	Super::PostLoad();
+	if (bAutoConstruction) RunConstruction();
+}
+
+void UWorldActionComponent::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	if (bAutoConstruction) RunConstruction();
+}
+#endif
---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/WorldActions/WorldActionComponent.h
@@ -0,0 +1,42 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "WorldActionBase.h"
+#include "Components/ActorComponent.h"
+#include "WorldActionComponent.generated.h"
+
+UCLASS(NotBlueprintable, ClassGroup = (Game), meta = (BlueprintSpawnableComponent))
+class TOROUTILITIES_API UWorldActionComponent final : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	UWorldActionComponent();
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		bool bAutoConstruction;
+
+	UFUNCTION(BlueprintCallable, Category = WorldActions)
+		void SetActions(UPARAM(ref) TArray<TInstancedStruct<FWorldActionBase>>& InActions, const bool bConstruct);
+
+	UFUNCTION(BlueprintCallable, Category = WorldActions)
+		void AppendActions(UPARAM(ref) TArray<TInstancedStruct<FWorldActionBase>>& InActions, const bool bConstruct);
+
+	UFUNCTION(BlueprintCallable, Category = WorldActions)
+		void RunActions();
+
+	void RunConstruction();
+
+private:
+
+	TArray<FWorldActionBase*> ActionPtrs;
+
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* TickFunc) override;
+#if WITH_EDITOR
+	virtual void PostLoad() override;
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+};
