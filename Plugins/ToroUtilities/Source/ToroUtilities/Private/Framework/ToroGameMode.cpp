e9b3eb4f6d40298040902298f118a270e2e5f765
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-10 20:21:06 -0400

    game mode

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/Framework/ToroGameMode.cpp
@@ -0,0 +1,37 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Framework/ToroGameMode.h"
+#include "Framework/ToroGameInstance.h"
+#include "Framework/ToroMusicManager.h"
+#include "Framework/ToroPlayerCharacter.h"
+#include "Framework/ToroPlayerController.h"
+#include "Framework/ToroWidgetManager.h"
+
+AToroGameMode::AToroGameMode()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
+	PrimaryActorTick.TickGroup = TG_DuringPhysics;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SetRootComponent(SceneRoot);
+
+	HUDClass = AToroWidgetManager::StaticClass();
+	GameStateClass = AToroMusicManager::StaticClass();
+	// PlayerStateClass = AToroPlayerState::StaticClass(); No need for this yet
+	DefaultPawnClass = AToroPlayerCharacter::StaticClass();
+	PlayerControllerClass = AToroPlayerController::StaticClass();
+}
+
+void AToroGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	GameInstance = UToroGameInstance::Get(this);
+	if (GameInstance) GameInstance->OnWorldBeginPlay(GetWorld());
+}
+
+void AToroGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (GameInstance) GameInstance->OnWorldTick(DeltaTime);
+}
