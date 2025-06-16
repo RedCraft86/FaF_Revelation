ba7e4b758b6e4fa5dab97985fe95d431ff7414b1
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-15 23:04:15 -0400

    custom event actions

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/WorldActions/Actions/CustomAction.cpp
@@ -0,0 +1,44 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "WorldActions/Actions/CustomAction.h"
+
+bool FWACustomObject::ShouldTick() const
+{
+	return ActionObject && ActionObject->bCanEverTick;
+}
+
+void FWACustomObject::RunAction()
+{
+	if (ActionObject)
+	{
+		ActionObject->WorldContext = GetMutableContext();
+		ActionObject->RunAction();
+	}
+}
+
+void FWACustomObject::OnBeginPlay()
+{
+	if (ActionObject)
+	{
+		ActionObject->WorldContext = GetMutableContext();
+		ActionObject->OnBeginPlay();
+	}
+}
+
+void FWACustomObject::OnTick(const float DeltaTime)
+{
+	if (ActionObject && ActionObject->bCanEverTick)
+	{
+		ActionObject->WorldContext = GetMutableContext();
+		ActionObject->OnTick(DeltaTime);
+	}
+}
+
+void FWACustomObject::OnConstruction()
+{
+	if (ActionObject)
+	{
+		ActionObject->WorldContext = const_cast<UObject*>(GetWorldContext());
+		ActionObject->OnConstruction();
+	}
+}
