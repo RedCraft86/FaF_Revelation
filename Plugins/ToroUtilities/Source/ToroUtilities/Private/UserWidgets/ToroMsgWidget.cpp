9fab93c85d9c650e8c4ca0a7c8b3b78d881d7f75
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-18 19:36:15 -0400

    message widget

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/UserWidgets/ToroMsgWidget.cpp
@@ -0,0 +1,124 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "UserWidgets/ToroMsgWidget.h"
+#include "Framework/ToroWidgetManager.h"
+#include "Components/TextBlock.h"
+
+UToroMsgWidget::UToroMsgWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	ZOrder = 99;
+}
+
+void UToroMsgWidget::QueueNotice(const UObject* ContextObject, const FToroSimpleMsg& InNotice)
+{
+	if (AToroWidgetManager* Manager = AToroWidgetManager::Get(ContextObject))
+	{
+		if (UToroMsgWidget* Widget = Manager->FindWidget<UToroMsgWidget>())
+		{
+			Widget->AddNotice(InNotice);
+		}
+	}
+}
+
+void UToroMsgWidget::QueueTitle(const UObject* ContextObject, const FToroSimpleMsg& InTitle)
+{
+	if (AToroWidgetManager* Manager = AToroWidgetManager::Get(ContextObject))
+	{
+		if (UToroMsgWidget* Widget = Manager->FindWidget<UToroMsgWidget>())
+		{
+			Widget->AddTitle(InTitle);
+		}
+	}
+}
+
+void UToroMsgWidget::NextNotice()
+{
+	if (!Notices.IsEmpty())
+	{
+		ShowNotice(Notices[0]);
+		Notices.RemoveAt(0);
+	}
+	else
+	{
+		PlayAnimationReverse(NoticeAnim);
+	}
+}
+
+void UToroMsgWidget::ShowNotice(const FToroSimpleMsg& InData)
+{
+	NoticeText->SetText(InData.Message);
+	PlayAnimation(NoticeAnim);
+
+	GetWorld()->GetTimerManager().SetTimer(NoticeTimer, this,
+		&UToroMsgWidget::NextNotice, InData.CalcTime(), false);
+}
+
+void UToroMsgWidget::AddNotice(const FToroSimpleMsg& InNotice, const bool bImmediate)
+{
+	if (!InNotice.IsValidMessage()) return;
+	if (!Notices.IsEmpty() && Notices[Notices.Num() - 1].Message.EqualTo(InNotice.Message)) return;
+
+	if (bImmediate)
+	{
+		GetWorld()->GetTimerManager().ClearTimer(NoticeTimer);
+		ShowNotice(InNotice);
+	}
+	else
+	{
+		Notices.Add(InNotice);
+		if (!GetWorld()->GetTimerManager().IsTimerActive(NoticeTimer))
+		{
+			NextNotice();
+		}
+	}
+}
+
+void UToroMsgWidget::NextTitle()
+{
+	if (!Titles.IsEmpty())
+	{
+		ShowNotice(Titles[0]);
+		Titles.RemoveAt(0);
+	}
+	else
+	{
+		PlayAnimationReverse(ActivateAnim);
+	}
+}
+
+void UToroMsgWidget::ShowTitle(const FToroSimpleMsg& InData)
+{
+	TitleText->SetText(InData.Message);
+	PlayAnimation(ActivateAnim);
+
+	GetWorld()->GetTimerManager().SetTimer(TitleTimer, this,
+		&UToroMsgWidget::NextTitle, InData.CalcTime(), false);
+}
+
+void UToroMsgWidget::AddTitle(const FToroSimpleMsg& InTitle, const bool bImmediate)
+{
+	if (!InTitle.IsValidMessage()) return;
+	if (!Titles.IsEmpty() && Titles[Titles.Num() - 1].Message.EqualTo(InTitle.Message)) return;
+
+	if (bImmediate)
+	{
+		GetWorld()->GetTimerManager().ClearTimer(TitleTimer);
+		ShowTitle(InTitle);
+	}
+	else
+	{
+		Titles.Add(InTitle);
+		if (!GetWorld()->GetTimerManager().IsTimerActive(TitleTimer))
+		{
+			NextTitle();
+		}
+	}
+}
+
+void UToroMsgWidget::InitWidget()
+{
+	Super::InitWidget();
+	NoticeText->SetText(FText::GetEmpty());
+	TitleText->SetText(FText::GetEmpty());
+}
---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/UserWidgets/ToroMsgWidget.h
@@ -0,0 +1,71 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "ToroSettings.h"
+#include "UserWidgets/ToroWidgetBase.h"
+#include "ToroMsgWidget.generated.h"
+
+class UTextBlock;
+
+USTRUCT(BlueprintType)
+struct FToroSimpleMsg
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = SimpleMessage)
+		FText Message;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = SimpleMessage, meta = (ClampMin = 0.0f, UIMin = 0.0f))
+		float ExtraTime;
+
+	FToroSimpleMsg(): ExtraTime(0.5f) {}
+	FToroSimpleMsg(const FText& InMessage, const float InExtraTime)
+		: Message(InMessage), ExtraTime(InExtraTime)
+	{}
+
+	bool IsValidMessage() const { return Message.IsEmptyOrWhitespace() || ExtraTime < 0.0f; }
+	float CalcTime() const { return ExtraTime + UToroSettings::Get()->GetReadingTime(Message.ToString()); }
+};
+
+UCLASS(Abstract)
+class TOROUTILITIES_API UToroMsgWidget : public UToroWidgetBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UToroMsgWidget(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> NoticeText; // TODO UExprTextBlock
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> TitleText; // TODO UExprTextBlock
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Animations, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> NoticeAnim;
+
+	UFUNCTION(BlueprintCallable, Category = Message, meta = (WorldContext = "ContextObject"))
+		static void QueueNotice(const UObject* ContextObject, const FToroSimpleMsg& InNotice);
+
+	UFUNCTION(BlueprintCallable, Category = Message, meta = (WorldContext = "ContextObject"))
+		static void QueueTitle(const UObject* ContextObject, const FToroSimpleMsg& InTitle);
+
+protected:
+
+	FTimerHandle NoticeTimer;
+	FTimerHandle TitleTimer;
+	TArray<FToroSimpleMsg> Notices;
+	TArray<FToroSimpleMsg> Titles;
+	
+	void NextNotice();
+	void ShowNotice(const FToroSimpleMsg& InData);
+	void AddNotice(const FToroSimpleMsg& InNotice, const bool bImmediate = false);
+
+	void NextTitle();
+	void ShowTitle(const FToroSimpleMsg& InData);
+	void AddTitle(const FToroSimpleMsg& InTitle, const bool bImmediate = false);
+
+	virtual void InitWidget() override;
+};
