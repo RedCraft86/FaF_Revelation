413fd48f1950ad83ef0acdd2e54075fefa2f6969
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-18 19:36:49 -0400

    loading widget base

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/UserWidgets/ToroLoadingWidget.h
@@ -0,0 +1,21 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "UserWidgets/ToroWidgetBase.h"
+#include "ToroLoadingWidget.generated.h"
+
+// Meant to be subclassed in game modules!
+UCLASS(Abstract)
+class TOROUTILITIES_API UToroLoadingWidget : public UToroWidgetBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UToroLoadingWidget(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
+	{
+		ZOrder = 98;
+		bAutoActivate = false;
+	}
+};
