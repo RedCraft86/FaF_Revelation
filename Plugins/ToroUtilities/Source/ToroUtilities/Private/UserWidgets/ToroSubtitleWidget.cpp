bd559ed233024bfc0a7dc60e34ffcd41d4f53456
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-18 18:48:30 -0400

    subtitle widget

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/UserWidgets/ToroSubtitleWidget.cpp
@@ -0,0 +1,101 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "UserWidgets/ToroSubtitleWidget.h"
+#include "Framework/ToroWidgetManager.h"
+#include "Components/TextBlock.h"
+#include "SubtitleManager.h"
+
+UToroSubtitleWidget::UToroSubtitleWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer), bCaptureNativeSubtitles(false)
+{
+	ZOrder = 95;
+}
+
+void UToroSubtitleWidget::QueueSubtitle(const UObject* ContextObject, const FToroSubtitle& InSubtitle, const bool bSmartQueue)
+{
+	if (AToroWidgetManager* Manager = AToroWidgetManager::Get(ContextObject))
+	{
+		if (UToroSubtitleWidget* Widget = Manager->FindWidget<UToroSubtitleWidget>())
+		{
+			Widget->AddSubtitle(InSubtitle, bSmartQueue);
+		}
+	}
+}
+
+void UToroSubtitleWidget::QueueSubtitles(const UObject* ContextObject, const TArray<FToroSubtitle> InSubtitles, const bool bSmartQueue)
+{
+	if (AToroWidgetManager* Manager = AToroWidgetManager::Get(ContextObject))
+	{
+		if (UToroSubtitleWidget* Widget = Manager->FindWidget<UToroSubtitleWidget>())
+		{
+			Widget->AddSubtitles(InSubtitles, bSmartQueue);
+		}
+	}
+}
+
+void UToroSubtitleWidget::AddSubtitle(const FToroSubtitle& InSubtitle, const bool bSmartQueue)
+{
+	if (!InSubtitle.IsValidSubtitle()) return;
+	if (bSmartQueue && !Subtitles.IsEmpty() && Subtitles[Subtitles.Num() - 1].Line.EqualTo(InSubtitle.Line)) return;
+
+	Subtitles.Add(InSubtitle);
+	if (!GetWorld()->GetTimerManager().IsTimerActive(SubtitleTimer))
+	{
+		NextSubtitle();
+	}
+}
+
+void UToroSubtitleWidget::AddSubtitles(const TArray<FToroSubtitle> InSubtitles, const bool bSmartQueue)
+{
+	for (const FToroSubtitle& Subtitle : InSubtitles)
+	{
+		AddSubtitle(Subtitle, bSmartQueue);
+	}
+}
+
+void UToroSubtitleWidget::NextSubtitle()
+{
+	if (!Subtitles.IsEmpty())
+	{
+		ShowSubtitle(Subtitles[0]);
+		Subtitles.RemoveAt(0);
+	}
+	else
+	{
+		PlayAnimationReverse(ActivateAnim);
+	}
+}
+
+void UToroSubtitleWidget::ShowSubtitle(const FToroSubtitle& InData)
+{
+	SetSubtitleText(InData.Name, InData.Line);
+	GetWorld()->GetTimerManager().SetTimer(SubtitleTimer, this,
+		&UToroSubtitleWidget::NextSubtitle, InData.CalcTime(), false);
+}
+
+void UToroSubtitleWidget::OnNativeSubtitle(const FText& InText)
+{
+	GetWorld()->GetTimerManager().ClearTimer(SubtitleTimer);
+	ShowSubtitle({FText::GetEmpty(), InText, 0.5f});
+}
+
+void UToroSubtitleWidget::SetSubtitleText(const FText& InName, const FText& InLine)
+{
+	PlayAnimation(ActivateAnim);
+	LineText->SetText(InLine);
+	NameText->SetText(FText::Format(INVTEXT("{0}:"), InName));
+	NameText->SetVisibility(InName.IsEmptyOrWhitespace() ?
+		ESlateVisibility::Collapsed : ESlateVisibility::HitTestInvisible);
+}
+
+void UToroSubtitleWidget::InitWidget()
+{
+	Super::InitWidget();
+	LineText->SetText(FText::GetEmpty());
+	NameText->SetText(FText::GetEmpty());
+	if (bCaptureNativeSubtitles)
+	{
+		FSubtitleManager::GetSubtitleManager()->OnSetSubtitleText()
+			.AddUObject(this, &UToroSubtitleWidget::OnNativeSubtitle);
+	}
+}
---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/UserWidgets/ToroSubtitleWidget.h
@@ -0,0 +1,74 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "ToroSettings.h"
+#include "UserWidgets/ToroWidgetBase.h"
+#include "ToroSubtitleWidget.generated.h"
+
+class UTextBlock;
+
+USTRUCT(BlueprintType)
+struct FToroSubtitle
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Subtitle)
+		FText Name;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Subtitle)
+		FText Line;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Subtitle, meta = (ClampMin = 0.0f, UIMin = 0.0f))
+		float ExtraTime;
+
+	FToroSubtitle(): ExtraTime(0.5f) {}
+	FToroSubtitle(const FText& InName, const FText& InLine, const float InExtraTime)
+		: Name(InName), Line(InLine), ExtraTime(InExtraTime)
+	{}
+
+	bool IsValidSubtitle() const { return Line.IsEmptyOrWhitespace() || ExtraTime < 0.0f; }
+	float CalcTime() const { return ExtraTime + UToroSettings::Get()->GetReadingTime(Line.ToString()); }
+};
+
+UCLASS(Abstract)
+class TOROUTILITIES_API UToroSubtitleWidget : public UToroWidgetBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UToroSubtitleWidget(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> NameText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> LineText; // TODO UExprTextBlock
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		bool bCaptureNativeSubtitles;
+
+	UFUNCTION(BlueprintCallable, Category = Subtitles, meta = (WorldContext = "ContextObject"))
+		static void QueueSubtitle(const UObject* ContextObject, const FToroSubtitle& InSubtitle, const bool bSmartQueue = true);
+
+	UFUNCTION(BlueprintCallable, Category = Subtitles, meta = (WorldContext = "ContextObject"))
+		static void QueueSubtitles(const UObject* ContextObject, const TArray<FToroSubtitle> InSubtitles, const bool bSmartQueue = true);
+
+private:
+
+	FTimerHandle SubtitleTimer;
+	TArray<FToroSubtitle> Subtitles;
+
+	void NextSubtitle();
+	void ShowSubtitle(const FToroSubtitle& InData);
+	void AddSubtitle(const FToroSubtitle& InSubtitle, const bool bSmartQueue);
+	void AddSubtitles(const TArray<FToroSubtitle> InSubtitles, const bool bSmartQueue);
+
+	void OnNativeSubtitle(const FText& InText);
+	void SetSubtitleText(const FText& InName, const FText& InLine);
+
+	virtual void InitWidget() override;
+	virtual bool ShouldBeHidden() override { return Super::ShouldBeHidden(); } // TODO Hide on narrative dialogue
+	virtual bool CanCreateWidget() const override { return UToroSettings::Get()->IsOnGameplayMap(this); }
+};
