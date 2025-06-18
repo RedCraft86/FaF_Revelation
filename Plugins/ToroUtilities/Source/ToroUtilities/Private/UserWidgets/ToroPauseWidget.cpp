7fbcd6cad942f3bf83aeba94369e4a5b6a0749b9
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-18 17:39:25 -0400

    pause widget

--- Plugins/ToroUtilities/Source/ToroUtilities/Private/UserWidgets/ToroPauseWidget.cpp
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/UserWidgets/ToroPauseWidget.cpp
@@ -1,13 +1,104 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
 #include "UserWidgets/ToroPauseWidget.h"
+#include "UserWidgets/ToroSettingsWidget.h"
+#include "Framework/ToroPlayerController.h"
+#include "Framework/ToroWidgetManager.h"
+#include "Libraries/ToroGeneralUtils.h"
+#include "EnhancedCodeFlow.h"
 
 UToroPauseWidget::UToroPauseWidget(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
+	// TODO ZOrder
+	bAutoActivate = false;
+}
+
+void UToroPauseWidget::ResumeGame()
+{
+	OnGamePause(false);
+}
+
+void UToroPauseWidget::SettingsMenu()
+{
+	if (Settings)
+	{
+		DeactivateWidget();
+		Settings->ActivateWidget();
+	}
+}
+
+void UToroPauseWidget::Checkpoint()
+{
+	FadeOut([this]()
+	{
+		UToroGeneralUtils::RestartLevel(this);
+	});
+}
+
+void UToroPauseWidget::MainMenu()
+{
+	FadeOut([this]()
+	{
+		UGameplayStatics::OpenLevelBySoftObjectPtr(this, UToroSettings::Get()->LaunchMap);
+	});
+}
+
+void UToroPauseWidget::QuitGame()
+{
+	FadeOut([this]()
+	{
+		UKismetSystemLibrary::QuitGame(this, Controller,
+			EQuitPreference::Quit, false);
+	});
+}
+
+void UToroPauseWidget::OnGamePause(const bool bPaused)
+{
+	if (bPaused)
+	{
+		CachedInput = Controller->GetInputModeData();
+		Controller->SetInputModeData({EGameInputMode::UI_Only, true,
+			EMouseLockMode::LockAlways, true, this});
+
+		ActivateWidget();
+	}
+	else
+	{
+		Controller->SetInputModeData(CachedInput);
+		DeactivateWidget();
+	}
+}
+
+void UToroPauseWidget::FadeOut(const TFunction<void()>& Callback)
+{
+	PlayAnimationReverse(ActivateAnim);
+	if (Controller->PlayerCameraManager)
+	{
+		Controller->PlayerCameraManager->StartCameraFade(0.0f, 1.0f,
+			1.0f, FLinearColor::Black, true, true);
+	}
+
+	FFlow::Delay(this, 1.0f, Callback);
+}
+
+void UToroPauseWidget::InitWidget()
+{
+	Super::InitWidget();
+	Controller = AToroPlayerController::Get(this);
+	Controller->OnGamePaused.AddUObject(this, &UToroPauseWidget::OnGamePause);
+}
+
+void UToroPauseWidget::InternalProcessActivation()
+{
+	Super::InternalProcessActivation();
+	if (AToroWidgetManager* Manager = AToroWidgetManager::Get(this))
+	{
+		Settings = Manager->FindWidget<UToroSettingsWidget>();
+	}
 }
 
 void UToroPauseWidget::ReturnToWidget_Implementation(UUserWidget* FromWidget)
 {
-	SetHidden(false);
+	if (FromWidget == Settings) ActivateWidget();
 }
--- Plugins/ToroUtilities/Source/ToroUtilities/Public/UserWidgets/ToroPauseWidget.h
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/UserWidgets/ToroPauseWidget.h
@@ -3,9 +3,12 @@
 #pragma once
 
 #include "ToroSettings.h"
+#include "DataTypes/InputModeData.h"
 #include "Interfaces/ExitInterface.h"
 #include "UserWidgets/ToroWidgetBase.h"
 #include "ToroPauseWidget.generated.h"
+
+class UButton;
 
 UCLASS(Abstract)
 class TOROUTILITIES_API UToroPauseWidget : public UToroWidgetBase, public IExitInterface
@@ -16,8 +19,38 @@ public:
 
 	UToroPauseWidget(const FObjectInitializer& ObjectInitializer);
 
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> ResumeBtn;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> SettingsBtn;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> CheckpointBtn;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> MainMenuBtn;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> QuitBtn;
+
 protected:
 
+	UPROPERTY() FGameInputModeData CachedInput;
+	UPROPERTY(Transient) TObjectPtr<class UToroSettingsWidget> Settings;
+	UPROPERTY(Transient) TObjectPtr<class AToroPlayerController> Controller;
+
+	UFUNCTION() void ResumeGame();
+	UFUNCTION() void SettingsMenu();
+	UFUNCTION() void Checkpoint();
+	UFUNCTION() void MainMenu();
+	UFUNCTION() void QuitGame();
+
+	void OnGamePause(const bool bPaused);
+	void FadeOut(const TFunction<void()>& Callback);
+
+	virtual void InitWidget() override;
+	virtual void InternalProcessActivation() override;
 	virtual void ReturnToWidget_Implementation(UUserWidget* FromWidget) override;
 	virtual bool CanCreateWidget() const override { return UToroSettings::Get()->IsOnGameplayMap(this); }
 };
