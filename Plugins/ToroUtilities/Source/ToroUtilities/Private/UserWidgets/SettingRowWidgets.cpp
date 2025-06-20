ad2fc6762423ef3707be1877897ecc0f6c3b9fc7
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-19 22:07:14 -0400

    setting rows

--- Plugins/ToroUtilities/Source/ToroUtilities/Private/UserWidgets/SettingRowWidgets.cpp
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/UserWidgets/SettingRowWidgets.cpp
@@ -1,3 +1,399 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
 #include "UserWidgets/SettingRowWidgets.h"
+#include "UserWidgets/ToroWidgetBase.h"
+#include "Components/ComboBoxString.h"
+#include "Components/TextBlock.h"
+#include "Components/CheckBox.h"
+#include "Components/SpinBox.h"
+#include "Components/Button.h"
+#include "EnhancedCodeFlow.h"
+
+void USettingRowTooltip::UpdateTooltip(const USettingRowBase* SettingRow) const
+{
+	if (SettingRow)
+	{
+		DescText->SetText(SettingRow->GetToolTipText());
+		ImpactText->SetVisibility(ESlateVisibility::HitTestInvisible);
+		switch (SettingRow->Performance)
+		{
+		case ESettingPerformance::Low:
+			{
+				ImpactText->SetText(INVTEXT("Impact: Low"));
+				ImpactText->SetColorAndOpacity(FLinearColor::Green);
+			} break;
+		case ESettingPerformance::Medium:
+			{
+				ImpactText->SetText(INVTEXT("Impact: Medium"));
+				ImpactText->SetColorAndOpacity(FLinearColor::Yellow);
+			} break;
+		case ESettingPerformance::High:
+			{
+				ImpactText->SetText(INVTEXT("Impact: High"));
+				ImpactText->SetColorAndOpacity(FLinearColor::Red);
+			} break;
+		default:
+			ImpactText->SetVisibility(ESlateVisibility::Collapsed);
+		}
+	}
+}
+
+USettingRowBase::USettingRowBase(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
+	, DisplayName(INVTEXT("Setting Row")), Performance(ESettingPerformance::None), bSkipSetter(false)
+{
+	ToolTipWidgetDelegate.BindDynamic(this, &USettingRowBase::CreateTooltip);
+}
+
+void USettingRowBase::UpdateWidget()
+{
+	ResetButton->SetVisibility(IsDefaultValue() ? ESlateVisibility::Collapsed : ESlateVisibility::Visible);
+}
+
+void USettingRowBase::NativeConstruct()
+{
+	Super::NativeConstruct();
+	UserSettings = UToroUserSettings::Get();
+	ResetButton->OnClicked.AddUniqueDynamic(this, &USettingRowBase::ResetToDefault);
+	FFlow::DelayTicks(this, 1, [this]()
+	{
+		LoadValue();
+	});
+}
+
+void USettingRowBase::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	if (LabelText) LabelText->SetText(DisplayName);
+}
+
+// TOGGLE BOX
+
+bool UToggleSettingRow::GetValue() const
+{
+	return CheckBox->IsChecked();
+}
+
+void UToggleSettingRow::SetValue(const bool InValue)
+{
+	if (GetValue() != InValue)
+	{
+		CheckBox->SetCheckedState(InValue ? ECheckBoxState::Checked : ECheckBoxState::Unchecked);
+		UpdateWidget();
+		ApplyValue();
+	}
+}
+
+void UToggleSettingRow::ApplyValue()
+{
+	if (!bSkipSetter)
+	{
+		SetterFunc(GetValue());
+		OnValueChanged.Broadcast();
+	}
+}
+
+void UToggleSettingRow::NativeConstruct()
+{
+	Super::NativeConstruct();
+	CheckBox->OnCheckStateChanged.AddUniqueDynamic(this, &UToggleSettingRow::OnStateChanged);
+}
+
+void UToggleSettingRow::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	if (CheckBox) CheckBox->SetIsChecked(bDefaultValue);
+}
+
+// SLIDER BOX
+
+float USliderSettingRow::GetValue() const
+{
+	return SliderBox->GetValue();
+}
+
+void USliderSettingRow::SetValue(const float InValue)
+{
+	if (GetValue() != InValue)
+	{
+		SliderBox->SetValue(InValue);
+		UpdateWidget();
+		//ApplyValue();
+	}
+}
+
+void USliderSettingRow::ApplyValue()
+{
+	if (!bSkipSetter)
+	{
+		SetterFunc(SliderBox->GetValue());
+		OnValueChanged.Broadcast();
+	}
+}
+
+void USliderSettingRow::NativeConstruct()
+{
+	Super::NativeConstruct();
+	SliderBox->OnEndSliderMovement.AddDynamic(this, &USliderSettingRow::OnSliderEnd);
+	SliderBox->OnValueCommitted.AddDynamic(this, &USliderSettingRow::OnValueCommited);
+	if (bAlwaysUpdate) SliderBox->OnValueChanged.AddDynamic(this, &USliderSettingRow::OnSliderValueChanged);
+}
+
+void USliderSettingRow::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	if (SliderBox)
+	{
+		SliderBox->SetMinValue((float)Range.GetMin());
+		SliderBox->SetMaxValue((float)Range.GetMax());
+		SliderBox->SetMinSliderValue((float)Range.GetMin());
+		SliderBox->SetMaxSliderValue((float)Range.GetMax());
+		SliderBox->SetMaxFractionalDigits(NumOfDecimals);
+		SliderBox->SetMinFractionalDigits(NumOfDecimals > 0 ? 1 : 0);
+		SliderBox->SetDelta(NumOfDecimals > 0 ? 0.0f : 1.0f);
+		SliderBox->SetValue(DefaultValue);
+	}
+}
+
+// SWAPPER BOX
+
+void USwapperSettingRow::SetValue(const uint8 InValue)
+{
+	if (Value != InValue)
+	{
+		Value = InValue;
+		UpdateWidget();
+		ApplyValue();
+	}
+}
+
+void USwapperSettingRow::OnPrevClicked()
+{
+	if (Value > 0) SetValue(Value - 1);
+	else SetValue(Options.Num() - 1);
+}
+
+void USwapperSettingRow::OnNextClicked()
+{
+	if (Value >= Options.Num() - 1) SetValue(0);
+	else SetValue(Value + 1);
+}
+
+void USwapperSettingRow::ApplyValue()
+{
+	if (!bSkipSetter)
+	{
+		SetterFunc(Value);
+		OnValueChanged.Broadcast();
+	}
+}
+
+void USwapperSettingRow::UpdateWidget()
+{
+	Super::UpdateWidget();
+	if (!Options.IsEmpty())
+	{
+		OptionText->SetText(Options[Value]);
+	}
+}
+
+void USwapperSettingRow::NativeConstruct()
+{
+	Super::NativeConstruct();
+	PrevButton->OnClicked.AddUniqueDynamic(this, &USwapperSettingRow::OnPrevClicked);
+	NextButton->OnClicked.AddUniqueDynamic(this, &USwapperSettingRow::OnNextClicked);
+}
+
+void USwapperSettingRow::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	Value = FMath::Clamp(DefaultValue, 0, Options.Num() - 1);
+	if (OptionText) OptionText->SetText(Options.IsEmpty() ? INVTEXT("INVALID DATA") : Options[Value]);
+}
+
+// DROPDOWN BOX
+
+uint8 UDropdownSettingRow::GetValue() const
+{
+	return DropdownBox->GetSelectedIndex();
+}
+
+FString UDropdownSettingRow::GetValueStr() const
+{
+	return DropdownBox->GetSelectedOption();
+}
+
+void UDropdownSettingRow::SetValue(const uint8 InValue)
+{
+	if (Options.IsValidIndex(InValue) && GetValue() != InValue)
+	{
+		DropdownBox->SetSelectedIndex(InValue);
+		UpdateWidget();
+		//ApplyValue();
+	}
+}
+
+void UDropdownSettingRow::SetValueStr(const FString& InValue)
+{
+	if (Options.Contains(InValue) && GetValueStr() != InValue)
+	{
+		DropdownBox->SetSelectedOption(InValue);
+		UpdateWidget();
+		//ApplyValue();
+	}
+}
+
+void UDropdownSettingRow::ApplyValue()
+{
+	if (!bSkipSetter)
+	{
+		SetterFunc(DropdownBox->GetSelectedIndex());
+		OnValueChanged.Broadcast();
+	}
+}
+
+void UDropdownSettingRow::FillOptions()
+{
+	DropdownBox->ClearOptions();
+	for (const FString& Option : Options)
+	{
+		DropdownBox->AddOption(Option);
+	}
+	if (!Options.Contains(DefaultValue))
+	{
+		DefaultValue = Options.IsEmpty() ? TEXT("INVALID DATA") : Options[0];
+	}
+	DropdownBox->SetSelectedOption(DefaultValue);
+}
+
+void UDropdownSettingRow::NativeConstruct()
+{
+	Super::NativeConstruct();
+	DropdownBox->OnSelectionChanged.AddUniqueDynamic(this, &UDropdownSettingRow::OnSelection);
+}
+
+void UDropdownSettingRow::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	FillOptions();
+}
+
+// RESOLUTION BOX
+
+void UResolutionSettingRow::AcceptResolution()
+{
+	LastIdx = DropdownBox->GetSelectedIndex();
+}
+
+void UResolutionSettingRow::RevertResolution() const
+{
+	DropdownBox->SetSelectedIndex(LastIdx);
+}
+
+FIntPoint UResolutionSettingRow::GetResValue() const
+{
+	if (UToroUserSettings::SupportedResolutions.IsValidIndex(GetValue()))
+	{
+		return UToroUserSettings::SupportedResolutions[GetValue()];
+	}
+	return UToroUserSettings::FullscreenRes;
+}
+
+void UResolutionSettingRow::SetResValue(const FIntPoint& InValue)
+{
+	UToroUserSettings::CheckSupportedResolutions();
+	SetValue(UToroUserSettings::SupportedResolutions.Find(InValue));
+}
+
+void UResolutionSettingRow::LoadValue()
+{
+	bSkipSetter = true;
+	SetResValue(UserSettings->GetScreenResolution());
+	bSkipSetter = false;
+}
+
+void UResolutionSettingRow::OnSelection(FString SelectedItem, ESelectInfo::Type SelectionType)
+{
+	if (!bSkipSetter)
+	{
+		UserSettings->SetScreenResolution(GetResValue());
+		UserSettings->SetFullscreenMode(UserSettings->GetScreenResolution() == UToroUserSettings::FullscreenRes ?
+#if WITH_EDITOR
+			EWindowMode::Type::WindowedFullscreen
+#else
+			(UserSettings->GetBorderless() ? EWindowMode::Type::WindowedFullscreen : EWindowMode::Type::Fullscreen)
+#endif
+			: EWindowMode::Type::Windowed);
+		UserSettings->ApplyResolutionSettings(false);
+		OnValueChanged.Broadcast();
+	}
+}
+
+void UResolutionSettingRow::FillOptions()
+{
+	Options.Empty(1);
+	DropdownBox->ClearOptions();
+	UToroUserSettings::CheckSupportedResolutions();
+	for (const FIntPoint& Res : UToroUserSettings::SupportedResolutions)
+	{
+		const FString AsString(FString::Printf(TEXT("%dx%d"), Res.X, Res.Y));
+		DropdownBox->AddOption(AsString);
+		Options.Add(AsString);
+	}
+	if (!Options.Contains(DefaultValue))
+	{
+		DefaultValue = Options.IsEmpty() ? TEXT("1920x1080") : DropdownBox->GetOptionAtIndex(0);
+	}
+	DropdownBox->SetSelectedOption(DefaultValue);
+}
+
+// IMAGE FIDELITY BOX
+
+EImageFidelityMode UImageFidelitySettingRow::GetFidelityMode() const
+{
+	const EImageFidelityMode* Mode = ModeToName.FindKey(DropdownBox->GetSelectedOption());
+	return Mode && UToroUserSettings::SupportedFidelityModes.Contains(*Mode) ? *Mode : EImageFidelityMode::TAA;
+}
+
+void UImageFidelitySettingRow::SetFidelityMode(const EImageFidelityMode InValue)
+{
+	UToroUserSettings::CheckSupportedFidelityModes();
+	if (UToroUserSettings::SupportedFidelityModes.Contains(InValue))
+	{
+		const FString* Name = ModeToName.Find(InValue);
+		SetValueStr(Name ? *Name : ModeToName.FindRef(EImageFidelityMode::TAA));
+	}
+}
+
+void UImageFidelitySettingRow::LoadValue()
+{
+	bSkipSetter = true;
+	SetFidelityMode(UserSettings->GetImageFidelityMode());
+	bSkipSetter = false;
+}
+
+void UImageFidelitySettingRow::OnSelection(FString SelectedItem, ESelectInfo::Type SelectionType)
+{
+	if (!bSkipSetter)
+	{
+		UserSettings->SetImageFidelityMode(GetFidelityMode());
+		OnValueChanged.Broadcast();
+	}
+}
+
+void UImageFidelitySettingRow::FillOptions()
+{
+	Options.Empty(4);
+	DropdownBox->ClearOptions();
+	UToroUserSettings::CheckSupportedFidelityModes();
+	for (const EImageFidelityMode& Mode : UToroUserSettings::SupportedFidelityModes)
+	{
+		const FString AsString(ModeToName.FindRef(Mode));
+		DropdownBox->AddOption(AsString);
+		Options.Add(AsString);
+	}
+	if (!Options.Contains(DefaultValue))
+	{
+		DefaultValue = ModeToName.FindRef(EImageFidelityMode::TAA);
+	}
+	DropdownBox->SetSelectedOption(DefaultValue);
+}
--- Plugins/ToroUtilities/Source/ToroUtilities/Public/UserWidgets/SettingRowWidgets.h
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/UserWidgets/SettingRowWidgets.h
@@ -3,10 +3,359 @@
 #pragma once
 
 #include "Blueprint/UserWidget.h"
+#include "UserSettings/ToroUserSettings.h"
+#include "UserSettings/UserSettingTypes.h"
 #include "SettingRowWidgets.generated.h"
 
-UCLASS()
-class TOROUTILITIES_API UToroSettingRow : public UUserWidget
+class UButton;
+class USpinBox;
+class UCheckBox;
+class UTextBlock;
+
+#define ADD_DEFAULT_FUNCTIONS(Type) \
+	virtual void AssignGetter(const TFunction<Type()>& Func) { if (Func) GetterFunc = Func; } \
+	virtual void AssignSetter(const TFunction<void(const Type)>& Func) { if (Func) SetterFunc = Func; } \
+	virtual void LoadValue() override { \
+		bSkipSetter = true; \
+		SetValue(GetterFunc()); \
+		bSkipSetter = false; \
+		Super::LoadValue(); \
+	}
+
+#define ADD_DEFAULT_PROPERTIES(Type) \
+	TFunction<Type()> GetterFunc; \
+	TFunction<void(const Type)> SetterFunc;
+
+UCLASS(Abstract)
+class TOROUTILITIES_API USettingRowTooltip : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+
+	USettingRowTooltip(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer) {}
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> DescText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> ImpactText;
+
+	void UpdateTooltip(const class USettingRowBase* SettingRow) const;
+};
+
+UCLASS(Abstract)
+class TOROUTILITIES_API USettingRowBase : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	USettingRowBase(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> LabelText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> ResetButton;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		FText DisplayName;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		FText Description;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		ESettingPerformance Performance;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		EImageFidelityMode OnlyShowWhen;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		TSubclassOf<USettingRowTooltip> TooltipClass;
+
+	DECLARE_MULTICAST_DELEGATE(FOnValueChanged);
+	FOnValueChanged OnValueChanged;
+
+	virtual void LoadValue()
+	{
+		if (OnlyShowWhen != EImageFidelityMode::None)
+		{
+			SetVisibility(UserSettings->GetImageFidelityMode() == OnlyShowWhen
+				? ESlateVisibility::Visible : ESlateVisibility::Collapsed);
+		}
+	}
+
+protected:
+
+	UPROPERTY() bool bSkipSetter;
+	UPROPERTY(Transient) TObjectPtr<USettingRowTooltip> Tooltip;
+	UPROPERTY(Transient) TObjectPtr<UToroUserSettings> UserSettings;
+	
+	UFUNCTION() virtual void ResetToDefault() {}
+	UFUNCTION() virtual UWidget* CreateTooltip()
+	{
+		if (!Tooltip) Tooltip = CreateWidget<USettingRowTooltip>(this, TooltipClass);
+		Tooltip->UpdateTooltip(this);
+		return Tooltip;
+	}
+
+	virtual void ApplyValue() {}
+	virtual void UpdateWidget();
+	virtual bool IsDefaultValue() { return false; }
+
+	virtual void NativeConstruct() override;
+	virtual void NativePreConstruct() override;
+	virtual void SynchronizeProperties() override
+	{
+		if (!ToolTipWidgetDelegate.IsBound()) ToolTipWidgetDelegate.BindDynamic(this, &USettingRowBase::CreateTooltip);
+		Super::SynchronizeProperties();
+	}
+};
+
+UCLASS(Abstract)
+class TOROUTILITIES_API UToggleSettingRow : public USettingRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UToggleSettingRow(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer), bDefaultValue(false) {}
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UCheckBox> CheckBox;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		bool bDefaultValue;
+
+	bool GetValue() const;
+	void SetValue(const bool InValue);
+
+	ADD_DEFAULT_FUNCTIONS(bool)
+	
+protected:
+
+	ADD_DEFAULT_PROPERTIES(bool);
+
+	UFUNCTION() void OnStateChanged(bool bIsChecked) { ApplyValue(); }
+
+	virtual void ApplyValue() override;
+	virtual void ResetToDefault() override { SetValue(bDefaultValue); }
+	virtual bool IsDefaultValue() override { return GetValue() == bDefaultValue; }
+	
+	virtual void NativeConstruct() override;
+	virtual void NativePreConstruct() override;
+};
+
+UCLASS(Abstract)
+class TOROUTILITIES_API USliderSettingRow : public USettingRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	USliderSettingRow(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
+		, DefaultValue(0.0f), Range(0.0f, 100.0f), NumOfDecimals(0), bAlwaysUpdate(false)
+	{}
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<USpinBox> SliderBox;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		float DefaultValue;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		FVector2D Range;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		uint8 NumOfDecimals;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		bool bAlwaysUpdate;
+
+	float GetValue() const;
+	void SetValue(const float InValue);
+
+	ADD_DEFAULT_FUNCTIONS(float)
+	
+protected:
+
+	ADD_DEFAULT_PROPERTIES(float);
+
+	UFUNCTION() void OnSliderEnd(float Value) { ApplyValue(); }
+	UFUNCTION() void OnSliderValueChanged(float Value) { ApplyValue(); }
+	UFUNCTION() void OnValueCommited(float Value, ETextCommit::Type Type) { ApplyValue(); }
+
+	virtual void ApplyValue() override;
+	virtual void ResetToDefault() override { SetValue(DefaultValue); }
+	virtual bool IsDefaultValue() override { return FMath::IsNearlyEqual(GetValue(), DefaultValue); }
+
+	virtual void NativeConstruct() override;
+	virtual void NativePreConstruct() override;
+};
+
+UCLASS(Abstract)
+class TOROUTILITIES_API USwapperSettingRow : public USettingRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	USwapperSettingRow(const FObjectInitializer& ObjectInitializer)
+		: Super(ObjectInitializer), DefaultValue(0), Value(0)
+	{}
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> OptionText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> PrevButton;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> NextButton;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		TArray<FText> Options;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		uint8 DefaultValue;
+
+	void SetValue(const uint8 InValue);
+	uint8 GetValue() const { return Value; }
+
+	ADD_DEFAULT_FUNCTIONS(uint8)
+	
+protected:
+
+	UPROPERTY() uint8 Value;
+	ADD_DEFAULT_PROPERTIES(uint8);
+
+	UFUNCTION() void OnPrevClicked();
+	UFUNCTION() void OnNextClicked();
+
+	virtual void ApplyValue() override;
+	virtual void UpdateWidget() override;
+	virtual void ResetToDefault() override { SetValue(DefaultValue); }
+	virtual bool IsDefaultValue() override { return GetValue() == DefaultValue; }
+	
+	virtual void NativeConstruct() override;
+	virtual void NativePreConstruct() override;
+};
+
+UCLASS(Abstract)
+class TOROUTILITIES_API UDropdownSettingRow : public USettingRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UDropdownSettingRow(const FObjectInitializer& ObjectInitializer)
+		: Super(ObjectInitializer), DefaultValue(TEXT(""))
+	{}
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<class UComboBoxString> DropdownBox;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		TArray<FString> Options;
+
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (GetOptions = Options))
+		FString DefaultValue;
+
+	uint8 GetValue() const;
+	FString GetValueStr() const;
+	void SetValue(const uint8 InValue);
+	void SetValueStr(const FString& InValue);
+
+	ADD_DEFAULT_FUNCTIONS(uint8)
+	
+protected:
+
+	ADD_DEFAULT_PROPERTIES(uint8);
+
+	UFUNCTION() virtual void OnSelection(FString SelectedItem, ESelectInfo::Type SelectionType) { ApplyValue(); }
+
+	virtual void ApplyValue() override;
+	virtual void ResetToDefault() override { SetValueStr(DefaultValue); }
+	virtual bool IsDefaultValue() override { return GetValueStr() == DefaultValue; }
+	
+	virtual void FillOptions();
+	virtual void NativeConstruct() override;
+	virtual void NativePreConstruct() override;
+};
+
+UCLASS(Abstract)
+class TOROUTILITIES_API UResolutionSettingRow : public UDropdownSettingRow
+{
+	GENERATED_BODY()
+
+public:
+
+	UResolutionSettingRow(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer), LastIdx(0) {}
+
+	void AcceptResolution();
+	void RevertResolution() const;
+	
+	FIntPoint GetResValue() const;
+	void SetResValue(const FIntPoint& InValue);
+	virtual void LoadValue() override;
+
+protected:
+
+	UPROPERTY() uint8 LastIdx;
+
+	virtual void AssignGetter(const TFunction<uint8()>& Function) override { }
+	virtual void AssignSetter(const TFunction<void(const uint8)>& Function) override { }
+	virtual void OnSelection(FString SelectedItem, ESelectInfo::Type SelectionType) override;
+	virtual void FillOptions() override;
+#if WITH_EDITOR
+	virtual bool CanEditChange(const FProperty* InProperty) const override
+	{
+		if (InProperty && InProperty->GetFName() == GET_MEMBER_NAME_CHECKED(UResolutionSettingRow, Options))
+		{
+			return false;
+		}
+		return Super::CanEditChange(InProperty);
+	}
+#endif
+};
+
+UCLASS(Abstract)
+class TOROUTILITIES_API UImageFidelitySettingRow : public UDropdownSettingRow
+{
+	GENERATED_BODY()
+
+public:
+
+	UImageFidelitySettingRow(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer) {}
+
+	EImageFidelityMode GetFidelityMode() const;
+	void SetFidelityMode(const EImageFidelityMode InValue);
+	virtual void LoadValue() override;
+
+protected:
+
+	static inline TMap<EImageFidelityMode, FString> ModeToName = {
+		{EImageFidelityMode::None, TEXT("None")},
+		{EImageFidelityMode::FXAA, TEXT("Fast Approximate AA")},
+		{EImageFidelityMode::TAA, TEXT("Temporal AA")},
+		{EImageFidelityMode::TSR, TEXT("Temporal Super Resolution")},
+		{EImageFidelityMode::FSR, TEXT("FidelityFX Super Resolution 3.1")},
+		{EImageFidelityMode::XeSS, TEXT("Xe Super Sampling")}
+	};
+
+	virtual void AssignGetter(const TFunction<uint8()>& Function) override { }
+	virtual void AssignSetter(const TFunction<void(const uint8)>& Function) override { }
+	virtual void OnSelection(FString SelectedItem, ESelectInfo::Type SelectionType) override;
+	virtual void FillOptions() override;
+#if WITH_EDITOR
+	virtual bool CanEditChange(const FProperty* InProperty) const override
+	{
+		if (InProperty && InProperty->GetFName() == GET_MEMBER_NAME_CHECKED(UImageFidelitySettingRow, Options))
+		{
+			return false;
+		}
+		return Super::CanEditChange(InProperty);
+	}
+#endif
 };
