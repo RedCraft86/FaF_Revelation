be5fa97ad5f0622a8ff3d886424ee12757e2af86
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-20 15:45:02 -0400

    user dialog

---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/UserWidgets/UserDialogWidget.cpp
@@ -0,0 +1,66 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "UserWidgets/UserDialogWidget.h"
+#include "Framework/ToroWidgetManager.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+
+UUserDialogWidget::UUserDialogWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	ZOrder = 98;
+	bAutoActivate = false;
+}
+
+void UUserDialogWidget::ShowDialog(const UObject* ContextObject, const FText& InTitle, const FText& InMessage,
+	const FText& InButtonA, const FText& InButtonB, const TFunction<void(uint8)>& Callback)
+{
+	if (InTitle.IsEmptyOrWhitespace() || InMessage.IsEmptyOrWhitespace()
+		|| InButtonA.IsEmptyOrWhitespace() || !Callback) return;
+
+	if (AToroWidgetManager* Manager = AToroWidgetManager::Get(ContextObject))
+	{
+		if (UUserDialogWidget* Widget = Manager->FindWidget<UUserDialogWidget>())
+		{
+			Widget->ActivateWidget();
+			Widget->InitData(InTitle, InMessage, InButtonA, InButtonB, Callback);
+		}
+	}
+}
+
+void UUserDialogWidget::InitData(const FText& InTitle, const FText& InMessage,
+	const FText& InButtonA, const FText& InButtonB, const TFunction<void(uint8)>& Callback)
+{
+	TitleText->SetText(InTitle);
+	MsgText->SetText(InMessage);
+	OptionAText->SetText(InButtonA);
+	OptionBText->SetText(InButtonB);
+	OptionBBtn->SetVisibility(InButtonB.IsEmptyOrWhitespace()
+		? ESlateVisibility::Collapsed : ESlateVisibility::Visible);
+
+	CallbackFunc = Callback;
+}
+
+void UUserDialogWidget::ButtonAEvent()
+{
+	if (CallbackFunc) CallbackFunc(0);
+	DeactivateWidget();
+}
+
+void UUserDialogWidget::ButtonBEvent()
+{
+	if (CallbackFunc) CallbackFunc(1);
+	DeactivateWidget();
+}
+
+void UUserDialogWidget::InitWidget()
+{
+	OptionABtn->OnClicked.AddDynamic(this, &UUserDialogWidget::ButtonAEvent);
+	OptionBBtn->OnClicked.AddDynamic(this, &UUserDialogWidget::ButtonBEvent);
+}
+
+void UUserDialogWidget::InternalProcessDeactivation()
+{
+	CallbackFunc = nullptr;
+	Super::InternalProcessDeactivation();
+}
---
+++ Plugins/ToroUtilities/Source/ToroUtilities/Public/UserWidgets/UserDialogWidget.h
@@ -0,0 +1,53 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "UserWidgets/ToroWidgetBase.h"
+#include "UserDialogWidget.generated.h"
+
+class UButton;
+class UTextBlock;
+
+UCLASS(Abstract)
+class TOROUTILITIES_API UUserDialogWidget final : public UToroWidgetBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UUserDialogWidget(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> TitleText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> MsgText; // TODO UExprTextBlock
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> OptionABtn;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> OptionAText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> OptionBBtn;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> OptionBText;
+
+	static void ShowDialog(const UObject* ContextObject, const FText& InTitle, const FText& InMessage,
+		const FText& InButtonA, const FText& InButtonB, const TFunction<void(uint8)>& Callback);
+
+private:
+
+	TFunction<void(uint8)> CallbackFunc;
+
+	void InitData(const FText& InTitle, const FText& InMessage, const FText& InButtonA,
+		const FText& InButtonB, const TFunction<void(uint8)>& Callback);
+
+	UFUNCTION() void ButtonAEvent();
+	UFUNCTION() void ButtonBEvent();
+
+	virtual void InitWidget() override;
+	virtual void InternalProcessDeactivation() override;
+};
