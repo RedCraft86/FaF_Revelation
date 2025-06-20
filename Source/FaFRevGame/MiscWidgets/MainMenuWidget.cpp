f24af13a3cc3ac3c52625e451763e69a8cd5f3b7
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-20 18:30:48 -0400

    main menu impl

--- Source/FaFRevGame/MiscWidgets/MainMenuWidget.cpp
+++ Source/FaFRevGame/MiscWidgets/MainMenuWidget.cpp
@@ -1,9 +1,100 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
 #include "MainMenuWidget.h"
+#include "UserWidgets/SettingsWidget.h"
+#include "Framework/ToroPlayerController.h"
+#include "Framework/ToroWidgetManager.h"
+#include "EnhancedCodeFlow.h"
+#include "ExtrasWidget.h"
+#include "PhaseWidget.h"
+#include "Components/Button.h"
 
 UMainMenuWidget::UMainMenuWidget(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 	ZOrder = 96;
+	bAutoActivate = false;
+}
+
+void UMainMenuWidget::PlayGame()
+{
+	FadeOut([this]()
+	{
+		UGameplayStatics::OpenLevelBySoftObjectPtr(this, UToroSettings::Get()->GameplayMap);
+	});
+}
+
+void UMainMenuWidget::PhaseMenu()
+{
+	if (Phases)
+	{
+		DeactivateWidget();
+		Phases->ActivateWidget();
+	}
+}
+
+void UMainMenuWidget::SettingsMenu()
+{
+	if (Settings)
+	{
+		DeactivateWidget();
+		Settings->ActivateWidget();
+	}
+}
+
+void UMainMenuWidget::ExtrasMenu()
+{
+	if (Extras)
+	{
+		DeactivateWidget();
+		Extras->ActivateWidget();
+	}
+}
+
+void UMainMenuWidget::QuitGame()
+{
+	FadeOut([this]()
+	{
+		UKismetSystemLibrary::QuitGame(this, nullptr,
+			EQuitPreference::Quit, false);
+	});
+}
+
+void UMainMenuWidget::FadeOut(const TFunction<void()>& Callback)
+{
+	PlayAnimationReverse(ActivateAnim);
+	if (Controller->PlayerCameraManager)
+	{
+		Controller->PlayerCameraManager->StartCameraFade(0.0f, 1.0f,
+			1.0f, FLinearColor::Black, true, true);
+	}
+
+	FFlow::Delay(this, 1.0f, Callback);
+}
+
+void UMainMenuWidget::InitWidget()
+{
+	Super::InitWidget();
+	Controller = AToroPlayerController::Get(this);
+	PlayBtn->OnClicked.AddDynamic(this, &UMainMenuWidget::PlayGame);
+	PhaseBtn->OnClicked.AddDynamic(this, &UMainMenuWidget::PhaseMenu);
+	SettingsBtn->OnClicked.AddDynamic(this, &UMainMenuWidget::SettingsMenu);
+	ExtrasBtn->OnClicked.AddDynamic(this, &UMainMenuWidget::ExtrasMenu);
+	QuitBtn->OnClicked.AddDynamic(this, &UMainMenuWidget::QuitGame);
+}
+
+void UMainMenuWidget::InternalProcessActivation()
+{
+	Super::InternalProcessActivation();
+	if (AToroWidgetManager* Manager = AToroWidgetManager::Get(this))
+	{
+		Phases = Manager->FindWidget<UPhaseWidget>();
+		Extras = Manager->FindWidget<UExtrasWidget>();
+		Settings = Manager->FindWidget<USettingsWidget>();
+	}
+}
+
+void UMainMenuWidget::ReturnToWidget_Implementation(UUserWidget* FromWidget)
+{
+	if (FromWidget == Phases || FromWidget == Extras || FromWidget == Settings) ActivateWidget();
 }
--- Source/FaFRevGame/MiscWidgets/MainMenuWidget.h
+++ Source/FaFRevGame/MiscWidgets/MainMenuWidget.h
@@ -3,11 +3,14 @@
 #pragma once
 
 #include "ToroSettings.h"
+#include "Interfaces/ExitInterface.h"
 #include "UserWidgets/ToroWidgetBase.h"
 #include "MainMenuWidget.generated.h"
 
+class UButton;
+
 UCLASS(Abstract)
-class FAFREVGAME_API UMainMenuWidget final : public UToroWidgetBase
+class FAFREVGAME_API UMainMenuWidget final : public UToroWidgetBase, public IExitInterface
 {
 	GENERATED_BODY()
 
@@ -15,7 +18,38 @@ public:
 
 	UMainMenuWidget(const FObjectInitializer& ObjectInitializer);
 
-private: // TODO
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> PlayBtn;
 
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> PhaseBtn;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> SettingsBtn;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> ExtrasBtn;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> QuitBtn;
+
+private:
+
+	UPROPERTY(Transient) TObjectPtr<class UPhaseWidget> Phases;
+	UPROPERTY(Transient) TObjectPtr<class UExtrasWidget> Extras;
+	UPROPERTY(Transient) TObjectPtr<class USettingsWidget> Settings;
+	UPROPERTY(Transient) TObjectPtr<class AToroPlayerController> Controller;
+
+	UFUNCTION() void PlayGame();
+	UFUNCTION() void PhaseMenu();
+	UFUNCTION() void SettingsMenu();
+	UFUNCTION() void ExtrasMenu();
+	UFUNCTION() void QuitGame();
+
+	void FadeOut(const TFunction<void()>& Callback);
+
+	virtual void InitWidget() override;
+	virtual void InternalProcessActivation() override;
+	virtual void ReturnToWidget_Implementation(UUserWidget* FromWidget) override;
 	virtual bool CanCreateWidget() const override { return UToroSettings::Get()->IsOnLaunchMap(this); }
 };
