70f74e46735e98409f7304f279e299787c51a9e9
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-22 21:01:13 -0400

    runtime graph classes

---
+++ Plugins/ToroUtilities/Source/ToroGraph/Public/ToroDataEdge.h
@@ -0,0 +1,50 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ToroDataEdge.generated.h"
+
+class UToroDataNode;
+class UToroDataGraph;
+
+UCLASS(NotBlueprintable, BlueprintType)
+class TOROGRAPH_API UToroDataEdge : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UToroDataEdge(): EdgeID(FGuid::NewGuid()), Name(INVTEXT("Generic Data Edge"))
+		, bDrawTitle(false), EdgeColor(0.9f, 0.9f, 0.9f, 1.0f)
+	{}
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Edge)
+		FGuid EdgeID;
+
+	UPROPERTY(EditDefaultsOnly, Category = Edge, AdvancedDisplay)
+		FText Name;
+
+#if WITH_EDITORONLY_DATA
+	UPROPERTY(EditDefaultsOnly, Category = Edge, AdvancedDisplay, DisplayName = "Show Title")
+		bool bDrawTitle;
+
+	UPROPERTY(EditDefaultsOnly, Category = Edge, AdvancedDisplay)
+		FLinearColor EdgeColor;
+#endif
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Edge, AdvancedDisplay)
+		TObjectPtr<UToroDataGraph> OwningGraph;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Edge, AdvancedDisplay, meta = (DisplayThumbnail = false))
+		TObjectPtr<UToroDataNode> StartNode;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Edge, AdvancedDisplay, meta = (DisplayThumbnail = false))
+		TObjectPtr<UToroDataNode> EndNode;
+
+#if WITH_EDITOR
+	virtual FText GetNodeTitle() const { return Name; }
+	virtual void SetNodeTitle(const FText& InTitle) { Name = InTitle; }
+#endif
+};
--- Plugins/ToroUtilities/Source/ToroGraph/ToroGraph.Build.cs
+++ Plugins/ToroUtilities/Source/ToroGraph/ToroGraph.Build.cs
@@ -14,6 +14,7 @@ public class ToroGraph : ModuleRules
                 "Core",
                 "CoreUObject",
                 "Engine",
+                "ToroCore"
             }
         );
     }
