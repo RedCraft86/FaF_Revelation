906b064387b2d05d47cd65fcb1ab248bb951ebd1
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-24 21:02:30 -0400

    duration getter

--- Plugins/ToroUtilities/Source/ToroCore/Public/UserWidgets/ToroWidgetBase.h
+++ Plugins/ToroUtilities/Source/ToroCore/Public/UserWidgets/ToroWidgetBase.h
@@ -3,12 +3,15 @@
 #pragma once
 
 #include "CommonActivatableWidget.h"
+#include "Animation/WidgetAnimation.h"
 #include "ToroWidgetBase.generated.h"
 
 #define InitAnim(Anim){ \
 		PlayAnimation(Anim); \
 		StopAnimation(Anim); \
 	}
+
+#define GetAnimDuration(Anim) (Anim->GetEndTime() - Anim->GetStartTime())
 
 UCLASS(Abstract)
 class TOROCORE_API UToroWidgetBase : public UCommonActivatableWidget
