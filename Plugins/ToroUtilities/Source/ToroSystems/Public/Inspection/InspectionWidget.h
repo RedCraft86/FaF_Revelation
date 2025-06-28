bd840daf2f2aea6d9ca53412b2dec6adeeda63e8
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-28 19:00:10 -0400

    inspection widget

--- Plugins/ToroUtilities/Source/ToroSystems/Public/Inspection/InspectionWidget.h
+++ Plugins/ToroUtilities/Source/ToroSystems/Public/Inspection/InspectionWidget.h
@@ -2,6 +2,11 @@
 
 #pragma once
 
+#include "ToroUtilities.h"
+#include "Components/TextBlock.h"
+#include "DataTypes/InputModeData.h"
+#include "Inventory/InventoryAsset.h"
+#include "UserWidgets/ExprTextBlock.h"
 #include "UserWidgets/ToroWidgetBase.h"
 #include "InspectionWidget.generated.h"
 
@@ -10,7 +15,39 @@ class TOROSYSTEMS_API UInspectionWidget final : public UToroWidgetBase
 {
 	GENERATED_BODY()
 
+	friend class UInspectionComponent;
+
 public:
 
 	UInspectionWidget(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> TitleText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UExprTextBlock> DescText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UExprTextBlock> ContentText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> SecretText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Animations, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> SecretAnim;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+	TObjectPtr<USoundBase> SecretSound;
+
+private:
+
+	UPROPERTY() bool bSecretFound;
+	UPROPERTY() FGameInputModeData CachedInput;
+
+	void MarkSecretFound();
+	void LoadData(const UInventoryAsset* InArchive, const bool bSecretKnown);
+
+	virtual void InternalProcessActivation() override;
+	virtual void InternalProcessDeactivation() override;
+	virtual bool CanCreateWidget() const override { return UToroUtilSettings::Get()->IsOnGameplayMap(this); }
 };
