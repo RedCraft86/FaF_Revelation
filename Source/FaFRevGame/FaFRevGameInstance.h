a51f1a775a75992c81b5d261cd3850449fcd428e
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-29 22:58:00 -0400

    new game inst

---
+++ Source/FaFRevGame/FaFRevGameInstance.h
@@ -0,0 +1,16 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Framework/ToroGameInstance.h"
+#include "FaFRevGameInstance.generated.h"
+
+UCLASS()
+class FAFREVGAME_API UFaFRevGameInstance final : public UToroGameInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY() TOptional<FGuid> PlayFromPhase;
+};
