b729022af3a0a5b3b4cb9baa86d8ae280fdac146
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-06-30 22:04:27 -0400

    phase widget

--- Source/FaFRevGame/UserWidgets/PhaseWidget.cpp
+++ Source/FaFRevGame/UserWidgets/PhaseWidget.cpp
@@ -1,10 +1,162 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
 #include "PhaseWidget.h"
+#include "FaFRevSaves.h"
+#include "FaFRevSettings.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Interfaces/ExitInterface.h"
+#include "Libraries/ToroShortcutLibrary.h"
+#include "SaveSystem/ToroSaveManager.h"
+#include "UserWidgets/ExprTextBlock.h"
+#include "FaFRevGameInstance.h"
+#include "EnhancedCodeFlow.h"
 
-UPhaseWidget::UPhaseWidget(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
+UGamePhaseGraph* UPhaseNodeWidget::GetGraph()
 {
-	ZOrder = 96;
-	bAutoActivate = false;
+	if (PhaseGraph) return PhaseGraph;
+	PhaseGraph = UFaFRevSettings::Get()->PhaseGraph.LoadSynchronous();
+	return PhaseGraph;
+}
+
+void UPhaseNodeWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	if (const UFaFRevGlobalSave* Save = UToroSaveManager::GetSaveObject<UFaFRevGlobalSave>(this, SaveTags::TAG_Global))
+	{
+		SetIsEnabled(Save->PhaseNodes.Contains(PhaseID));
+	}
+	else
+	{
+		SetIsEnabled(false);
+	}
+}
+
+void UPhaseNodeWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	const UGamePhaseGraph* Graph = GetGraph();
+	if (Graph && NameText)
+	{
+		if (const UToroDataNode* Node = Graph->GetNodeByID(PhaseID))
+		{
+			NameText->SetText(Node->Name);
+		}
+	}
+}
+
+FReply UPhaseNodeWidget::NativeOnMouseButtonDown(const FGeometry& Geometry, const FPointerEvent& MouseEvent)
+{
+	if (GetIsEnabled() && MouseEvent.IsMouseButtonDown(EKeys::LeftMouseButton))
+	{
+		ParentWidget->SelectPhase(PhaseID);
+		return FReply::Handled();
+	}
+
+	return FReply::Unhandled();
+}
+
+#if WITH_EDITOR
+TArray<FString> UPhaseNodeWidget::GetPhaseNames()
+{
+	TArray<FString> Result;
+	if (UGamePhaseGraph* Graph = GetGraph())
+	{
+		for (const UToroDataNode* Node : Graph->AllNodes)
+		{
+			Result.Add(Node->Name.ToString());
+		}
+	}
+	return Result;
+}
+
+void UPhaseNodeWidget::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	if (PropertyChangedEvent.GetPropertyName() == GET_MEMBER_NAME_CHECKED(ThisClass, PhaseName))
+	{
+		if (UGamePhaseGraph* Graph = GetGraph())
+		{
+			PhaseID = {};
+			for (const UToroDataNode* Node : Graph->AllNodes)
+			{
+				if (Node && Node->Name.ToString().Equals(PhaseName))
+				{
+					PhaseID = Node->NodeID;
+					break;
+				}
+			}
+		}
+	}
+}
+#endif
+
+void UPhaseWidget::SelectPhase(const FGuid& InPhaseID)
+{
+	if (InPhaseID.IsValid() && SelectedPhase != InPhaseID)
+	{
+		SelectedPhase = InPhaseID;
+		PlayButton->SetIsEnabled(true);
+		if (const UGamePhaseGraph* Graph = GetGraph())
+		{
+			if (const UToroDataNode* Node = Graph->GetNodeByID(SelectedPhase))
+			{
+				NameText->SetText(Node->Name);
+				DescText->SetText(Node->GetNodeDescription());
+			}
+		}
+	
+		const TArray<UPhaseNodeWidget*>& Widgets = GetNodeWidgets();
+		for (UPhaseNodeWidget* Widget : Widgets)
+		{
+			if (Widget) Widget->MarkSelected(Widget->PhaseID == SelectedPhase);
+		}
+
+		PlayAnimation(InfoAnim);
+	}
+}
+
+void UPhaseWidget::OnPlayClicked()
+{
+	if (SelectedPhase.IsValid())
+	{
+		PlayAnimationReverse(ActivateAnim);
+		SetVisibility(ESlateVisibility::HitTestInvisible);
+		UToroShortcutLibrary::StartCameraFade(this, 0.0f);
+		FFlow::Delay(this, 1.0f, [this]()
+		{
+			if (UFaFRevGameInstance* GI = GetGameInstance<UFaFRevGameInstance>())
+			{
+				GI->PlayFromPhase = SelectedPhase;
+				UGameplayStatics::OpenLevelBySoftObjectPtr(this, UToroUtilSettings::Get()->GameplayMap);
+			}
+		});
+	}
+}
+
+UGamePhaseGraph* UPhaseWidget::GetGraph()
+{
+	if (PhaseGraph) return PhaseGraph;
+	PhaseGraph = UFaFRevSettings::Get()->PhaseGraph.LoadSynchronous();
+	return PhaseGraph;
+}
+
+void UPhaseWidget::InitWidget()
+{
+	Super::InitWidget();
+	PlayButton->SetIsEnabled(false);
+	PlayButton->OnClicked.AddDynamic(this, &UPhaseWidget::OnPlayClicked);
+	ExitButton->OnClicked.AddDynamic(this, &UPhaseWidget::OnExitClicked);
+	
+	const TArray<UPhaseNodeWidget*>& Widgets = GetNodeWidgets();
+	for (UPhaseNodeWidget* Widget : Widgets)
+	{
+		if (Widget) Widget->ParentWidget = this;
+	}
+}
+
+void UPhaseWidget::InternalProcessDeactivation()
+{
+	Super::InternalProcessDeactivation();
+	IExitInterface::ReturnToWidget(ParentUI, this);
 }
