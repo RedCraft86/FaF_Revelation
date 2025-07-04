cf4ec8714e374f7c79cf29a2818f7bfcb1ee77c9
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-07-04 18:51:56 -0400

    vision cone

---
+++ Plugins/ToroUtilities/Source/ToroCore/Private/Components/VisionConeComponent.cpp
@@ -0,0 +1,40 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Components/VisionConeComponent.h"
+
+UVisionConeComponent::UVisionConeComponent(): Distance(2000.0f), OuterAngle(50.0f), InnerAngle(10.0f)
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	TraceMethod = CreateDefaultSubobject<UVisionTrace_Origin>("TraceMethod");
+}
+
+bool UVisionConeComponent::GetTraceToActor(const AActor* InActor) const
+{
+	return InActor && (!TraceMethod || TraceMethod->TraceActor(this, InActor));
+}
+
+float UVisionConeComponent::GetAngleToActor(const AActor* InActor) const
+{
+	if (!InActor) return -1.0f;
+	const FVector DotA = InActor->GetActorLocation() - GetComponentLocation(), DotB = GetForwardVector();
+	return 180.0f / UE_PI * FMath::Acos(FVector::DotProduct(DotA.GetSafeNormal(), DotB.GetSafeNormal()));
+}
+
+bool UVisionConeComponent::IsActorInRange(const AActor* InActor) const
+{
+	if (!InActor) return false;
+	return FVector::Dist(GetComponentLocation(), InActor->GetActorLocation()) <= FMath::Max(Distance, 10.0f);
+}
+
+EVisionState UVisionConeComponent::GetActorVisionState(const AActor* InActor) const
+{
+	if (IsActorInRange(InActor) && GetTraceToActor(InActor))
+	{
+		if (const float Angle = GetAngleToActor(InActor); Angle >= 0.0f && Angle <= GetOuterAngle())
+		{
+			return Angle <= GetInnerAngle() ? EVisionState::Sighted : EVisionState::Peripheral;
+		}
+	}
+	
+	return EVisionState::None;
+}
---
+++ Plugins/ToroUtilities/Source/ToroCore/Public/Components/VisionConeComponent.h
@@ -0,0 +1,59 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Components/SceneComponent.h"
+#include "MiscObjects/VisionTraceMethod.h"
+#include "VisionConeComponent.generated.h"
+
+UENUM(BlueprintType)
+enum class EVisionState : uint8
+{
+	None,
+	Peripheral,
+	Sighted
+};
+
+UCLASS(NotBlueprintable, ClassGroup = (Game), meta = (BlueprintSpawnableComponent))
+class TOROCORE_API UVisionConeComponent final : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	UVisionConeComponent();
+
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (ClampMin = 10.0f, UIMin = 10.0f))
+		float Distance;
+
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (ClampMin = 10.0f, UIMin = 10.0f, ClampMax = 90.0f, UIMax = 90.0f))
+		float OuterAngle;
+
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (ClampMin = 0.0f, UIMin = 0.0f))
+		float InnerAngle;
+
+	UPROPERTY(EditAnywhere, Category = Settings, Instanced)
+		TObjectPtr<UVisionTraceMethod> TraceMethod;
+
+	UFUNCTION(BlueprintPure, Category = VisionCone)
+		bool GetTraceToActor(const AActor* InActor) const;
+
+	UFUNCTION(BlueprintPure, Category = VisionCone)
+		float GetAngleToActor(const AActor* InActor) const;
+
+	UFUNCTION(BlueprintPure, Category = VisionCone)
+		bool IsActorInRange(const AActor* InActor) const;
+
+	UFUNCTION(BlueprintPure, Category = VisionCone)
+		EVisionState GetActorVisionState(const AActor* InActor) const;
+
+	float GetOuterAngle() const
+	{
+		return FMath::Max(0.0f, FMath::Min(OuterAngle, 90.0f));
+	}
+
+	float GetInnerAngle() const
+	{
+		return FMath::Max(0.0f, FMath::Min(GetOuterAngle() + InnerAngle, 90.0f));
+	}
+};
