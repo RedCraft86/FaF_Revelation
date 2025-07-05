a9ad8c05a6388fd7e4e739c00847718911fb32e1
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-07-04 23:22:59 -0400

    sighted enemy

---
+++ Source/FaFRevGame/Characters/SightedEnemy.cpp
@@ -0,0 +1,61 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "SightedEnemy.h"
+
+ASightedEnemy::ASightedEnemy()
+	: VisionCooldown(0.1f), VisionTimer(0.0f), VisionState(EVisionState::None), PendingState(EVisionState::None)
+{
+	PrimaryActorTick.TickInterval = 0.05f;
+	PrimaryActorTick.bStartWithTickEnabled = true;
+	PrimaryActorTick.TickGroup = TG_DuringPhysics;
+
+	VisionCone = CreateDefaultSubobject<UVisionConeComponent>("VisionCone");
+	VisionCone->SetRelativeLocation(FVector(0.0f, 0.0f, 70.0f));
+	VisionCone->SetupAttachment(GetMesh());
+}
+
+void ASightedEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	GetWorldTimerManager().SetTimerForNextTick([this]()
+	{
+		PlayerChar = AGamePlayerChar::Get<AGamePlayerChar>(this);
+	});
+}
+
+void ASightedEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (!PlayerChar) return;
+
+	PendingState = VisionCone->GetActorVisionState(PlayerChar);
+	if (VisionTimer > 0.0f)
+	{
+		VisionTimer -= DeltaTime;
+		if (VisionState == PendingState)
+		{
+			VisionTimer = 0.0f;
+		}
+		else if (VisionTimer <= 0.0f)
+		{
+			VisionTimer = 0.0f;
+			VisionState = PendingState;
+			OnVisionUpdate(VisionState);
+		}
+	}
+	else if (VisionState != PendingState)
+	{
+		VisionTimer = VisionCooldown;
+	}
+}
+
+void ASightedEnemy::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	if (!GetVisionAttachmentBone().IsNone())
+	{
+		VisionCone->AttachToComponent(GetMesh(),
+			FAttachmentTransformRules::KeepRelativeTransform,
+			GetVisionAttachmentBone());
+	}
+}
---
+++ Source/FaFRevGame/Characters/SightedEnemy.h
@@ -0,0 +1,45 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "EnemyBase.h"
+#include "PlayerChar/GamePlayerChar.h"
+#include "Components/VisionConeComponent.h"
+#include "SightedEnemy.generated.h"
+
+UCLASS(Abstract)
+class FAFREVGAME_API ASightedEnemy final : public AEnemyBase
+{
+	GENERATED_BODY()
+
+public:
+
+	ASightedEnemy();
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Subobjects)
+		TObjectPtr<UVisionConeComponent> VisionCone;
+
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (ClampMin = 0.0f, UIMin = 0.0f))
+		float VisionCooldown;
+
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = "Vision Update")
+		void OnVisionUpdate(const EVisionState State);
+
+	UFUNCTION(BlueprintNativeEvent)
+		FName GetVisionAttachmentBone();
+	FName GetVisionAttachmentBone_Implementation() { return FName("head"); }
+
+	UFUNCTION(BlueprintPure, Category = AI)
+		EVisionState GetVisionState() const { return VisionState; }
+
+private:
+
+	UPROPERTY() float VisionTimer;
+	UPROPERTY() EVisionState VisionState;
+	UPROPERTY() EVisionState PendingState;
+	UPROPERTY(Transient) TObjectPtr<AGamePlayerChar> PlayerChar;
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void OnConstruction(const FTransform& Transform) override;
+};
