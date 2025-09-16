82aebc1d5b7503c9dc7bc00dff96462aada5ec00
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-15 22:05:03 -0400

    clear cache

--- Plugins/ToroUtilities/Source/ToroUtilities/Private/ToroUserSettings.cpp
+++ Plugins/ToroUtilities/Source/ToroUtilities/Private/ToroUserSettings.cpp
@@ -14,6 +14,7 @@ UToroUserSettings::UToroUserSettings()
 
 void UToroUserSettings::CheckSupportedResolutions()
 {
+	SupportedResolutions.Empty();
 	UKismetSystemLibrary::GetSupportedFullscreenResolutions(SupportedResolutions);
 	FullscreenRes = SupportedResolutions.Last();
 	Algo::Reverse(SupportedResolutions);
