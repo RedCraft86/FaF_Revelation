db6700029ccc84c0063558cb88653c8a969ead26
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-17 18:54:04 -0400

    misc types

---
+++ Plugins/ToroUtilities/Source/ToroCore/Public/DataTypes/MiscTypes.h
@@ -0,0 +1,19 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "MiscTypes.generated.h"
+
+UENUM(BlueprintInternalUseOnly)
+enum class EToroFoundPins : uint8
+{
+	Found,
+	NotFound
+};
+
+UENUM(BlueprintInternalUseOnly)
+enum class EToroValidPins : uint8
+{
+	Valid,
+	NotValid
+};
