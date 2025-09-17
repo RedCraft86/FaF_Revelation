aa21ac5f751a41ad88b130c05260f6da4e47c5b2
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-17 17:12:16 -0400

    data assets

---
+++ Plugins/ToroUtilities/Source/ToroCore/Private/MiscObjects/ToroDataAsset.cpp
@@ -0,0 +1,39 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "MiscObjects/ToroDataAsset.h"
+
+#if WITH_EDITOR
+void UToroDataAsset::PostLoad()
+{
+	Super::PostLoad();
+	if (!FApp::IsGame())
+	{
+		UpdateData();
+	}
+}
+
+void UToroDataAsset::PostInitProperties()
+{
+	Super::PostInitProperties();
+	if (!FApp::IsGame())
+	{
+		UpdateData();
+	}
+}
+
+void UToroDataAsset::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	if (!FApp::IsGame())
+	{
+		UpdateData();
+	}
+}
+
+FText UToroDatabase::GetDescription() const
+{
+	const FIntPoint Data = GetDataResults();
+	return FText::Format(INVTEXT("All: {0}\nValid: {1}\nInvalid: {2}"),
+		Data.GetMax(), Data.GetMin(), Data.GetMax() - Data.GetMin());
+}
+#endif
---
+++ Plugins/ToroUtilities/Source/ToroCore/Public/MiscObjects/ToroDataAsset.h
@@ -0,0 +1,35 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/DataAsset.h"
+#include "ToroDataAsset.generated.h"
+
+UCLASS(Abstract, NotBlueprintable, BlueprintType)
+class TOROCORE_API UToroDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+#if WITH_EDITOR
+public:
+	virtual void UpdateData() {}
+	virtual FText GetDescription() const { return FText::GetEmpty(); }
+protected:
+	virtual void PostLoad() override;
+	virtual void PostInitProperties() override;
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+};
+
+UCLASS(Abstract, NotBlueprintable, BlueprintType)
+class TOROCORE_API UToroDatabase : public UToroDataAsset
+{
+	GENERATED_BODY()
+
+#if WITH_EDITOR
+public:
+	/* X = Total Data, Y = Valid Data (Swappable, bigger value is always Total) */
+	virtual FIntPoint GetDataResults() const { return FIntPoint::ZeroValue; }
+	virtual FText GetDescription() const override;
+#endif
+};
