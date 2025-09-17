8cbddaf35d9999f24a6763fab699a0c2f1628bc1
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-17 15:33:29 -0400

    nav path visualizer

---
+++ Plugins/ToroUtilities/Source/ToroCore/Public/Actors/NavPathVisualizer.h
@@ -0,0 +1,74 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Components/EditorShapeComponent.h"
+#include "Components/BillboardComponent.h"
+#include "NavPathVisualizer.generated.h"
+
+UCLASS(NotBlueprintable, NotBlueprintType, meta = (HiddenCategories = "Actor,Collision"))
+class TOROCORE_API ANavPathVisualizer final : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	
+	ANavPathVisualizer(): bRefresh(false), PathPoints({FVector{0.0f}})
+	{
+		PrimaryActorTick.bCanEverTick = false;
+
+		SceneRoot = CreateDefaultSubobject<UBillboardComponent>("DefaultIconBillboard");
+		SceneRoot->SetSprite(LoadObject<UTexture2D>(nullptr,
+			TEXT("/Engine/EditorResources/EmptyActor.EmptyActor")));
+		SceneRoot->bIsScreenSizeScaled = true;
+		SceneRoot->SetHiddenInGame(true);
+		SetRootComponent(SceneRoot);
+	
+#if WITH_EDITORONLY_DATA
+		ShapeComponent = CreateEditorOnlyDefaultSubobject<UEditorShapeComponent>("ShapeComponent");
+		if (ShapeComponent)
+		{
+			FWireStringData Data; Data.String = TEXT("NavPath Visualizer");
+			ShapeComponent->WireStrings.Add(TEXT("Label"), Data);
+		}
+
+		bRunConstructionScriptOnDrag = true;
+#endif
+		bIsEditorOnlyActor = true;
+		SetCanBeDamaged(false);
+	}
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+	bool bRefresh;
+	
+	UPROPERTY(EditAnywhere, Category = Settings, meta = (MakeEditWidget = true))
+	TArray<FVector> PathPoints;
+	
+private:
+	
+	UPROPERTY() TObjectPtr<UBillboardComponent> SceneRoot;
+#if WITH_EDITORONLY_DATA
+	UPROPERTY() TObjectPtr<UEditorShapeComponent> ShapeComponent;
+#endif
+	
+	virtual void BeginPlay() override
+	{
+		Super::BeginPlay();
+		K2_DestroyActor();
+	}
+	
+	virtual void OnConstruction(const FTransform& Transform) override
+	{
+		Super::OnConstruction(Transform);
+		SetActorScale3D(FVector{1.0f});
+#if WITH_EDITORONLY_DATA
+		if (ShapeComponent)
+		{
+			ShapeComponent->WireNavPath.Targets = PathPoints;
+			ShapeComponent->UpdateNavPoints();
+		}
+#endif
+		bRefresh = false;
+	}
+};
