c57e90146d3282cfca7ee97c305f4f29a1d03ec7
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-17 18:36:45 -0400

    no unity

--- Plugins/ToroUtilities/Source/ToroCore/ToroCore.Build.cs
+++ Plugins/ToroUtilities/Source/ToroCore/ToroCore.Build.cs
@@ -7,6 +7,7 @@ public class ToroCore : ModuleRules
     public ToroCore(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        bUseUnity = false;
 
         PublicDependencyModuleNames.AddRange(
             [
