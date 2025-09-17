2278b1d433e4c7e355c5f4bc347e8cc99fdd4c92
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-16 20:58:00 -0400

    toro actor

---
+++ Plugins/ToroUtilities/Source/ToroCore/Private/Actors/ToroActor.cpp
@@ -0,0 +1,51 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Actors/ToroActor.h"
+
+AToroActor::AToroActor(): bEnabled(true), bStartWithCollision(true)
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SetRootComponent(SceneRoot);
+
+	SetCanBeDamaged(false);
+}
+
+void AToroActor::SetEnabled(const bool bInEnabled)
+{
+	if (bEnabled != bInEnabled)
+	{
+		bEnabled = bInEnabled;
+		BroadcastStateChanged();
+	}
+}
+
+void AToroActor::BroadcastStateChanged()
+{
+	EnableStateChanged(bEnabled);
+	EnableStateChangedBP(bEnabled);
+	OnEnableStateChanged.Broadcast(bEnabled);
+	OnEnableStateChangedBP.Broadcast(bEnabled);
+}
+
+void AToroActor::EnableStateChanged(const bool bIsEnabled)
+{
+	SetActorHiddenInGame(!bIsEnabled);
+	SetActorEnableCollision(bIsEnabled);
+	SetActorTickEnabled(PrimaryActorTick.bStartWithTickEnabled && bIsEnabled);
+}
+
+void AToroActor::BeginPlay()
+{
+	Super::BeginPlay();
+	SetActorEnableCollision(bStartWithCollision);
+	if (!bEnabled) BroadcastStateChanged();
+}
+
+void AToroActor::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	if (!RuntimeGuid.IsValid()) RuntimeGuid = FGuid::NewGuid();
+}
---
+++ Plugins/ToroUtilities/Source/ToroCore/Public/Actors/ToroActor.h
@@ -0,0 +1,63 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "ToroActor.generated.h"
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FActorEnableChanged, const bool);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActorEnableChangedBP, const bool, bEnabled);
+
+UCLASS(Abstract)
+class TOROCORE_API AToroActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	AToroActor();
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Subobjects)
+		TObjectPtr<USceneComponent> SceneRoot;
+
+	UPROPERTY(BlueprintAssignable, DisplayName = "On Enable State Changed")
+		FActorEnableChangedBP OnEnableStateChangedBP;
+
+	UFUNCTION(BlueprintCallable, Category = Actor)
+		virtual void SetEnabled(const bool bInEnabled);
+
+	UFUNCTION(BlueprintPure, Category = Actor)
+		bool IsEnabled() const { return bEnabled; }
+
+	UFUNCTION(BlueprintPure, Category = Actor)
+		FGuid GetRuntimeGuid() const { return RuntimeGuid; }
+
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = "Enable State Changed")
+		void EnableStateChangedBP(const bool bState);
+
+	FActorEnableChanged OnEnableStateChanged;
+
+protected:
+#if WITH_EDITORONLY_DATA
+	UPROPERTY(EditDefaultsOnly, Category = Tick)
+		bool bTickInEditor = false;
+#endif
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		bool bEnabled;
+
+	UPROPERTY(EditAnywhere, Category = Actor, NonPIEDuplicateTransient, TextExportTransient, NonTransactional)
+		FGuid RuntimeGuid;
+
+	UPROPERTY(EditAnywhere, Category = Actor)
+		bool bStartWithCollision;
+
+	void BroadcastStateChanged();
+	virtual void EnableStateChanged(const bool bIsEnabled);
+
+	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
+#if WITH_EDITORONLY_DATA
+	virtual bool ShouldTickIfViewportsOnly() const override { return bTickInEditor; }
+#endif
+};
