9411865f28b48a17cce19c7de302aeea693802d5
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-17 17:12:36 -0400

    unneeded

--- Plugins/ToroUtilities/Source/ToroCore/Private/ToroCore.cpp
+++ Plugins/ToroUtilities/Source/ToroCore/Private/ToroCore.cpp
@@ -3,19 +3,5 @@
 #include "ToroCore.h"
 
 DEFINE_LOG_CATEGORY(LogToroCore);
-
-#define LOCTEXT_NAMESPACE "ToroCore"
-
-void FToroCoreModule::StartupModule()
-{
-    
-}
-
-void FToroCoreModule::ShutdownModule()
-{
-    
-}
-
-#undef LOCTEXT_NAMESPACE
     
 IMPLEMENT_MODULE(FToroCoreModule, ToroCore)
