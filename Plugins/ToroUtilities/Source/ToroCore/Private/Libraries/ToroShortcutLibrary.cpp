9cba246e07c9f0952c2c6c974b7440ab1c779805
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-17 18:14:49 -0400

    shortcut library

---
+++ Plugins/ToroUtilities/Source/ToroCore/Private/Libraries/ToroShortcutLibrary.cpp
@@ -0,0 +1,54 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Libraries/ToroShortcutLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+void UToroShortcutLibrary::ClearCameraFade(const UObject* ContextObject)
+{
+	if (APlayerCameraManager* PCM = UGameplayStatics::GetPlayerCameraManager(ContextObject, 0))
+	{
+		PCM->StopCameraFade();
+	}
+}
+
+void UToroShortcutLibrary::SetCameraFade(const UObject* ContextObject, const float Alpha, const FLinearColor Color, const bool bFadeAudio)
+{
+	if (APlayerCameraManager* PCM = UGameplayStatics::GetPlayerCameraManager(ContextObject, 0))
+	{
+		PCM->SetManualCameraFade(Alpha, Color, bFadeAudio);
+	}
+}
+
+void UToroShortcutLibrary::StartCameraFade(UObject* ContextObject, const float FromAlpha, const float ToAlpha,
+	const float Duration, const FLinearColor Color, const bool bShouldFadeAudio, const bool bHoldWhenFinished)
+{
+	if (APlayerCameraManager* PCM = UGameplayStatics::GetPlayerCameraManager(ContextObject, 0))
+	{
+		PCM->StartCameraFade(FromAlpha, ToAlpha, Duration, Color, bShouldFadeAudio, bHoldWhenFinished);
+	}
+}
+
+void UToroShortcutLibrary::SetViewTarget(UObject* ContextObject, AActor* NewTarget, const float BlendTime,
+	const EViewTargetBlendFunction BlendFunc, const float BlendExp, const bool bLockOutgoing)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(ContextObject, 0))
+	{
+		PC->SetViewTargetWithBlend(NewTarget, BlendTime, BlendFunc, BlendExp, bLockOutgoing);
+	}
+}
+
+void UToroShortcutLibrary::AddActorTag(AActor* Target, const FName InTag)
+{
+	if (IsValid(Target))
+	{
+		Target->Tags.AddUnique(InTag);
+	}
+}
+
+void UToroShortcutLibrary::RemoveActorTag(AActor* Target, const FName InTag)
+{
+	if (IsValid(Target))
+	{
+		Target->Tags.Remove(InTag);
+	}
+}
