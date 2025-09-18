82faf3a4570dcb33c44491f7e2607fee1bb826d2
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-18 10:13:28 -0400

    achievements

---
+++ Plugins/ToroUtilities/Source/ToroGameplay/Public/Achievements/AchievementManager.h
@@ -0,0 +1,14 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "AchievementManager.generated.h"
+
+UCLASS(NotBlueprintable, ClassGroup = (Player), meta = (BlueprintSpawnableComponent))
+class TOROGAMEPLAY_API UAchievementManager : public UActorComponent
+{
+	GENERATED_BODY()
+
+	// TODO
+};
