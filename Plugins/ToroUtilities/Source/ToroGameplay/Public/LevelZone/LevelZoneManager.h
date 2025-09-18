532def4d408a63aff671d6a95c9cd4d5b2fad73e
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-18 09:28:06 -0400

    level zone

---
+++ Plugins/ToroUtilities/Source/ToroGameplay/Public/LevelZone/LevelZoneManager.h
@@ -0,0 +1,14 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Subsystems/WorldSubsystem.h"
+#include "LevelZoneManager.generated.h"
+
+UCLASS()
+class TOROGAMEPLAY_API ULevelZoneManager final : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+	// TODO
+};
