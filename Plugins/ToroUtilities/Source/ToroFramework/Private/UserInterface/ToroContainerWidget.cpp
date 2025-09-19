8bf3a8da953723d8dd13f0b64c9b1c112a8c3873
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-19 17:26:33 -0400

    game widget system

---
+++ Plugins/ToroUtilities/Source/ToroFramework/Private/UserInterface/ToroContainerWidget.cpp
@@ -0,0 +1,70 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "UserInterface/ToroContainerWidget.h"
+#include "UserInterface/ToroManagedWidget.h"
+
+bool UToroContainerWidget::PushEntry(UToroManagedWidget* Widget)
+{
+	if (Widget && !ManagedWidgets.Contains(Widget))
+	{
+		ManagedWidgets.Add(Widget);
+		return true;
+	}
+	return false;
+}
+
+bool UToroContainerWidget::PopEntry(UToroManagedWidget* Widget)
+{
+	if (Widget && ManagedWidgets.Contains(Widget))
+	{
+		ManagedWidgets.Remove(Widget);
+		return true;
+	}
+	return false;
+}
+
+void UToroContainerWidget::InitWidget(APlayerController* Controller)
+{
+	Super::InitWidget(Controller);
+	AddToViewport(ZOrder);
+}
+
+bool UToroOverlayContainer::PushEntry(UToroManagedWidget* Widget)
+{
+	if (Super::PushEntry(Widget))
+	{
+		WidgetStack->AddChild(Widget);
+		return true;
+	}
+	return false;
+}
+
+bool UToroOverlayContainer::PopEntry(UToroManagedWidget* Widget)
+{
+	if (Super::PopEntry(Widget))
+	{
+		WidgetStack->RemoveChild(Widget);
+		return true;
+	}
+	return false;
+}
+
+bool UToroStackContainer::PushEntry(UToroManagedWidget* Widget)
+{
+	if (Super::PushEntry(Widget))
+	{
+		WidgetStack->AddWidgetInstance(*Widget);
+		return true;
+	}
+	return false;
+}
+
+bool UToroStackContainer::PopEntry(UToroManagedWidget* Widget)
+{
+	if (Super::PopEntry(Widget))
+	{
+		WidgetStack->RemoveWidget(*Widget);
+		return true;
+	}
+	return false;
+}
---
+++ Plugins/ToroUtilities/Source/ToroFramework/Public/UserInterface/ToroManagedWidget.h
@@ -0,0 +1,41 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "ToroWidgetBase.h"
+#include "ToroManagedWidget.generated.h"
+
+class UToroContainerWidget;
+
+/* A Managed Widget is a widget Managed/Hosted by a Container Widget. */
+UCLASS(Abstract)
+class TOROFRAMEWORK_API UToroManagedWidget : public UToroWidgetBase
+{
+	GENERATED_BODY()
+
+	friend class AToroWidgetManager;
+
+public:
+	
+	UToroManagedWidget(const FObjectInitializer& ObjectInitializer)
+		: Super(ObjectInitializer), bAutoPush(true), bIsPushed(false)
+	{}
+
+	virtual void PushWidget();
+	virtual void PopWidget();
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = Settings)
+		TSubclassOf<UToroContainerWidget> ContainerClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = Settings)
+		bool bAutoPush;
+
+	bool bIsPushed;
+
+	virtual UToroContainerWidget* GetContainerWidget() const;
+
+	virtual void BeginDestroy() override;
+	virtual void InitWidget(APlayerController* Controller) override;
+};
