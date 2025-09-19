146e7e98642f87b4378bc811e07b4871c8c3d13f
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-19 16:08:31 -0400

    manager

--- Plugins/ToroUtilities/Source/ToroFramework/Private/UserInterface/ToroWidgetManager.cpp
+++ Plugins/ToroUtilities/Source/ToroFramework/Private/UserInterface/ToroWidgetManager.cpp
@@ -1,3 +1,52 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
 #include "UserInterface/ToroWidgetManager.h"
+
+AToroWidgetManager::AToroWidgetManager()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	PrimaryActorTick.TickGroup = TG_DuringPhysics;
+}
+
+UToroWidgetBase* AToroWidgetManager::AddWidget(const TSubclassOf<UToroWidgetBase> WidgetClass)
+{
+	if (!WidgetClass)
+	{
+		return nullptr;
+	}
+
+	UToroWidgetBase* Widget = FindWidget(WidgetClass);
+	if (!Widget)
+	{
+		Widget = UToroWidgetBase::CreateToroWidget(GetOwningPlayerController(), WidgetClass);
+		WidgetObjects.Add(Widget);
+	}
+
+	return Widget;
+}
+
+UToroWidgetBase* AToroWidgetManager::FindWidget(const TSubclassOf<UToroWidgetBase> WidgetClass)
+{
+	if (!WidgetClass)
+	{
+		return nullptr;
+	}
+
+	for (UToroWidgetBase* Widget : WidgetObjects)
+	{
+		if (Widget->IsA(WidgetClass))
+		{
+			return Widget;
+		}
+	}
+
+	return nullptr;
+}
+
+void AToroWidgetManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// TODO automatic loading
+}
--- Plugins/ToroUtilities/Source/ToroFramework/Public/UserInterface/ToroWidgetManager.h
+++ Plugins/ToroUtilities/Source/ToroFramework/Public/UserInterface/ToroWidgetManager.h
@@ -2,7 +2,9 @@
 
 #pragma once
 
+#include "ToroWidgetBase.h"
 #include "GameFramework/HUD.h"
+#include "Kismet/GameplayStatics.h"
 #include "ToroWidgetManager.generated.h"
 
 UCLASS()
@@ -10,5 +12,42 @@ class TOROFRAMEWORK_API AToroWidgetManager : public AHUD
 {
 	GENERATED_BODY()
 
-	// TODO
+public:
+
+	AToroWidgetManager();
+
+	template <typename T = AToroWidgetManager>
+	static T* Get(const UObject* ContextObject, const int32 PlayerIdx = 0)
+	{
+		const APlayerController* PC = UGameplayStatics::GetPlayerController(ContextObject, PlayerIdx);
+		return PC ? Cast<T>(PC->GetHUD()) : nullptr;
+	}
+
+	UFUNCTION(BlueprintCallable, Category = WidgetManager, meta = (DeterminesOutputType = "WidgetClass"))
+		UToroWidgetBase* AddWidget(const TSubclassOf<UToroWidgetBase> WidgetClass);
+
+	UFUNCTION(BlueprintPure, Category = WidgetManager, meta = (DeterminesOutputType = "WidgetClass"))
+		UToroWidgetBase* FindWidget(const TSubclassOf<UToroWidgetBase> WidgetClass);
+
+	template<typename T = UToroWidgetBase>
+	T* FindWidget(const TSubclassOf<UToroWidgetBase>& WidgetClass)
+	{
+		return Cast<T>(FindWidget(WidgetClass));
+	}
+
+protected:
+
+	UPROPERTY(Transient)
+	TSet<TObjectPtr<UToroWidgetBase>> WidgetObjects;
+
+	virtual void BeginPlay() override;
+
+public: // Statics
+
+	template<typename T = UToroWidgetBase>
+	static T* GetWidget(const UObject* ContextObject, const TSubclassOf<UToroWidgetBase>& WidgetClass)
+	{
+		AToroWidgetManager* Manager = AToroWidgetManager::Get(ContextObject);
+		return Manager ? Manager->FindWidget<T>(WidgetClass) : nullptr;
+	}
 };
