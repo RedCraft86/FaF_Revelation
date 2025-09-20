c56f695f6fb2387e67c5e96e753ccdb34d75e621
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-20 19:46:39 -0400

    player character

--- Plugins/ToroUtilities/Source/ToroFramework/Private/Framework/ToroPlayerCharacter.cpp
+++ Plugins/ToroUtilities/Source/ToroFramework/Private/Framework/ToroPlayerCharacter.cpp
@@ -1,3 +1,139 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
 #include "Framework/ToroPlayerCharacter.h"
+#include "Framework/ToroPlayerController.h"
+#include "Components/CapsuleComponent.h"
+#include "Libraries/ToroLightingUtils.h"
+
+namespace PlayerLockTags
+{
+	DEFINE_GAMEPLAY_TAG(PlayerLock)
+	DEFINE_GAMEPLAY_TAG_CHILD(PlayerLock, MainMenu)
+	DEFINE_GAMEPLAY_TAG_CHILD(PlayerLock, Startup)
+	DEFINE_GAMEPLAY_TAG_CHILD(PlayerLock, Loading)
+	DEFINE_GAMEPLAY_TAG_CHILD(PlayerLock, Dialogue)
+	DEFINE_GAMEPLAY_TAG_CHILD(PlayerLock, Cinematic)
+	DEFINE_GAMEPLAY_TAG_CHILD(PlayerLock, GameOver)
+}
+
+AToroPlayerCharacter::AToroPlayerCharacter(): SlowTickInterval(0.1f), LockTags({PlayerLockTags::TAG_Startup.GetTag()})
+{
+	PrimaryActorTick.bCanEverTick = true;
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	AutoReceiveInput = EAutoReceiveInput::Player0;
+	
+	PlayerLight = CreateDefaultSubobject<UPointLightComponent>("PlayerLight");
+	PlayerLight->SetupAttachment(GetCapsuleComponent());
+
+	CharacterID = CharacterTags::TAG_Player;
+	LightSettings.Intensity = 0.025f;
+	LightSettings.AttenuationRadius = 500.0f;
+	LightSettings.bUseTemperature = true;
+	LightSettings.Temperature = 12000.0f;
+	LightSettings.bLightCastShadows = false;
+	LightSettings.bUseInverseSquaredFalloff = true;
+	LightSettings.LightFalloffExponent = 1.0f;
+	LightSettings.SpecularScale = 0.0f;
+	UToroLightingUtils::SetPointLightProperties(PlayerLight, LightSettings);
+}
+
+void AToroPlayerCharacter::AddLockTag(const FGameplayTag InTag)
+{
+	if (PlayerLockTags::IsValidTag(InTag)) LockTags.AddTag(InTag);
+}
+
+void AToroPlayerCharacter::ClearLockTag(const FGameplayTag InTag)
+{
+	if (PlayerLockTags::IsValidTag(InTag)) LockTags.RemoveTag(InTag);
+}
+
+bool AToroPlayerCharacter::HasLockTag(const FGameplayTag InTag) const
+{
+	return LockTags.HasTagExact(InTag);
+}
+
+void AToroPlayerCharacter::SetLightSettings(const FPointLightProperties& InSettings)
+{
+	LightSettings = InSettings;
+	UToroLightingUtils::SetPointLightProperties(PlayerLight, LightSettings);
+}
+
+void AToroPlayerCharacter::EnterCinematic(AActor* CinematicActor)
+{
+	if (AToroPlayerController* PC = GetController<AToroPlayerController>())
+	{
+		PC->EnterCinematic(CinematicActor);
+		AddLockTag(PlayerLockTags::TAG_Cinematic);
+	}
+}
+
+void AToroPlayerCharacter::ExitCinematic()
+{
+	if (AToroPlayerController* PC = GetController<AToroPlayerController>())
+	{
+		PC->ExitCinematic();
+		ClearLockTag(PlayerLockTags::TAG_Cinematic);
+	}
+}
+
+void AToroPlayerCharacter::FadeToBlack(const float InTime, const bool bAudio) const
+{
+	const AToroPlayerController* PC = GetController<AToroPlayerController>();
+	if (APlayerCameraManager* CamManager = PC ? PC->PlayerCameraManager : nullptr)
+	{
+		if (FMath::IsNearlyZero(InTime))
+		{
+			CamManager->SetManualCameraFade(1.0f, FLinearColor::Black, bAudio);
+		}
+		else
+		{
+			CamManager->StartCameraFade(0.0f, 1.0f,
+				InTime, FLinearColor::Black, bAudio, true);
+		}
+	}
+}
+
+void AToroPlayerCharacter::FadeFromBlack(const float InTime, const bool bAudio) const
+{
+	const AToroPlayerController* PC = GetController<AToroPlayerController>();
+	if (APlayerCameraManager* CamManager = PC ? PC->PlayerCameraManager : nullptr)
+	{
+		if (FMath::IsNearlyZero(InTime))
+		{
+			CamManager->SetManualCameraFade(0.0f, FLinearColor::Black, bAudio);
+		}
+		else
+		{
+			CamManager->StartCameraFade(1.0f, 0.0f,
+				InTime, FLinearColor::Black, bAudio, true);
+		}
+	}
+}
+
+void AToroPlayerCharacter::ClearFade() const
+{
+	const AToroPlayerController* PC = GetController<AToroPlayerController>();
+	if (APlayerCameraManager* CamManager = PC ? PC->PlayerCameraManager : nullptr)
+	{
+		CamManager->StopCameraFade();
+	}
+}
+
+void AToroPlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	// if (UToroFrameworkSettings::Get()->IsOnLaunchMap(this))
+	// {
+	// 	LockTags.AddTag(PlayerLockTags::TAG_MainMenu);
+	// }
+
+	GetWorldTimerManager().SetTimer(SlowTickTimer, this,
+		&AToroPlayerCharacter::SlowTick, SlowTickInterval, true, 0.0f);
+}
+
+void AToroPlayerCharacter::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	LockTags.RemoveTag(PlayerLockTags::TAG_PlayerLock);
+	UToroLightingUtils::SetPointLightProperties(PlayerLight, LightSettings);
+}
--- Plugins/ToroUtilities/Source/ToroFramework/Public/Framework/ToroPlayerCharacter.h
+++ Plugins/ToroUtilities/Source/ToroFramework/Public/Framework/ToroPlayerCharacter.h
@@ -2,13 +2,82 @@
 
 #pragma once
 
+#include "ToroGameMode.h"
 #include "Actors/ToroCharacter.h"
+#include "DataTypes/LightingData.h"
+#include "Helpers/GameplayTagMacros.h"
+#include "Helpers/ClassGetterMacros.h"
+#include "Components/PointLightComponent.h"
 #include "ToroPlayerCharacter.generated.h"
+
+namespace PlayerLockTags
+{
+	TOROFRAMEWORK_API DECLARE_GAMEPLAY_TAG(PlayerLock)
+	TOROFRAMEWORK_API DECLARE_GAMEPLAY_TAG(MainMenu)
+	TOROFRAMEWORK_API DECLARE_GAMEPLAY_TAG(Startup)
+	TOROFRAMEWORK_API DECLARE_GAMEPLAY_TAG(Loading)
+	TOROFRAMEWORK_API DECLARE_GAMEPLAY_TAG(Dialogue)
+	TOROFRAMEWORK_API DECLARE_GAMEPLAY_TAG(Cinematic)
+	TOROFRAMEWORK_API DECLARE_GAMEPLAY_TAG(GameOver)
+
+	TOROFRAMEWORK_API CREATE_TAG_VERIFIER(PlayerLock)
+}
 
 UCLASS()
 class TOROFRAMEWORK_API AToroPlayerCharacter : public AToroCharacter
 {
 	GENERATED_BODY()
 
-	// TODO
+public:
+
+	AToroPlayerCharacter();
+
+	PLAYER_CLASS_GETTER(AToroPlayerCharacter, GetPlayerPawn);
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Subobjects)
+		TObjectPtr<UPointLightComponent> PlayerLight;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Tick, meta = (ClampMin = 0.05f, UIMin = 0.05f))
+		float SlowTickInterval;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Settings)
+		FGameplayTagContainer LockTags;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Settings, AdvancedDisplay)
+		FPointLightProperties LightSettings;
+
+	UFUNCTION(BlueprintCallable, Category = Player)
+		virtual void AddLockTag(UPARAM(meta = (Categories = "PlayerLock")) const FGameplayTag InTag);
+
+	UFUNCTION(BlueprintCallable, Category = Player)
+		virtual void ClearLockTag(UPARAM(meta = (Categories = "PlayerLock")) const FGameplayTag InTag);
+
+	UFUNCTION(BlueprintPure, Category = Player)
+		virtual bool HasLockTag(UPARAM(meta = (Categories = "PlayerLock")) const FGameplayTag InTag) const;
+
+	UFUNCTION(BlueprintCallable, Category = Player)
+		virtual void SetLightSettings(const FPointLightProperties& InSettings);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, Category = Player)
+		virtual void EnterCinematic(AActor* CinematicActor);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, Category = Player)
+		virtual void ExitCinematic();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, Category = Player)
+		virtual void FadeToBlack(const float InTime, const bool bAudio = true) const;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, Category = Player)
+		virtual void FadeFromBlack(const float InTime, const bool bAudio = true) const;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, Category = Player)
+		virtual void ClearFade() const;
+
+protected:
+
+	FTimerHandle SlowTickTimer;
+
+	virtual void SlowTick() {}
+	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
 };
