31fe2f08adf4454f75c292981d7959d67c1fad81
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-20 20:21:53 -0400

    cutscene actor

--- Plugins/ToroUtilities/Source/ToroFramework/Private/MiscActors/ToroCutsceneActor.cpp
+++ Plugins/ToroUtilities/Source/ToroFramework/Private/MiscActors/ToroCutsceneActor.cpp
@@ -1,3 +1,90 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
 #include "MiscActors/ToroCutsceneActor.h"
+#include "Framework/ToroPlayerCharacter.h"
+#include "Libraries/ToroShortcutLibrary.h"
+#include "LevelSequencePlayer.h"
+
+AToroCutsceneActor::AToroCutsceneActor(const FObjectInitializer& Init)
+	: Super(Init), bSkippable(false)
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	SetCanBeDamaged(false);
+}
+
+void AToroCutsceneActor::PlayAfterFade(const float FadeTime)
+{
+	if (!GetWorldTimerManager().IsTimerActive(FadedPlayHandle))
+	{
+		UToroShortcutLibrary::StartCameraFade(this, 0.0f, 1.0f, FadeTime);
+		GetWorldTimerManager().SetTimer(FadedPlayHandle, [this]()
+		{
+			FadedPlayHandle.Invalidate();
+			PlayCutscene();
+		}, FadeTime + UE_SMALL_NUMBER, false);
+	}
+}
+
+void AToroCutsceneActor::PlayCutscene()
+{
+	if (!GetWorldTimerManager().IsTimerActive(FadedPlayHandle))
+	{
+		UToroShortcutLibrary::ClearCameraFade(this);
+		GetSequencePlayer()->Play();
+		LockPlayer();
+	}
+}
+
+void AToroCutsceneActor::SkipCutscene()
+{
+	if (!GetWorldTimerManager().IsTimerActive(FadedPlayHandle))
+	{
+		GetSequencePlayer()->GoToEndAndStop();
+		UnlockPlayer();
+	}
+}
+
+void AToroCutsceneActor::OnFinished()
+{
+	UnlockPlayer();
+	OnFinishedEvent.Broadcast();
+
+	if (!bSkippable) return;
+	// TODO save
+}
+
+void AToroCutsceneActor::LockPlayer()
+{
+	if (AToroPlayerCharacter* Controller = AToroPlayerCharacter::Get(this))
+	{
+		Controller->EnterCinematic(this);
+	}
+
+	if (!bSkippable) return;
+	// TODO ui
+}
+
+void AToroCutsceneActor::UnlockPlayer()
+{
+	if (AToroPlayerCharacter* Controller = AToroPlayerCharacter::Get(this))
+	{
+		Controller->ExitCinematic();
+	}
+
+	if (!bSkippable) return;
+	// TODO ui
+}
+
+void AToroCutsceneActor::BeginPlay()
+{
+	Super::BeginPlay();
+	GetSequencePlayer()->OnNativeFinished.BindUObject(this, &AToroCutsceneActor::OnFinished);
+}
+
+void AToroCutsceneActor::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	if (!CutsceneGuid.IsValid()) CutsceneGuid = FGuid::NewGuid();
+}
--- Plugins/ToroUtilities/Source/ToroFramework/Public/MiscActors/ToroCutsceneActor.h
+++ Plugins/ToroUtilities/Source/ToroFramework/Public/MiscActors/ToroCutsceneActor.h
@@ -3,12 +3,49 @@
 #pragma once
 
 #include "LevelSequenceActor.h"
+#include "Interfaces/GuidInterface.h"
+#include "DataTypes/GameInputConfig.h"
 #include "ToroCutsceneActor.generated.h"
 
 UCLASS()
-class TOROFRAMEWORK_API AToroCutsceneActor final : public ALevelSequenceActor
+class TOROFRAMEWORK_API AToroCutsceneActor final : public ALevelSequenceActor, public IGuidInterface
 {
 	GENERATED_BODY()
 
-	// TODO
+public:
+
+	AToroCutsceneActor(const FObjectInitializer& Init);
+
+	UFUNCTION(BlueprintCallable, Category = "Sequencer|Player")
+		void PlayAfterFade(const float FadeTime = 0.5f);
+
+	UFUNCTION(BlueprintCallable, Category = "Sequencer|Player")
+		void PlayCutscene();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, Category = "Sequencer|Player")
+		void SkipCutscene();
+
+	DECLARE_MULTICAST_DELEGATE(FOnFinishedEvent)
+	FOnFinishedEvent OnFinishedEvent;
+
+	virtual FGuid GetUniqueGUID_Implementation() override { return CutsceneGuid; }
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = General)
+		bool bSkippable;
+
+	UPROPERTY(EditAnywhere, Category = General, NonPIEDuplicateTransient, TextExportTransient, NonTransactional)
+		FGuid CutsceneGuid;
+
+	// TODO widget
+	FGameInputConfig CachedInput;
+	FTimerHandle FadedPlayHandle;
+
+	void OnFinished();
+	void LockPlayer();
+	void UnlockPlayer();
+
+	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
 };
