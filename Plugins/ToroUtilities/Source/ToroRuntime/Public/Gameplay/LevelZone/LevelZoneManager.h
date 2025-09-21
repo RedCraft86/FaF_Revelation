588e57810f6480442f8eb201eaf4fbd75a8f0253
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-21 13:00:15 -0400

    zone manager

--- Plugins/ToroUtilities/Source/ToroRuntime/Public/Gameplay/LevelZone/LevelZoneManager.h
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Gameplay/LevelZone/LevelZoneManager.h
@@ -4,10 +4,17 @@
 
 #include "ToroSettings.h"
 #include "Helpers/ClassGetterMacros.h"
+#include "Helpers/GameplayTagMacros.h"
 #include "Subsystems/WorldSubsystem.h"
 #include "LevelZoneManager.generated.h"
 
-UCLASS()
+namespace ZoneTags
+{
+	TORORUNTIME_API DECLARE_GAMEPLAY_TAG(Zone)
+	TORORUNTIME_API CREATE_TAG_VERIFIER(Zone)
+}
+
+UCLASS(BlueprintType, DisplayName = "Zone Manager")
 class TORORUNTIME_API ULevelZoneManager final : public UWorldSubsystem
 {
 	GENERATED_BODY()
@@ -18,8 +25,29 @@ public:
 
 	WORLD_SUBSYSTEM_GETTER(ULevelZoneManager);
 
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FZoneActivity, const FGameplayTag&, Character, const FGameplayTag&, Zone);
+	UPROPERTY(BlueprintAssignable) FZoneActivity OnEnterZone;
+	UPROPERTY(BlueprintAssignable) FZoneActivity OnExitZone;
+
+	UFUNCTION(BlueprintCallable, Category = LevelZone)
+		void SetCharacterInZone(
+			UPARAM(meta = (Categories = "Character")) const FGameplayTag Character,
+			UPARAM(meta = (Categories = "Zone")) const FGameplayTag Zone);
+	
+	UFUNCTION(BlueprintCallable, Category = LevelZone)
+		bool IsCharacterInZone(UPARAM(meta = (Categories = "Character")) const FGameplayTag Character,
+			UPARAM(meta = (Categories = "Zone")) const FGameplayTag Zone);
+
+	UFUNCTION(BlueprintCallable, Category = LevelZone)
+		FGameplayTag GetZoneByCharacter(UPARAM(meta = (Categories = "Character")) const FGameplayTag Character);
+
+	UFUNCTION(BlueprintCallable, Category = LevelZone)
+		FGameplayTagContainer GetCharactersInZone(UPARAM(meta = (Categories = "Zone")) const FGameplayTag Zone);
+
 	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
 	virtual bool DoesSupportWorldType(const EWorldType::Type WorldType) const override;
 
 private:
+
+	TMap<FGameplayTag, FGameplayTag> CharToZone;
 };
