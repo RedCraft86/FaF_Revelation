a5e77655aab913924f2a3d836a532652d65711da
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-21 21:46:31 -0400

    misc actions

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/Gameplay/WorldActions/Actions/MiscActions.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Gameplay/WorldActions/Actions/MiscActions.h"
+#include "Kismet/GameplayStatics.h"
+#include "Helpers/LatentInfo.h"
+
+void FWTMisc_LevelStreaming::RunAction()
+{
+	for (const TPair<TSoftObjectPtr<UWorld>, bool>& Level : Load)
+	{
+		UGameplayStatics::LoadStreamLevelBySoftObjectPtr(GetWorldContext(), Level.Key,
+			Level.Value, false, LatentInfo::Make());
+	}
+	for (const TSoftObjectPtr<UWorld>& Level : Unload)
+	{
+		UGameplayStatics::UnloadStreamLevelBySoftObjectPtr(GetWorldContext(),
+			Level, LatentInfo::Make(), false);
+	}
+}
+
+void FWTMisc_LevelStreaming::OnBeginPlay()
+{
+	for (auto It = Load.CreateIterator(); It; ++It)
+	{
+		if (It.Key().IsNull()) It.RemoveCurrent();
+	}
+	for (auto It = Unload.CreateIterator(); It; ++It)
+	{
+		if (It->IsNull()) It.RemoveCurrent();
+	}
+}
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Gameplay/WorldActions/Actions/MiscActions.h
@@ -0,0 +1,32 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Gameplay/WorldActions/WorldActionBase.h"
+#include "MiscActions.generated.h"
+
+USTRUCT(BlueprintInternalUseOnly, DisplayName = "Misc", meta = (Hidden))
+struct TORORUNTIME_API FWATask_Misc : public FWorldActionBase
+{
+	GENERATED_BODY()
+};
+
+USTRUCT(BlueprintType, DisplayName = "Level Streaming")
+struct TORORUNTIME_API FWTMisc_LevelStreaming final : public FWATask_Misc
+{
+	GENERATED_BODY()
+
+	/* (Level, MakeVisibleAfterLoad) */
+	UPROPERTY(EditAnywhere, Category = Action)
+		TMap<TSoftObjectPtr<UWorld>, bool> Load;
+
+	UPROPERTY(EditAnywhere, Category = Action)
+		TSet<TSoftObjectPtr<UWorld>> Unload;
+
+	FWTMisc_LevelStreaming() {}
+
+protected:
+
+	virtual void RunAction() override;
+	virtual void OnBeginPlay() override;
+};
