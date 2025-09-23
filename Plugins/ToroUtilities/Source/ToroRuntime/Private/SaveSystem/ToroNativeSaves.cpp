e82a1f1dcba8eeb627d1e2d5e38920d2f0945557
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-23 19:50:27 -0400

    native saves

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/SaveSystem/ToroNativeSaves.cpp
@@ -0,0 +1,53 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "SaveSystem/ToroNativeSaves.h"
+#include "Helpers/WindowsHelpers.h"
+#include "ToroRuntime.h"
+
+namespace ContentTags
+{
+	DEFINE_GAMEPLAY_TAG(Content)
+	DEFINE_GAMEPLAY_TAG_CHILD(Content, Cutscene)
+	DEFINE_GAMEPLAY_TAG_CHILD(Content, Tutorial)
+	DEFINE_GAMEPLAY_TAG_CHILD(Content, Character)
+	DEFINE_GAMEPLAY_TAG_CHILD(Content, Ending)
+	DEFINE_GAMEPLAY_TAG_CHILD(Content, Misc)
+}
+
+UToroGlobalSave::UToroGlobalSave()
+{
+	bSingleSlot = true;
+}
+
+void UToroGlobalSave::DeleteData()
+{
+#if !WITH_EDITOR
+	WindowsHelpers::OpenDialogue(TEXT("Report this to the dev!"),
+		TEXT("Attempting to delete global persistent data, which should not be possible."),
+		EWindowsDialogueType::Ok, EWindowsDialogueIcon::Error);
+#else
+	UE_LOG(LogToroRuntime, Error, TEXT("Cannot delete global persistent data!"));
+#endif
+}
+
+void UToroGlobalSave::SerializeData(FArchive& Ar)
+{
+	Ar << Content;
+	Ar << Achievements;
+}
+
+UToroGameSave::UToroGameSave(): PlayTime(0.0f)
+{
+	bSingleSlot = false;
+}
+
+void UToroGameSave::DeleteData()
+{
+	Super::DeleteData();
+	PlayTime = 0.0f;
+}
+
+void UToroGameSave::SerializeData(FArchive& Ar)
+{
+	Ar << PlayTime;
+}
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/SaveSystem/ToroNativeSaves.h
@@ -0,0 +1,70 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "ToroSaveTypes.h"
+#include "Helpers/GameplayTagMacros.h"
+#include "ToroNativeSaves.generated.h"
+
+namespace ContentTags
+{
+	TORORUNTIME_API DECLARE_GAMEPLAY_TAG(Content)
+	TORORUNTIME_API DECLARE_GAMEPLAY_TAG(Cutscene)
+	TORORUNTIME_API DECLARE_GAMEPLAY_TAG(Tutorial)
+	TORORUNTIME_API DECLARE_GAMEPLAY_TAG(Character)
+	TORORUNTIME_API DECLARE_GAMEPLAY_TAG(Ending)
+	TORORUNTIME_API DECLARE_GAMEPLAY_TAG(Misc)
+
+	TORORUNTIME_API inline bool IsValidTag(const FGameplayTag& InTag)
+	{
+		static FGameplayTag BaseTag = TAG_Content.GetTag();
+		return InTag.MatchesTag(BaseTag)
+			&& InTag != TAG_Content
+			&& InTag != TAG_Cutscene
+			&& InTag != TAG_Tutorial
+			&& InTag != TAG_Character
+			&& InTag != TAG_Ending
+			&& InTag != TAG_Misc;
+	}
+}
+
+UCLASS()
+class TORORUNTIME_API UToroGlobalSave : public UToroSaveObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UToroGlobalSave();
+
+	UPROPERTY(BlueprintReadWrite, Category = GlobalSave)
+		TMap<FGameplayTag, uint8> Achievements;
+
+	UPROPERTY(BlueprintReadWrite, Category = GlobalSave)
+		TSet<FGameplayTag> Content;
+
+	virtual void DeleteData() override;
+
+protected:
+
+	virtual void SerializeData(FArchive& Ar) override;
+};
+
+UCLASS()
+class TORORUNTIME_API UToroGameSave : public UToroSaveObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UToroGameSave();
+
+	UPROPERTY(BlueprintReadOnly, Category = GameSave)
+		float PlayTime;
+
+	virtual void DeleteData() override;
+
+protected:
+
+	virtual void SerializeData(FArchive& Ar) override;
+};
