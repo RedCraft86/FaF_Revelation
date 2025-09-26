42c63b0ac6297820520ed4e7f904dbe08739873a
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-26 14:05:52 -0400

    inventory data

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/Gameplay/Inventory/InventorySlotData.cpp
@@ -0,0 +1,169 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Gameplay/Inventory/InventorySlotData.h"
+
+void FInventoryItemSlot::AddAmount(const uint8 InAmount)
+{
+	if (InAmount == 0) return;
+	if (const UInventoryAsset* AssetPtr = Asset.LoadSynchronous())
+	{
+		Amount = FMath::Min(Amount + InAmount, (int32)AssetPtr->GetStackLimit());
+	}
+}
+
+bool FInventoryItemSlot::TakeAmount(const uint8 InAmount)
+{
+	if (Amount >= InAmount)
+	{
+		Amount -= InAmount;
+		return true;
+	}
+	return false;
+}
+
+FJsonObjectWrapper FInventoryItemSlot::GetJsonMetadata() const
+{
+	FJsonObjectWrapper Wrapper;
+	Wrapper.JsonObjectFromString(Metadata);
+	return Wrapper;
+}
+
+void FInventoryItemSlot::SetJsonMetadata(const FJsonObjectWrapper& InJson)
+{
+	InJson.JsonObjectToString(Metadata);
+}
+
+void FInventoryItems::CleanupAndSort()
+{
+	Slots.RemoveAllSwap([](const FInventoryItemSlot& Element)
+	{
+		return !Element.IsValidSlot();
+	});
+
+	Slots.Sort([](const FInventoryItemSlot& A, const FInventoryItemSlot& B)
+	{
+		const UInventoryAsset *ItemA = A.GetAsset(), *ItemB = B.GetAsset();
+		return ItemA->DisplayName.CompareTo(ItemB->DisplayName) < 0;
+	});
+}
+
+void FInventoryItems::ForEachSlot(const TFunction<void(const FInventoryItemSlot&)>& Func)
+{
+	if (!Func) return;
+	CleanupAndSort();
+	for (const FInventoryItemSlot& Item : Slots)
+	{
+		Func(Item);
+	}
+}
+
+const FInventoryItemSlot* FInventoryItems::AddItem(UInventoryAsset* Item, const uint8 Amount)
+{
+	if (!Item || Item->AssetType != EInvAssetType::Item || Amount == 0) 
+	{
+		return nullptr;
+	}
+		
+	if (FInventoryItemSlot* Slot = FindItem(Item))
+	{
+		Slot->AddAmount(Amount);
+		return Slot;
+	}
+
+	return &Slots.Emplace_GetRef(Item, Amount);
+}
+
+bool FInventoryItems::TakeItem(const UInventoryAsset* Item, const uint8 Amount)
+{
+	if (!Item || Item->AssetType != EInvAssetType::Item || Amount == 0) 
+	{
+		return false;
+	}
+
+	const int32 Idx = Slots.IndexOfByPredicate([Item](const FInventoryItemSlot& Element)
+	{
+		return Element.Asset == Item;
+	});
+
+	if (Slots.IsValidIndex(Idx) && Slots[Idx].TakeAmount(Amount))
+	{
+		if (Slots[Idx].Amount == 0)
+		{
+			Slots.RemoveAtSwap(Idx);
+		}
+		return true;
+	}
+
+	return false;
+}
+
+FInventoryItemSlot* FInventoryItems::FindItem(const UInventoryAsset* Item)
+{
+	return Slots.FindByPredicate([Item](const FInventoryItemSlot& Element)
+	{
+		return Element.Asset == Item;
+	});
+}
+
+bool FInventoryArchiveSlot::MarkSecretFound()
+{
+	if (!bSecretFound)
+	{
+		bSecretFound = true;
+		return true;
+	}
+	return false;
+}
+
+void FInventoryArchives::ForEachSlot(const TFunction<void(const FInventoryArchiveSlot&)>& Func)
+{
+	if (!Func) return;
+	for (const FInventoryArchiveSlot& Item : Slots)
+	{
+		Func(Item);
+	}
+}
+
+const FInventoryArchiveSlot* FInventoryArchives::AddArchive(UInventoryAsset* Archive, const bool bSecretFound)
+{
+	if (!Archive || Archive->AssetType != EInvAssetType::Archive) 
+	{
+		return nullptr;
+	}
+
+	const int32 Idx = Slots.IndexOfByPredicate([Archive](const FInventoryArchiveSlot& Element)
+	{
+		return Element.Asset == Archive;
+	});
+       
+	if (Slots.IsValidIndex(Idx))
+	{
+		FInventoryArchiveSlot* Slot = &Slots[Idx];
+		if (bSecretFound && Slot->MarkSecretFound())
+		{
+			MoveToLast(Idx);
+			return &Slots.Last();
+		}
+		return Slot;
+	}
+
+	return &Slots.Emplace_GetRef(Archive, bSecretFound);
+}
+
+void FInventoryArchives::MoveToLast(const int32 IndexToMove)
+{
+	if (Slots.IsValidIndex(IndexToMove) && IndexToMove != Slots.Num() - 1)
+	{
+		FInventoryArchiveSlot Item = MoveTemp(Slots[IndexToMove]);
+		Slots.RemoveAt(IndexToMove);
+		Slots.Add(MoveTemp(Item));
+	}
+}
+
+FInventoryArchiveSlot* FInventoryArchives::FindArchive(const UInventoryAsset* Archive)
+{
+	return Slots.FindByPredicate([Archive](const FInventoryArchiveSlot& Element)
+	{
+		return Element.Asset == Archive;
+	});
+}
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Gameplay/Inventory/InventorySlotData.h
@@ -0,0 +1,117 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "InventoryAsset.h"
+#include "JsonObjectWrapper.h"
+#include "InventorySlotData.generated.h"
+
+USTRUCT(BlueprintInternalUseOnly)
+struct TORORUNTIME_API FInventoryItemSlot
+{
+	GENERATED_BODY()
+
+	UPROPERTY() TSoftObjectPtr<UInventoryAsset> Asset;
+	UPROPERTY() uint8 Amount;
+	UPROPERTY() FString Metadata;
+
+	FInventoryItemSlot(): Asset(nullptr), Amount(0), Metadata(TEXT("{}")) {}
+
+	FInventoryItemSlot(UInventoryAsset* InAsset, const uint8 InAmount = 1)
+		: Asset(InAsset), Amount(InAmount), Metadata(TEXT("{}"))
+	{}
+
+	FORCEINLINE friend FArchive& operator<<(FArchive& Ar, FInventoryItemSlot& Data)
+	{
+		Ar << Data.Asset;
+		Ar << Data.Amount;
+		Ar << Data.Metadata;
+		return Ar;
+	}
+
+	FORCEINLINE bool IsValidSlot() const { return !Asset.IsNull() && Amount > 0; }
+	FORCEINLINE UInventoryAsset* GetAsset() const { return Asset.LoadSynchronous(); }
+
+	void AddAmount(const uint8 InAmount);
+	bool TakeAmount(const uint8 InAmount);
+	FJsonObjectWrapper GetJsonMetadata() const;
+	void SetJsonMetadata(const FJsonObjectWrapper& InJson);
+};
+
+USTRUCT(BlueprintInternalUseOnly)
+struct TORORUNTIME_API FInventoryItems
+{
+	GENERATED_BODY()
+
+private:
+
+	UPROPERTY() TArray<FInventoryItemSlot> Slots;
+	
+public:
+
+	FORCEINLINE friend FArchive& operator<<(FArchive& Ar, FInventoryItems& Data)
+	{
+		Ar << Data.Slots;
+		return Ar;
+	}
+
+	void CleanupAndSort();
+	void ForEachSlot(const TFunction<void(const FInventoryItemSlot&)>& Func);
+	const FInventoryItemSlot* AddItem(UInventoryAsset* Item, const uint8 Amount = 1);
+	bool TakeItem(const UInventoryAsset* Item, const uint8 Amount = 1);
+	
+	FInventoryItemSlot* FindItem(const UInventoryAsset* Item);
+	const FInventoryItemSlot* FindItem(const UInventoryAsset* Item) const { return FindItem(Item); }
+};
+
+USTRUCT(BlueprintInternalUseOnly)
+struct TORORUNTIME_API FInventoryArchiveSlot
+{
+	GENERATED_BODY()
+
+	UPROPERTY() TSoftObjectPtr<UInventoryAsset> Asset;
+	UPROPERTY() bool bSecretFound;
+	
+	FInventoryArchiveSlot(): Asset(nullptr), bSecretFound(false) {}
+
+	FInventoryArchiveSlot(UInventoryAsset* InAsset, const bool bKnowSecret = false)
+		: Asset(InAsset), bSecretFound(bKnowSecret)
+	{}
+
+	FORCEINLINE friend FArchive& operator<<(FArchive& Ar, FInventoryArchiveSlot& Data)
+	{
+		Ar << Data.Asset;
+		Ar << Data.bSecretFound;
+		return Ar;
+	}
+
+	FORCEINLINE bool IsValidSlot() const { return !Asset.IsNull(); }
+	FORCEINLINE UInventoryAsset* GetAsset() const { return Asset.LoadSynchronous(); }
+
+	bool MarkSecretFound();
+};
+
+USTRUCT(BlueprintInternalUseOnly)
+struct TORORUNTIME_API FInventoryArchives
+{
+	GENERATED_BODY()
+
+private:
+
+	UPROPERTY() TArray<FInventoryArchiveSlot> Slots;
+	
+public:
+
+	FORCEINLINE friend FArchive& operator<<(FArchive& Ar, FInventoryArchives& Data)
+	{
+		Ar << Data.Slots;
+		return Ar;
+	}
+
+	void ForEachSlot(const TFunction<void(const FInventoryArchiveSlot&)>& Func);
+	const FInventoryArchiveSlot* AddArchive(UInventoryAsset* Archive, const bool bSecretFound = false);
+	void MoveToLast(const int32 IndexToMove);
+	
+	FInventoryArchiveSlot* FindArchive(const UInventoryAsset* Archive);
+	const FInventoryArchiveSlot* FindArchive(const UInventoryAsset* Archive) const { return FindArchive(Archive); }
+};
