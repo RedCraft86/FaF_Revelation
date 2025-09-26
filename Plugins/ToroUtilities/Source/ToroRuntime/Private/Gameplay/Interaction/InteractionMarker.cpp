2df47a8b2ce8b4768fc5536570b1139c7f3154f9
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-25 23:41:29 -0400

    interact marker

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/Gameplay/Interaction/InteractionMarker.cpp
@@ -0,0 +1,57 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Interaction/InteractionMarker.h"
+#include "Kismet/GameplayStatics.h"
+#if WITH_EDITOR
+#include "Subsystems/UnrealEditorSubsystem.h"
+#endif
+
+UInteractionMarker::UInteractionMarker(): MaxDistance(250.0f)
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.TickGroup = TG_DuringPhysics;
+	PrimaryComponentTick.TickInterval = 0.1f;
+	
+	SetHiddenInGame(false);
+	bIsScreenSizeScaled = true;
+	ScreenSize = 0.0005f;
+#if WITH_EDITOR
+	bTickInEditor = true;
+#endif
+}
+
+FVector UInteractionMarker::GetCameraPos() const
+{
+	FVector Position = FVector::ZeroVector;
+#if WITH_EDITOR
+	if (!FApp::IsGame())
+	{
+		if (!GEditor) return GetComponentLocation();
+		if (UUnrealEditorSubsystem* Subsystem = GEditor->GetEditorSubsystem<UUnrealEditorSubsystem>())
+		{
+			FRotator Rotation;
+			Subsystem->GetLevelViewportCameraInfo(Position, Rotation);
+		}
+	}
+	else
+#endif
+		if (CamManager)
+		{
+			Position = CamManager->GetCameraLocation();
+		}
+
+	return Position;
+}
+
+void UInteractionMarker::BeginPlay()
+{
+	Super::BeginPlay();
+	CamManager = UGameplayStatics::GetPlayerCameraManager(this, 0);
+}
+
+void UInteractionMarker::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	SetVisibility(FVector::Dist(GetComponentLocation(), GetCameraPos()) <= MaxDistance);
+}
+
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Gameplay/Interaction/InteractionMarker.h
@@ -0,0 +1,27 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Components/BillboardComponent.h"
+#include "InteractionMarker.generated.h"
+
+UCLASS(NotBlueprintable, ClassGroup = (Game), meta = (BlueprintSpawnableComponent))
+class TORORUNTIME_API UInteractionMarker final : public UBillboardComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	UInteractionMarker();
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		float MaxDistance;
+
+private:
+
+	TObjectPtr<APlayerCameraManager> CamManager;
+
+	FVector GetCameraPos() const;
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
