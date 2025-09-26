81f00f56e58e2aae46de2710c8853a58de358e37
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-26 17:05:16 -0400

    inventory manager

--- Plugins/ToroUtilities/Source/ToroRuntime/Public/Gameplay/Inventory/InventoryManager.h
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Gameplay/Inventory/InventoryManager.h
@@ -2,10 +2,23 @@
 
 #pragma once
 
+#include "InventorySlotData.h"
 #include "Components/ActorComponent.h"
 #include "Helpers/ClassGetterMacros.h"
 #include "Framework/ToroPlayerState.h"
+#include "Helpers/GameplayTagMacros.h"
+#include "SaveSystem/ToroSaveManager.h"
 #include "InventoryManager.generated.h"
+
+namespace InventoryTags
+{
+	TORORUNTIME_API DECLARE_GAMEPLAY_TAG(Inventory)
+	TORORUNTIME_API inline bool IsValidTag(const FGameplayTag& InTag)
+	{
+		static FGameplayTag BaseTag = TAG_Inventory.GetTag();
+		return InTag.MatchesTag(BaseTag) || InTag == BaseTag;
+	}
+}
 
 UCLASS(NotBlueprintable, ClassGroup = (Game), meta = (BlueprintSpawnableComponent))
 class TORORUNTIME_API UInventoryManager : public UActorComponent
@@ -24,5 +37,39 @@ public:
 		return Get(ContextObject, PlayerIdx);
 	}
 
-	// TODO
+	UFUNCTION(BlueprintCallable, Category = Inventory)
+		void AddItem(UInventoryAsset* InItem, const uint8 Amount = 1);
+
+	UFUNCTION(BlueprintCallable, Category = Inventory)
+	bool TakeItem(UInventoryAsset* InItem, const uint8 Amount = 1);
+
+	UFUNCTION(BlueprintCallable, Category = Inventory)
+		void AddArchive(UInventoryAsset* InItem, const bool bSecretFound = false);
+
+	UFUNCTION(BlueprintCallable, Category = Inventory)
+		void UnEquipItem();
+
+	UFUNCTION(BlueprintCallable, Category = Inventory)
+		void EquipItem(UInventoryAsset* InItem);
+
+	UFUNCTION(BlueprintCallable, Category = Inventory)
+		void SetEquipmentUsage(const bool bUsing) const;
+	
+	void PullFromSave(const FGameplayTag& Profile);
+	void PushToSave(const FGameplayTag& Profile) const;
+
+	FInventoryItems& GetItems() { return Items; }
+	FInventoryArchives& GetArchives() { return Archives; }
+
+	// TODO ui
+
+private:
+
+	FInventoryItems Items;
+	FInventoryArchives Archives;
+	TObjectPtr<AEquipmentActor> Equipment;
+	TObjectPtr<UToroSaveManager> SaveManager;
+
+	void UpdateUI();
+	virtual void BeginPlay() override;
 };
