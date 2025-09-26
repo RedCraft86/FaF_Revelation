bb2dfda3d716fa525994b6f4037b46472e8e154e
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-26 18:46:19 -0400

    factories

--- Plugins/ToroUtilities/Source/ToroEditor/ToroEditor.Build.cs
+++ Plugins/ToroUtilities/Source/ToroEditor/ToroEditor.Build.cs
@@ -16,6 +16,7 @@ public class ToroEditor : ModuleRules
                 "Engine",
                 "Slate",
                 "SlateCore",
+                "AssetDefinition",
                 "EditorSubsystem",
                 "DeveloperSettings",
                 
