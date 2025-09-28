82a13eafd33c7689e21d77c00fcaa5bce1155509
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-28 16:47:33 -0400

    baking stuff

--- Plugins/ToroUtilities/Source/ToroCore/Public/Actors/Procedural/ToroProcGenBase.h
+++ Plugins/ToroUtilities/Source/ToroCore/Public/Actors/Procedural/ToroProcGenBase.h
@@ -41,22 +41,35 @@ public:
 		}
 		return nullptr;
 	}
-	
+
 protected:
 
 	UPROPERTY(EditAnywhere, Category = Settings, AdvancedDisplay)
 		bool bRealtimeConstruction;
-	
+
 	UFUNCTION(CallInEditor, Category = Tools)
 		virtual void Construct() { EventConstruct(); }
-	
+
 	UFUNCTION(BlueprintImplementableEvent, DisplayName = "Construct")
 		void EventConstruct();
 
+#if WITH_EDITORONLY_DATA
+	/* Remove this source actor after baking */
+	UPROPERTY(EditAnywhere, Category = Tools)
+		bool bBakeRemoveSource = true;
+#endif
+#if WITH_EDITOR
+	UFUNCTION(CallInEditor, Category = Tools, DisplayName = "Bake Meshes")
+		void BakeMeshes() { BakeInternal(bBakeRemoveSource, true, nullptr); }
+#endif
 	virtual void OnConstruction(const FTransform& Transform) override;
+#if WITH_EDITOR
+public:
+	virtual void BakeInternal(const bool bRemoveSource, const bool bSelectActors, TArray<AActor*>* OutActors);
+#endif
 
 public: // Statics
-	
+
 	static TArray<int32> LoopMeshArray(const TArray<FTransformMeshData>& Sample,
 		const EGeneratorLoopMode Mode, const uint8 Amount);
 };
