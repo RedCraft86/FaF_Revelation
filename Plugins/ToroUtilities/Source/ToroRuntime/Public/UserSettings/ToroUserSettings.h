adaf07d5cff42755e76ab69573328e11b06e287d
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-30 23:53:14 -0400

    setting datatypes

--- Plugins/ToroUtilities/Source/ToroRuntime/Public/UserSettings/ToroUserSettings.h
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/UserSettings/ToroUserSettings.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "UserSettingTypes.h"
 #include "GameFramework/GameUserSettings.h"
 #include "ToroUserSettings.generated.h"
 
@@ -26,4 +27,52 @@ public:
 protected:
 
 	UPROPERTY(Config) bool bInitialized;
+
+	/* General */
+	UPROPERTY(Config) bool ShowFPS;
+	UPROPERTY(Config) bool DeveloperMode;
+	UPROPERTY(Config) EGameDifficulty Difficulty;
+
+	// Camera
+	UPROPERTY(Config) bool SmoothCamera;
+	UPROPERTY(Config) FVector2D Sensitivity;
+
+	// Accessibility
+	UPROPERTY(Config) EColorBlindMode ColorBlindMode;
+	UPROPERTY(Config) uint8 ColorBlindIntensity;
+
+	/* Graphics */
+	UPROPERTY(Config) bool Borderless;
+
+	// Visuals
+	UPROPERTY(Config) float Gamma;
+	UPROPERTY(Config) uint8 Brightness;
+	UPROPERTY(Config) bool FancyBloom;
+	UPROPERTY(Config) bool ScreenSpaceFogScattering;
+	UPROPERTY(Config) uint8 MotionBlur;
+
+	// Lumen
+	UPROPERTY(Config) uint8 LumenGI;
+	UPROPERTY(Config) uint8 LumenReflections;
+	UPROPERTY(Config) bool HitLightingReflections;
+
+	// Image Fidelity
+	UPROPERTY(Config) EImageFidelityMode ImageFidelity;
+	UPROPERTY(Config) uint8 TSRResolution;
+
+	// FSR 3
+	UPROPERTY(Config) uint8 FSRQuality;
+	UPROPERTY(Config) float FSRSharpness;
+	UPROPERTY(Config) bool FSRFrameInterp;
+	
+	// XeSS
+	UPROPERTY(Config) uint8 XeSSQuality;
+	UPROPERTY(Config) bool XeSSFrameGen;
+	UPROPERTY(Config) bool XeSSLowLatency;
+	
+	/* Cached auto-set scalability */
+	UPROPERTY(Config) TArray<uint8> AutoScalability{3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+	
+	/* Audio */
+	UPROPERTY(Config) TMap<ESoundClassType, uint8> AudioVolume;
 };
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/UserSettings/UserSettingTypes.h
@@ -0,0 +1,83 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "UserSettingTypes.generated.h"
+
+UENUM(BlueprintType)
+enum class ESettingPerformance : uint8
+{
+	None,
+	Low,
+	Medium,
+	High
+};
+
+UENUM(BlueprintType)
+enum class EGameDifficulty : uint8
+{
+	Easy,
+	Normal,
+	Hard
+};
+
+UENUM(BlueprintType)
+enum class EColorBlindMode : uint8
+{
+	None,
+	
+	// Deuteranope (Red-Green Weak/Blind)
+	Deuteranope,
+	
+	// Protanope (Red Weak/Blind)
+	Protanope,
+	
+	// Tritanope (Blue-Yellow Weak/Blind)
+	Tritanope
+};
+
+UENUM(BlueprintType)
+enum class ESoundClassType : uint8
+{
+	Master,
+	Music,
+	SFX,
+	Voice
+};
+ENUM_RANGE_BY_FIRST_AND_LAST(ESoundClassType, ESoundClassType::Master, ESoundClassType::Voice)
+
+UENUM(BlueprintType)
+enum class EImageFidelityMode : uint8
+{
+	// No Anti-Aliasing or Upscaling
+	None,
+	
+	// Fast Approximate Anti-Aliasing
+	FXAA,
+
+	// Temporal Anti-Aliasing
+	TAA,
+
+	// Subpixel Morphological Anti-Aliasing (TODO 5.7 implementation)
+	SMAA	UMETA(Hidden),
+
+	// Temporal Super-Resolution (Upscaler)
+	TSR,
+
+	// FidelityFX™ Super Resolution 3.1 (Upscaler)
+	FSR,
+	
+	// Intel® XeSS Super Resolution
+	XeSS
+};
+
+inline EAntiAliasingMethod ConvertImageFidelity(const EImageFidelityMode InMode)
+{
+	switch (InMode)
+	{
+		case EImageFidelityMode::FXAA:	return AAM_FXAA;
+		case EImageFidelityMode::TAA:	return AAM_TemporalAA;
+		// case EImageFidelityMode::SMAA:	return AAM_SMAA // TODO 5.7 implementation
+		default:						return AAM_TSR; // Catch TSR, FSR, XeSS
+	}
+}
