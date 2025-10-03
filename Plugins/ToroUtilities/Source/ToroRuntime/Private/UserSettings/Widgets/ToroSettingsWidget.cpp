62a7fc4f3243c4f469db840accbaea982f325447
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-10-03 18:13:50 -0400

    settings widget base

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/UserSettings/Widgets/ToroSettingsWidget.cpp
@@ -0,0 +1,8 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "UserSettings/Widgets/ToroSettingsWidget.h"
+
+UToroSettingsWidget::UToroSettingsWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
