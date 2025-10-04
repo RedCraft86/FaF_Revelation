0533f9131fc80c1adb81260f8d0325443de7127f
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-10-03 20:40:36 -0400

    button list

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/UserInterface/Elements/ToroButtonList.cpp
@@ -0,0 +1,107 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "UserInterface/Elements/ToroButtonList.h"
+#include "Components/VerticalBoxSlot.h"
+#include "Blueprint/WidgetTree.h"
+
+void UToroButtonEntry::SelectButton(bool bImmediate)
+{
+	if (!bSelected)
+	{
+		bSelected = true;
+		PlayAnimationForward(SelectAnim, bImmediate ? 10.0f : 1.0f);
+	}
+}
+
+void UToroButtonEntry::DeselectButton(bool bImmediate)
+{
+	if (bSelected)
+	{
+		bSelected = false;
+		PlayAnimationReverse(SelectAnim, bImmediate ? 10.0f : 1.0f);
+	}
+}
+
+void UToroButtonEntry::SetDisplayData(const FText& InText, const FSlateFontInfo& InFont) const
+{
+	Label->SetText(InText);
+	Label->SetFont(InFont);
+}
+
+void UToroButtonEntry::OnButtonClicked()
+{
+	if (OnClicked.IsBound())
+	{
+		SelectButton();
+		OnClicked.Execute(this);
+	}
+}
+
+void UToroButtonEntry::NativeConstruct()
+{
+	Super::NativeConstruct();
+	Button->OnClicked.AddUniqueDynamic(this, &UToroButtonEntry::OnButtonClicked);
+}
+
+UToroButtonList::UToroButtonList(const FObjectInitializer& ObjectInitializer)
+	: UUserWidget(ObjectInitializer), EntryPadding(0.0f, 2.0f), SelectedIndex(INDEX_NONE)
+{
+#if WITH_EDITOR
+	const ConstructorHelpers::FObjectFinder<UObject> FontFinder(TEXT("/Engine/EngineFonts/Roboto.Roboto"));
+	if (FontFinder.Succeeded())
+	{
+		EntryFont = FSlateFontInfo(FontFinder.Object, 24, "Regular");
+	}
+#endif
+}
+
+void UToroButtonList::OnSelectionMade(UToroButtonEntry* Button)
+{
+	for (int i = 0; i < Buttons.Num(); i++)
+	{
+		if (Buttons[i] == Button)
+		{
+			SelectedIndex = i;
+			OnSelection.Broadcast(i);
+			OnSelectionBP.Broadcast(i);
+		}
+		else
+		{
+			Buttons[i]->DeselectButton();
+		}
+	}
+}
+
+void UToroButtonList::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	if (!WidgetTree || !Container)
+		return;
+
+	Container->ClearChildren();
+	Buttons.Empty(Entries.Num());
+	for (const FText& Entry : Entries)
+	{
+		if (Entry.IsEmptyOrWhitespace()) continue;
+		if (UToroButtonEntry* Button = WidgetTree->ConstructWidget<UToroButtonEntry>(EntryClass))
+		{
+			Buttons.Add(Button);
+			Button->SetDisplayData(Entry, EntryFont);
+			if (UVerticalBoxSlot* VSlot = Cast<UVerticalBoxSlot>(Container->AddChild(Button)))
+			{
+				VSlot->SetSize(EntrySize);
+				VSlot->SetPadding(EntryPadding);
+			}
+#if WITH_EDITOR
+			if (IsDesignTime())
+				continue;
+#endif
+			Button->OnClicked.BindUObject(this, &UToroButtonList::OnSelectionMade);
+			if (Buttons.Num() == 1)
+			{
+				SelectedIndex = 0;
+				Button->SelectButton(true); // Select first
+			}
+		}
+	}
+}
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/UserInterface/Elements/ToroButtonList.h
@@ -0,0 +1,93 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Components/VerticalBox.h"
+#include "Blueprint/UserWidget.h"
+#include "ToroButtonList.generated.h"
+
+UCLASS(Abstract)
+class TORORUNTIME_API UToroButtonEntry final : public UUserWidget
+{
+	GENERATED_BODY()
+
+	friend class UToroButtonList;
+
+public:
+
+	UToroButtonEntry(const FObjectInitializer& ObjectInitializer)
+		: UUserWidget(ObjectInitializer), bSelected(false)
+	{}
+
+	DECLARE_DELEGATE_OneParam(FOnClicked, UToroButtonEntry*);
+	FOnClicked OnClicked;
+
+	void SelectButton(bool bImmediate = false);
+	void DeselectButton(bool bImmediate = false);
+	void SetDisplayData(const FText& InText, const FSlateFontInfo& InFont) const;
+
+protected:
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> Button;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> Label;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> SelectAnim;
+
+	bool bSelected;
+
+	UFUNCTION() void OnButtonClicked();
+	virtual void NativeConstruct() override;
+};
+
+UCLASS(Abstract, HideCategories = (Interaction, Performance, Localization), meta = (PrioritizeCategories = "ButtonList"))
+class TORORUNTIME_API UToroButtonList final : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UToroButtonList(const FObjectInitializer& ObjectInitializer);
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSelectionBP, const int32, Index);
+	UPROPERTY(BlueprintAssignable, DisplayName = "On Selection") FOnSelectionBP OnSelectionBP;
+
+	DECLARE_MULTICAST_DELEGATE_OneParam(FOnSelection, const int32);
+	FOnSelection OnSelection;
+
+	UFUNCTION(BlueprintPure, Category = ButtonList)
+		int32 GetSelectedIndex() const { return SelectedIndex; }
+
+protected:
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UVerticalBox> Container;
+
+	UPROPERTY(EditAnywhere, Category = ButtonList)
+		TSubclassOf<UToroButtonEntry> EntryClass;
+
+	UPROPERTY(EditAnywhere, Category = ButtonList)
+		FSlateChildSize EntrySize;
+
+	UPROPERTY(EditAnywhere, Category = ButtonList)
+		FMargin EntryPadding;
+
+	UPROPERTY(EditAnywhere, Category = ButtonList)
+		FSlateFontInfo EntryFont;
+
+	UPROPERTY(EditAnywhere, Category = ButtonList)
+		TArray<FText> Entries;
+
+	UPROPERTY(BlueprintReadOnly, Category = ButtonList)
+		TArray<TObjectPtr<UToroButtonEntry>> Buttons;
+
+	int32 SelectedIndex;
+
+	void OnSelectionMade(UToroButtonEntry* Button);
+	virtual void NativePreConstruct() override;
+};
