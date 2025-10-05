07801ea993ebcf54a4c3501ca3cbc12f7252b477
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-10-05 13:45:47 -0400

    achievement widget

---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/Gameplay/Achievements/AchievementWidget.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "Achievements/AchievementWidget.h"
+#include "Achievements/AchievementManager.h"
+#include "Animation/UMGSequencePlayer.h"
+#include "Components/PanelWidget.h"
+
+UAchievementEntryWidget* UAchievementEntryWidget::CreateEntry(UUserWidget* Owner, const FAchievementEntry& Entry)
+{
+	if (!Entry.IsValidData()) return nullptr;
+	if (UAchievementEntryWidget* Widget = CreateWidget<UAchievementEntryWidget>(Owner))
+	{
+		Widget->InitializeWidget(Entry);
+		return Widget;
+	}
+	return nullptr;
+}
+
+void UAchievementEntryWidget::InitializeWidget(const FAchievementEntry& Entry)
+{
+	TitleText->SetText(Entry.Name);
+	IconImage->SetBrushFromTexture(Entry.Icon.LoadSynchronous());
+	if (FWidgetAnimationState* State = PlayAnimation(EntryAnim).GetAnimationState())
+	{
+		State->GetOnWidgetAnimationFinished().AddUObject(
+			this, &UAchievementEntryWidget::OnAnimationFinished);
+	}
+}
+
+void UAchievementWidget::OnAchievement(const FAchievementEntry& Data)
+{
+	AchievementList->AddChild(UAchievementEntryWidget::CreateEntry(this, Data));
+}
+
+void UAchievementWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	GetWorld()->GetTimerManager().SetTimerForNextTick([this]()
+	{
+		if (UAchievementManager* Manager = UAchievementManager::Get(this))
+		{
+			Manager->OnAchievement.AddUObject(this, &UAchievementWidget::OnAchievement);
+		}
+	});
+}
---
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Gameplay/Achievements/AchievementWidget.h
@@ -0,0 +1,61 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "AchievementDatabase.h"
+#include "UserInterface/ToroManagedWidget.h"
+#include "AchievementWidget.generated.h"
+
+UCLASS(Abstract)
+class TORORUNTIME_API UAchievementEntryWidget final : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UAchievementEntryWidget(const FObjectInitializer& ObjectInitializer)
+		: UUserWidget(ObjectInitializer)
+	{}
+
+	static UAchievementEntryWidget* CreateEntry(UUserWidget* Owner, const FAchievementEntry& Entry);
+
+protected:
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UImage> IconImage;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> TitleText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> EntryAnim;
+
+	void InitializeWidget(const FAchievementEntry& Entry);
+	void OnAnimationFinished(FWidgetAnimationState& State)
+	{
+		RemoveFromParent();
+	}
+};
+
+UCLASS(Abstract)
+class TORORUNTIME_API UAchievementWidget final : public UToroManagedWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UAchievementWidget(const FObjectInitializer& ObjectInitializer)
+		: Super(ObjectInitializer)
+	{}
+
+protected:
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UPanelWidget> AchievementList;
+
+	void OnAchievement(const FAchievementEntry& Data);
+
+	virtual void NativeConstruct() override;
+};
