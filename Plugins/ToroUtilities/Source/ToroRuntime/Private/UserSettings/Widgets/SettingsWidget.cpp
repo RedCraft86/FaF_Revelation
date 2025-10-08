5ce815115674dccdf6ca36cc4d36cd9a45130e4d
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-10-08 14:41:21 -0400

    settings widget

--- Plugins/ToroUtilities/Source/ToroRuntime/Private/UserSettings/Widgets/SettingsWidget.cpp
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/UserSettings/Widgets/SettingsWidget.cpp
@@ -1,10 +1,17 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
 #include "UserSettings/Widgets/SettingsWidget.h"
+#include "UserInterface/ToroWidgetManager.h"
 #include "Interfaces/ExitInterface.h"
 
 USettingsWidget::USettingsWidget(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
+	: Super(ObjectInitializer), TextLabels({
+		INVTEXT("General"),
+		INVTEXT("Graphics"),
+		INVTEXT("Audio"),
+		INVTEXT("Keybinds"),
+		INVTEXT("Developer")
+	})
 {
 	bAutoPush = false;
 }
@@ -13,6 +20,43 @@ void USettingsWidget::ShowWidget(UUserWidget* InParent)
 {
 	ParentUI = InParent;
 	PushWidget();
+}
+
+// void USettingsWidget::ReturnToWidget_Implementation(UUserWidget* FromWidget)
+// {
+// 	if (FromWidget == Difficulty)
+// 	{
+// 		
+// 	}
+// }
+
+void USettingsWidget::OnExit()
+{
+	PopWidget();
+}
+
+void USettingsWidget::OnDifficulty()
+{
+	if (Difficulty) Difficulty->ShowWidget(this);
+}
+
+void USettingsWidget::OnAutoAdjust()
+{
+	if (Settings)
+	{
+		Settings->AutoAdjustScalability();
+		AutoAdjustButton->SetIsEnabled(false);
+	}
+}
+
+void USettingsWidget::OnScreenSelect(const int32 Idx)
+{
+	if (TextLabels.IsValidIndex(Idx) && ScreenSwitch->GetActiveWidgetIndex() != Idx)
+	{
+		LabelText->SetText(TextLabels[Idx]);
+		ScreenSwitch->SetActiveWidgetIndex(Idx);
+		PlayAnimation(FadeAnim);
+	}
 }
 
 void USettingsWidget::PopWidget()
@@ -26,10 +70,20 @@ void USettingsWidget::PopWidget()
 void USettingsWidget::PushWidget()
 {
 	Super::PushWidget();
+	OnScreenSelect(0);
+	AutoAdjustButton->SetIsEnabled(true);
 }
 
 void USettingsWidget::InitWidget(APlayerController* Controller)
 {
 	Super::InitWidget(Controller);
 	Settings = UToroUserSettings::Get();
+	ExitButton->OnClicked.AddUniqueDynamic(this, &USettingsWidget::OnExit);
+	DifficultyButton->OnClicked.AddUniqueDynamic(this, &USettingsWidget::OnDifficulty);
+	AutoAdjustButton->OnClicked.AddUniqueDynamic(this, &USettingsWidget::OnAutoAdjust);
+	ButtonList->OnSelection.AddUObject(this, &USettingsWidget::OnScreenSelect);
+	GetWorld()->GetTimerManager().SetTimerForNextTick([this]()
+	{
+		Difficulty = AToroWidgetManager::GetWidget<UDifficultyWidget>(this);
+	});
 }
--- Plugins/ToroUtilities/Source/ToroRuntime/Public/UserSettings/Widgets/SettingsWidget.h
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/UserSettings/Widgets/SettingsWidget.h
@@ -2,12 +2,16 @@
 
 #pragma once
 
+#include "Interfaces/ExitInterface.h"
+#include "Components/WidgetSwitcher.h"
+#include "NativeWidgets/DifficultyWidget.h"
 #include "UserInterface/ToroManagedWidget.h"
+#include "UserInterface/Elements/ToroButtonList.h"
 #include "UserSettings/ToroUserSettings.h"
 #include "SettingsWidget.generated.h"
 
 UCLASS(Abstract)
-class TORORUNTIME_API USettingsWidget : public UToroManagedWidget
+class TORORUNTIME_API USettingsWidget final : public UToroManagedWidget, public IExitInterface
 {
 	GENERATED_BODY()
 
@@ -16,13 +20,42 @@ public:
 	USettingsWidget(const FObjectInitializer& ObjectInitializer);
 
 	void ShowWidget(UUserWidget* InParent);
+	// void ReturnToWidget_Implementation(UUserWidget* FromWidget) override;
 
 protected:
 
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UToroButtonList> ButtonList;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UWidgetSwitcher> ScreenSwitch;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> LabelText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> DifficultyButton;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> AutoAdjustButton;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UButton> ExitButton;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> FadeAnim;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		TArray<FText> TextLabels;
+
 	TObjectPtr<UUserWidget> ParentUI;
+	TObjectPtr<UDifficultyWidget> Difficulty;
 	TObjectPtr<UToroUserSettings> Settings;
 
-	// TODO impl
+	UFUNCTION() void OnExit();
+	UFUNCTION() void OnDifficulty();
+	UFUNCTION() void OnAutoAdjust();
+	void OnScreenSelect(const int32 Idx);
 
 	virtual void PopWidget() override;
 	virtual void PushWidget() override;
