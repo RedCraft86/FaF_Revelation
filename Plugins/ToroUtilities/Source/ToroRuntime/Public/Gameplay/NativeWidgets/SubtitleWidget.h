83f9347cdaddabafc4275dd31aa740582eceaff3
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-10-08 13:34:10 -0400

    subtitle widget

--- Plugins/ToroUtilities/Source/ToroRuntime/Public/Gameplay/NativeWidgets/SubtitleWidget.h
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Gameplay/NativeWidgets/SubtitleWidget.h
@@ -2,8 +2,37 @@
 
 #pragma once
 
+#include "Narrative/NarrativeManager.h"
 #include "UserInterface/ToroManagedWidget.h"
 #include "SubtitleWidget.generated.h"
+
+USTRUCT(BlueprintType)
+struct FToroSubtitle
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Subtitle)
+		FText Name;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Subtitle)
+		FText Line;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Subtitle, meta = (ClampMin = 0.0f, UIMin = 0.0f))
+		float ExtraTime;
+
+	FToroSubtitle(): ExtraTime(0.0f) {}
+	FToroSubtitle(const FText& InName, const FText& InLine, const float InExtraTime = 0.0f)
+		: Name(InName), Line(InLine), ExtraTime(InExtraTime)
+	{}
+
+	bool IsPlayer() const { return Name.EqualTo(INVTEXT("You")); }
+	float CalcTime() const { return ExtraTime + UToroSettings::Get()->CalcReadingTime(Line); }
+	bool IsValidData() const { return !Line.IsEmptyOrWhitespace() && ExtraTime >= 0.0f; }
+	bool Matches(const FToroSubtitle& Other) const
+	{
+		return Name.EqualTo(Other.Name) && Line.EqualTo(Other.Line);
+	}
+};
 
 UCLASS(Abstract)
 class TORORUNTIME_API USubtitleWidget final : public UToroManagedWidget
@@ -14,7 +43,39 @@ public:
 
 	USubtitleWidget(const FObjectInitializer& ObjectInitializer);
 
+	UFUNCTION(BlueprintCallable, Category = Subtitles, meta = (WorldContext = "ContextObject"))
+		static void QueueSubtitle(const UObject* ContextObject, const FToroSubtitle& InSubtitle);
+
+	UFUNCTION(BlueprintCallable, Category = Subtitles, meta = (WorldContext = "ContextObject"))
+		static void QueueSubtitles(const UObject* ContextObject, const TArray<FToroSubtitle> InSubtitles);
+
 protected:
 
-	// TODO impl
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UTextBlock> NameText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidget))
+		TObjectPtr<UExprTextBlock> LineText;
+
+	UPROPERTY(Transient, BlueprintReadOnly, Category = Elements, meta = (BindWidgetAnim))
+		TObjectPtr<UWidgetAnimation> SubtitleAnim;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		FLinearColor NameColor;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		FLinearColor PlayerColor;
+
+	FTimerHandle SubtitleTimer;
+	TArray<FToroSubtitle> Subtitles;
+	TObjectPtr<UNarrativeManager> Narrative;
+
+	void NextSubtitle();
+	void ShowSubtitle(const FToroSubtitle& InData);
+	void AddSubtitle(const FToroSubtitle& InSubtitle);
+	void AddSubtitles(const TArray<FToroSubtitle>& InSubtitles);
+	void OnNativeSubtitle(const FText& InText);
+
+	virtual void InitWidget(APlayerController* Controller) override;
+	virtual bool ShouldHideWidget() const override { return Narrative && Narrative->IsInDialogue(); }
 };
