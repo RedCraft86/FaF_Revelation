393e6e1bac551ad989e1597dc927c2f93b1da4c9
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-10-10 17:06:31 -0400

    phase sub classes

---
+++ Source/FaFRevGame/GamePhaseObjects.h
@@ -0,0 +1,44 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Characters/Player/PlayerCharacter.h"
+#include "GamePhase/GamePhaseData.h"
+#include "GamePhaseObjects.generated.h"
+
+UCLASS()
+class TORORUNTIME_API UGamePhaseNode final : public UToroGamePhaseNode
+{
+	GENERATED_BODY()
+
+public:
+
+	UGamePhaseNode();
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = Player, meta = (Bitmask, BitmaskEnum = "/Script/FaFRevGame.EPlayerControlFlags"))
+		int32 PlayerAbilities;
+
+	virtual void ApplyPlayerSettings(AToroPlayerCharacter* PlayerChar) const override
+	{
+		Super::ApplyPlayerSettings(PlayerChar);
+		if (APlayerCharacter* Player = Cast<APlayerCharacter>(PlayerChar))
+		{
+			// TODO apply abilities
+		}
+	}
+};
+
+UCLASS()
+class TORORUNTIME_API UGamePhaseGraph final : public UToroGamePhaseGraph
+{
+	GENERATED_BODY()
+
+public:
+
+	UGamePhaseGraph()
+	{
+		CompatibleNode = UGamePhaseNode::StaticClass();
+	}
+};
