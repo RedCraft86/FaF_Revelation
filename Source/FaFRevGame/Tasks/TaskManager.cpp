58bfecc610a5a1c7428a57bf4aea19237e2ebfc0
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-10-19 19:04:08 -0400

    task manager base impl

---
+++ Source/FaFRevGame/Tasks/TaskManager.cpp
@@ -0,0 +1,28 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "TaskManager.h"
+
+bool UTaskManager::IsTickable() const
+{
+	return Super::IsTickable();
+}
+
+void UTaskManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void UTaskManager::OnWorldBeginPlay(UWorld& InWorld)
+{
+	Super::OnWorldBeginPlay(InWorld);
+}
+
+bool UTaskManager::ShouldCreateSubsystem(UObject* Outer) const
+{
+	return Super::ShouldCreateSubsystem(Outer);
+}
+
+bool UTaskManager::DoesSupportWorldType(const EWorldType::Type WorldType) const
+{
+	return Super::DoesSupportWorldType(WorldType);
+}
---
+++ Source/FaFRevGame/Tasks/TaskManager.h
@@ -0,0 +1,29 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "Helpers/ClassGetterMacros.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "TaskManager.generated.h"
+
+UCLASS()
+class FAFREVGAME_API UTaskManager final : public UTickableWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+
+	UTaskManager() {}
+
+	WORLD_SUBSYSTEM_GETTER(UTaskManager)
+
+private:
+
+
+	virtual bool IsTickable() const override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+	virtual bool DoesSupportWorldType(const EWorldType::Type WorldType) const override;
+	virtual TStatId GetStatId() const override { return GetStatID(); }
+};
