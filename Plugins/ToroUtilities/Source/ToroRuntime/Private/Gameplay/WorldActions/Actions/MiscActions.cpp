d0b8534e52ad70d5e88fd7352409d5fdd63c68b7
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-10-19 21:32:40 -0400

    more actions

--- Plugins/ToroUtilities/Source/ToroRuntime/Private/Gameplay/WorldActions/Actions/MiscActions.cpp
+++ Plugins/ToroUtilities/Source/ToroRuntime/Private/Gameplay/WorldActions/Actions/MiscActions.cpp
@@ -1,6 +1,9 @@
 ﻿// Copyright (C) RedCraft86. All Rights Reserved.
 
 #include "Gameplay/WorldActions/Actions/MiscActions.h"
+#include "Gameplay/GamePhase/GamePhaseManager.h"
+#include "Gameplay/Tutorials/TutorialManager.h"
+#include "SaveSystem/ToroGlobalSave.h"
 #include "Kismet/GameplayStatics.h"
 #include "Helpers/LatentInfo.h"
 
@@ -28,4 +31,42 @@ void FWTMisc_LevelStreaming::OnBeginPlay()
 	{
 		if (It->IsNull()) It.RemoveCurrent();
 	}
+}
+
+void FWTMisc_StepPhase::RunAction()
+{
+	if (UGamePhaseManager* Manager = UGamePhaseManager::Get(GetWorldContext()))
+	{
+		Manager->StepSequence(Index);
+	}
+}
+
+void FWTMisc_Content::RunAction()
+{
+	Content.Remove(FGameplayTag::EmptyTag);
+	UToroSaveManager* SaveManager = UToroSaveManager::Get(GetWorldContext());
+	if (UToroGlobalSave* Save = SaveManager->FindOrAddSave<UToroGlobalSave>(0))
+	{
+		Save->Content.Append(Content);
+		Save->SaveObject(nullptr);
+	}
+}
+
+void FWTMisc_Tutorial::RunAction()
+{
+	if (UTutorialManager* Manager = UTutorialManager::Get(GetWorldContext()))
+	{
+		Manager->QueueTutorials(Tutorials);
+	}
+}
+
+void FWTMisc_Subtitle::RunAction()
+{
+	USubtitleWidget::QueueSubtitles(GetWorldContext(), Subtitles);
+}
+
+void FWTMisc_Notice::RunAction()
+{
+	bTitle ? UNoticeWidget::QueueTitle(GetWorldContext(), Message, bImmediate)
+		: UNoticeWidget::QueueNotice(GetWorldContext(), Message, bImmediate);
 }
--- Plugins/ToroUtilities/Source/ToroRuntime/Public/Gameplay/WorldActions/Actions/MiscActions.h
+++ Plugins/ToroUtilities/Source/ToroRuntime/Public/Gameplay/WorldActions/Actions/MiscActions.h
@@ -2,6 +2,9 @@
 
 #pragma once
 
+#include "GameplayTagContainer.h"
+#include "NativeWidgets/NoticeWidget.h"
+#include "NativeWidgets/SubtitleWidget.h"
 #include "Gameplay/WorldActions/WorldActionBase.h"
 #include "MiscActions.generated.h"
 
@@ -11,7 +14,7 @@ struct TORORUNTIME_API FWATask_Misc : public FWorldActionBase
 	GENERATED_BODY()
 };
 
-USTRUCT(BlueprintType, DisplayName = "Level Streaming")
+USTRUCT(BlueprintType, DisplayName = "Stream Level")
 struct TORORUNTIME_API FWTMisc_LevelStreaming final : public FWATask_Misc
 {
 	GENERATED_BODY()
@@ -29,4 +32,85 @@ protected:
 
 	virtual void RunAction() override;
 	virtual void OnBeginPlay() override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Step Phase")
+struct TORORUNTIME_API FWTMisc_StepPhase final : public FWATask_Misc
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Action)
+		uint8 Index;
+
+	FWTMisc_StepPhase(): Index(0) {}
+
+protected:
+
+	virtual void RunAction() override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Unlock Content")
+struct TORORUNTIME_API FWTMisc_Content final : public FWATask_Misc
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Action)
+		TSet<FGameplayTag> Content;
+
+	FWTMisc_Content() {}
+
+protected:
+
+	virtual void RunAction() override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Show Tutorial")
+struct TORORUNTIME_API FWTMisc_Tutorial final : public FWATask_Misc
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Action, meta = (Categories = "Tutorial"))
+		TArray<FGameplayTag> Tutorials;
+
+	FWTMisc_Tutorial() {}
+
+protected:
+
+	virtual void RunAction() override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Show Subtitle")
+struct TORORUNTIME_API FWTMisc_Subtitle final : public FWATask_Misc
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Action)
+		TArray<FToroSubtitle> Subtitles;
+
+	FWTMisc_Subtitle() {}
+
+protected:
+
+	virtual void RunAction() override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Show Notice")
+struct TORORUNTIME_API FWTMisc_Notice final : public FWATask_Misc
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Action)
+		bool bTitle;
+
+	UPROPERTY(EditAnywhere, Category = Action)
+		bool bImmediate;
+
+	UPROPERTY(EditAnywhere, Category = Action, meta = (ShowOnlyInnerProperties = true))
+		FToroSimpleMsg Message;
+
+	FWTMisc_Notice(): bTitle(false), bImmediate(false) {}
+
+protected:
+
+	virtual void RunAction() override;
 };
