9632c3b82822cbca8bc06e8a4276a3b3bc8e2ea4
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-10-25 17:04:53 -0400

    native QTEs

---
+++ Source/FaFRevGame/QuickTime/NativeQTEs/HoldKeyQTE.h
@@ -0,0 +1,58 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "QuickTime/QTEInstance.h"
+#include "HoldKeyQTE.generated.h"
+
+UCLASS(Abstract)
+class FAFREVGAME_API UHoldKeyQTE final : public UQTEInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	UHoldKeyQTE()
+		: DrainRate(0.2f), GainRate(0.5f), TargetKey(EKeys::F), Progress(0.0f)
+	{
+		WidgetClass = nullptr;
+	}
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		float DrainRate;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		float GainRate;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		FKey TargetKey;
+
+	virtual float GetProgress() override
+	{
+		return Progress;
+	}
+
+	virtual void TickQTE_Implementation(const float DeltaTime) override
+	{
+		if (Controller && Controller->IsInputKeyDown(TargetKey))
+		{
+			Progress += GainRate * DeltaTime;
+			if (Progress >= 1.0f)
+			{
+				MarkFinished(true);
+			}
+		}
+		else
+		{
+			Progress -= DrainRate * DeltaTime;
+			if (Progress < -10.0f)
+			{
+				MarkFinished(false);
+			}
+		}
+	}
+
+protected:
+
+	float Progress;
+};
---
+++ Source/FaFRevGame/QuickTime/NativeQTEs/SpamKeyQTE.h
@@ -0,0 +1,59 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "QuickTime/QTEInstance.h"
+#include "SpamKeyQTE.generated.h"
+
+UCLASS(Abstract)
+class FAFREVGAME_API USpamKeyQTE final : public UQTEInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	USpamKeyQTE()
+		: DrainRate(0.5f), GainAmount(0.1f), TargetKey(EKeys::F), Progress(0.5f)
+	{
+		WidgetClass = nullptr;
+	}
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		float DrainRate;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		float GainAmount;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		FKey TargetKey;
+
+	virtual float GetProgress() override
+	{
+		return Progress;
+	}
+
+	virtual void TickQTE_Implementation(const float DeltaTime) override
+	{
+		Progress -= DrainRate * DeltaTime;
+		if (Progress < 0.0f)
+		{
+			MarkFinished(false);
+		}
+	}
+
+	virtual void OnKeyPress_Implementation(const FKey& Key) override
+	{
+		if (Key == TargetKey)
+		{
+			Progress += GainAmount;
+			if (Progress >= 1.0f)
+			{
+				MarkFinished(true);
+			}
+		}
+	}
+
+protected:
+
+	float Progress;
+};
