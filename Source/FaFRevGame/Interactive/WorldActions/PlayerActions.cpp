9b02fdef802049f66008bf177d04eafc659968ca
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-10-28 14:26:24 -0400

    Player related actions

---
+++ Source/FaFRevGame/Interactive/WorldActions/PlayerActions.cpp
@@ -0,0 +1,40 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#include "PlayerActions.h"
+#include "Player/PlayerCharacter.h"
+
+void FWTPlayer_LockTag::RunAction()
+{
+	if (APlayerCharacter* Player = APlayerCharacter::Get<APlayerCharacter>(GetWorldContext()))
+	{
+		for (const FGameplayTag& Tag : AddTags)
+		{
+			Player->AddLockTag(Tag);
+		}
+		for (const FGameplayTag& Tag : RemoveTags)
+		{
+			Player->RemoveLockTag(Tag);
+		}
+	}
+}
+
+void FWTPlayer_LockOn::RunAction()
+{
+	if (AActor* TargetPtr = Target.LoadSynchronous())
+	{
+		if (APlayerCharacter* Player = APlayerCharacter::Get<APlayerCharacter>(GetWorldContext()))
+		{
+			Player->LockOnSpeed = LockOnSpeed;
+			Player->SetLockOnTarget(TargetPtr);
+		}
+	}
+}
+
+void FWTPlayer_MoveSpeed::RunAction()
+{
+	if (ModifierKey.IsNone() || Multiplier < 0.1f) return;
+	if (APlayerCharacter* Player = APlayerCharacter::Get<APlayerCharacter>(GetWorldContext()))
+	{
+		bRemove ? Player->RemoveMovementSpeedMod(ModifierKey) : Player->AddMovementSpeedMod(ModifierKey, Multiplier);
+	}
+}
---
+++ Source/FaFRevGame/Interactive/WorldActions/PlayerActions.h
@@ -0,0 +1,70 @@
+﻿// Copyright (C) RedCraft86. All Rights Reserved.
+
+#pragma once
+
+#include "GameplayTagContainer.h"
+#include "WorldActions/WorldActionBase.h"
+#include "PlayerActions.generated.h"
+
+USTRUCT(BlueprintInternalUseOnly, DisplayName = "Player", meta = (Hidden))
+struct TORORUNTIME_API FWATask_Player : public FWorldActionBase
+{
+	GENERATED_BODY()
+};
+
+USTRUCT(BlueprintType, DisplayName = "Lock Tag")
+struct TORORUNTIME_API FWTPlayer_LockTag final : public FWATask_Player
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Action, meta = (Category = "PlayerLock"))
+		FGameplayTagContainer AddTags;
+
+	UPROPERTY(EditAnywhere, Category = Action, meta = (Category = "PlayerLock"))
+		FGameplayTagContainer RemoveTags;
+
+	FWTPlayer_LockTag() {}
+
+protected:
+
+	virtual void RunAction() override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Lock On")
+struct TORORUNTIME_API FWTPlayer_LockOn final : public FWATask_Player
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Action)
+		float LockOnSpeed;
+
+	UPROPERTY(EditAnywhere, Category = Action, meta = (MustImplement = "/Script/ToroCore.CharInterface"))
+		TSoftObjectPtr<AActor> Target;
+
+	FWTPlayer_LockOn(): LockOnSpeed(false) {}
+
+protected:
+
+	virtual void RunAction() override;
+};
+
+USTRUCT(BlueprintType, DisplayName = "Move Speed")
+struct TORORUNTIME_API FWTPlayer_MoveSpeed final : public FWATask_Player
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Action)
+		bool bRemove;
+
+	UPROPERTY(EditAnywhere, Category = Action)
+		FName ModifierKey;
+
+	UPROPERTY(EditAnywhere, Category = Action, meta = (ClampMin = 0.1f, UIMin = 0.1f))
+		float Multiplier;
+
+	FWTPlayer_MoveSpeed(): bRemove(false), Multiplier(1.0f) {}
+
+protected:
+
+	virtual void RunAction() override;
+};
